203e4cd6ccccdc0208d2e3174c28a88be883b626
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-04-06 15:52:15 +0200

    Start work on new corpus definition (#1)
    Not present yet: the fields.

---
+++ ianalyzer/corpora/dutchbanking.py
@@ -0,0 +1,53 @@
+import re
+import os
+import os.path as op
+import logging
+
+from flask import current_app
+
+from .common import XMLCorpus, Field
+
+
+class DutchBanking(XMLCorpus):
+    """ Alto XML corpus of Dutch banking year records. """
+    
+    # Data overrides from .common.Corpus (fields at bottom of class)
+    data_directory = current_app.config['DUTCHBANK_DATA']
+    min_date = current_app.config['DUTCHBANK_MIN_DATE']
+    max_date = current_app.config['DUTCHBANK_MAX_DATE']
+    es_index = current_app.config['DUTCHBANK_ES_INDEX']
+    es_doctype = current_app.config['DUTCHBANK_ES_DOCTYPE']
+    es_settings = None
+    
+    # Data overrides from .common.XMLCorpus
+    xml_tag_toplevel = 'alto'
+    xml_tag_entry = 'TextBlock'
+    
+    # New data members
+    filename_pattern = re.compile('([A-Z]+)_(\d{4})_(\d)_(\d{5})')
+    non_xml_msg = 'Skipping non-XML file {}'
+    non_match_msg = 'Skipping XML file with nonmatching name {}'
+    
+    def sources(self, start=min_date, end=max_date):
+        logger = logging.getLogger(__name__)
+        for filename in os.listdir(self.data_directory):
+            name, extension = op.splitext(filename)
+            if extension != '.xml':
+                logger.debug(self.non_xml_msg.format(filename))
+                continue
+            match = self.filename_pattern.match(name)
+            if not match:
+                logger.warning(self.non_match_msg.format(filename))
+                continue
+            bank, year, serial, scan = match.groups()
+            # TODO: check the date
+            yield op.join(self.data_directory, filename), {
+                'bank': bank,
+                'year': year,
+                'serial': serial,
+                'scan': scan,
+            }
+    
+    fields = [
+        
+    ]
