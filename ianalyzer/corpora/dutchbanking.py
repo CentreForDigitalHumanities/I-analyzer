03f3d0c1544e634314d46fd08205610c99817313
Julian Gonggrijp <j.gonggrijp@uu.nl>
2017-04-12 17:27:20 +0200

    Add the fields for the dutch banking corpus (#1)
    Together with the changes in commits 167755f and a2efd3a, this
    appears to make indexing work.

--- ianalyzer/corpora/dutchbanking.py
+++ ianalyzer/corpora/dutchbanking.py
@@ -6,6 +6,8 @@ import logging
 from flask import current_app
 
 from .. import config
+from ..extract import XML, Metadata, Combined
+from ..filters import MultipleChoiceFilter, RangeFilter
 from .common import XMLCorpus, Field
 
 
@@ -51,5 +53,89 @@ class DutchBanking(XMLCorpus):
             }
     
     fields = [
-        
+        Field(
+            name='bank',
+            description='Banking concern to which the report belongs.',
+            es_mapping={'type': 'keyword'},
+            search_filter=MultipleChoiceFilter(
+                description='Search only within these banks.',
+                options=sorted(config.DUTCHBANK_MAP.values()),
+            ),
+            extractor=Metadata(
+                key='bank',
+                transform=lambda x: config.DUTCHBANK_MAP[x],
+            ),
+        ),
+        Field(
+            name='year',
+            description='Year of the financial report.',
+            es_mapping={'type': 'integer'},
+            search_filter=RangeFilter(
+                description='Restrict the years from which search results will be returned.',
+                lower=min_date.year,
+                upper=max_date.year,
+            ),
+            extractor=Metadata(key='year', transform=int),
+        ),
+        Field(
+            name='issue',
+            description='Issue number of the financial report.',
+            es_mapping={'type': 'integer'},
+            extractor=Metadata(key='serial', transform=int),
+        ),
+        Field(
+            name='scan',
+            description='Scan number within the financial report. A scan contains one or two pages.',
+            es_mapping={'type': 'integer'},
+            extractor=Metadata(key='scan', transform=int),
+        ),
+        Field(
+            name='id',
+            description='Unique identifier of the text block.',
+            extractor=Combined(
+                Metadata(key='bank'),
+                Metadata(key='year'),
+                XML(attribute='ID'),
+                transform=lambda x: '_'.join(x),
+            ),
+        ),
+        Field(
+            name='content',
+            description='Text content of the block.',
+            extractor=XML(
+                tag='String',
+                attribute='CONTENT',
+                recursive=True,
+                multiple=True,
+                transform=lambda x: ' '.join(x),
+            ),
+        ),
+        Field(
+            name='hpos',
+            description='Horizontal position on the scan in pixels.',
+            indexed=False,
+            es_mapping={'type': 'integer'},
+            extractor=XML(attribute='HPOS', transform=int),
+        ),
+        Field(
+            name='vpos',
+            description='Vertical position on the scan in pixels.',
+            indexed=False,
+            es_mapping={'type': 'integer'},
+            extractor=XML(attribute='VPOS', transform=int),
+        ),
+        Field(
+            name='width',
+            description='Width on the scan in pixels.',
+            indexed=False,
+            es_mapping={'type': 'integer'},
+            extractor=XML(attribute='WIDTH', transform=int),
+        ),
+        Field(
+            name='height',
+            description='Height on the scan in pixels.',
+            indexed=False,
+            es_mapping={'type': 'integer'},
+            extractor=XML(attribute='HEIGHT', transform=int),
+        ),
     ]
