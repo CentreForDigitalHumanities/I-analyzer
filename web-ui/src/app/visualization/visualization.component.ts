ff86e99f949c75f538d5850e3abc39eec04d3c28
BeritJanssen <berit.janssen@gmail.com>
2017-10-11 10:54:29 +0200

    d3 types of axes and scales declared

--- web-ui/src/app/visualization/visualization.component.ts
+++ web-ui/src/app/visualization/visualization.component.ts
@@ -20,14 +20,13 @@ export class VisualizationComponent implements OnInit, OnChanges {
   @Input() private searchData: Array<any>;
 
   private margin: any = { top: 20, bottom: 20, left: 20, right: 20};		    
-  private chart: any;		  
+  private chart: any; 
   private width: number;	  
   private height: number;	  
-  private xScale: any;		  
-  private yScale: any;		  
-  private colors: any;		  
-  private xAxis: any;		  
-  private yAxis: any;
+  private xScale: d3.ScaleLinear<number, number>;		  
+  private yScale: d3.ScaleLinear<number, number>;		  	  
+  private xAxis: d3.Selection<any, any, any, any>;	  
+  private yAxis: d3.Selection<any, any, any, any>;
   private yMax: number;
   private timeLineData: Array<YearFrequencyPair>;
   private xDomain: Array<number>;
