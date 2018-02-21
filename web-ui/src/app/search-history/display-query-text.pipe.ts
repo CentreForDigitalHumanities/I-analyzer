d9d24fb7059bb4c740afaf7e61ec40db873b6d80
BeritJanssen <berit.janssen@gmail.com>
2018-02-21 15:46:09 +0100

    query text pipe added

---
+++ web-ui/src/app/search-history/display-query-text.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { QueryModel } from '../models/index'
+
+@Pipe({name: 'displayQueryText'})
+export class DisplayQueryTextPipe implements PipeTransform {
+
+    transform(queryModel: QueryModel): string {
+        let queryText = queryModel.queryText;
+
+        return queryText;
+    }
+
+}
