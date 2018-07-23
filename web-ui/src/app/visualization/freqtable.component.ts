08a68726a377774b2286a261ae9ed0448fb574ec
Jelte van Boheemen <j.vanboheemen@gmail.com>
2018-07-23 13:59:21 +0200

    component structure

--- web-ui/src/app/visualization/freqtable.component.ts
+++ web-ui/src/app/visualization/freqtable.component.ts
@@ -1,4 +1,4 @@
-import { Input, Component, OnChanges, ElementRef, ViewEncapsulation, SimpleChanges } from '@angular/core';
+import { Input, Component, OnInit, OnChanges, ElementRef, ViewEncapsulation, SimpleChanges } from '@angular/core';
 
 import * as d3 from 'd3';
 import * as _ from "lodash";
