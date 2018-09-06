c93508c06b23272edefaf6f722c1b9b7de686cb4
BeritJanssen <berit.janssen@gmail.com>
2018-09-06 10:15:06 +0200

    added button to calendar widget, changed condition for enabling filter

--- web-ui/src/app/search/search.component.ts
+++ web-ui/src/app/search/search.component.ts
@@ -212,7 +212,8 @@ export class SearchComponent implements OnInit, OnDestroy {
     }
 
     public updateFilterData(name: string, data: SearchFilterData) {
-        if (this.hasSearched!=undefined && this.queryField[name].data != data) {
+        if (this.hasSearched!=undefined && JSON.stringify(this.queryField[name].data) != JSON.stringify(data)) {
+            console.log(JSON.stringify(data.data));
             this.enableFilter(name);
         }
         this.queryField[name].data = data;
