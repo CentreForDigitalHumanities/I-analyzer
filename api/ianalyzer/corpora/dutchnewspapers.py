2477835945b67a346d02901a63c21a0b5e72ef31
Jelte van Boheemen <j.vanboheemen@gmail.com>
2018-09-11 09:44:48 +0200

    removed sortable from text fields in newspapers corpus

--- api/ianalyzer/corpora/dutchnewspapers.py
+++ api/ianalyzer/corpora/dutchnewspapers.py
@@ -166,7 +166,6 @@ class DutchNewspapers(XMLCorpus):
             preselected=True,
             es_mapping={'type': 'keyword'},
             visualization_type='term_frequency',
-            # sortable=True,
             search_filter=filters.MultipleChoiceFilter(
                 description='Accept only articles in these newspapers.',
                 options=papers
@@ -318,7 +317,6 @@ class DutchNewspapers(XMLCorpus):
             preselected=True,
             es_mapping={'type': 'keyword'},
             visualization_type='term_frequency',
-            sortable=True,
             search_filter=filters.MultipleChoiceFilter(
                 description='Accept only articles in newspapers with this distribution area.',
                 options=list(distribution.keys())
@@ -341,7 +339,6 @@ class DutchNewspapers(XMLCorpus):
             preselected=True,
             es_mapping={'type': 'keyword'},
             visualization_type='term_frequency',
-            sortable=True,
             search_filter=filters.MultipleChoiceFilter(
                 description='Accept only articles in newspapers with this publication frequency.',
                 options=['Dag', 'Week', 'Maand'],
