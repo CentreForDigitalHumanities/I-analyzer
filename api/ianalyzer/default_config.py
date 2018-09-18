78777dd513dbd3ee8c4e283ea07454e6d9c5c04c
Jelte van Boheemen <j.vanboheemen@gmail.com>
2018-09-18 12:40:47 +0200

    updated default config

--- api/ianalyzer/default_config.py
+++ api/ianalyzer/default_config.py
@@ -20,7 +20,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # the corpora dictionary provides the file path of the corpus definition(s)
 # these definitions can be anywhere on the file system
 CORPORA = {
-   'times': 'ianalyzer/corpora/times.py'
+    'times': 'ianalyzer/corpora/times.py'
 }
 
 # Specify indexing servers here
@@ -31,12 +31,12 @@ SERVERS = {
         'port': 9200,
         'username': '',
         'password': '',
-        'chunk_size': 900, # Maximum number of documents sent during ES bulk operation
-        'max_chunk_bytes': 1*1024*1024, # Maximum size of ES chunk during bulk operation
-        'bulk_timeout': '60s', # Timeout of ES bulk operation
-        'overview_query_size': 20, # Number of results to appear in the overview query
-        'scroll_timeout': '3m', # Time before scroll results time out
-        'scroll_page_size': 5000 # Number of results per scroll page
+        'chunk_size': 900,  # Maximum number of documents sent during ES bulk operation
+        'max_chunk_bytes': 1*1024*1024,  # Maximum size of ES chunk during bulk operation
+        'bulk_timeout': '60s',  # Timeout of ES bulk operation
+        'overview_query_size': 20,  # Number of results to appear in the overview query
+        'scroll_timeout': '3m',  # Time before scroll results time out
+        'scroll_page_size': 5000  # Number of results per scroll page
     }
 }
 
@@ -46,23 +46,41 @@ CORPUS_SERVER_NAMES = {
 }
 
 # Index configurations
+DUTCHNEWSPAPERS_TITLE = "Dutch Newspapers"
+DUTCHNEWSPAPERS_DESCRIPTION = "Description about this corpus"
+DUTCHNEWSPAPERS_ES_INDEX = 'dutchnewspapers'
+DUTCHNEWSPAPERS_ES_DOCTYPE = 'article'
+DUTCHNEWSPAPERS_DATA = '/Users/3248526/corpora/kranten_pd_voorbeeld'
+DUTCHNEWSPAPERS_MIN_DATE = datetime(year=1600, month=1, day=1)
+DUTCHNEWSPAPERS_MAX_DATE = datetime(year=2018, month=12, day=31)
+DUTCHNEWSPAPERS_IMAGE = '/static/images/dutchnewspapers.jpg'
+
+TML_TITLE = "Thesaurus Musicarum Latinarum"
+TML_DESCRIPTION = "Description about this corpus"
+TML_ES_INDEX = 'tml'
+TML_ES_DOCTYPE = 'article'
+TML_DATA = '/Users/3248526/corpora/tml'
+TML_MIN_DATE = datetime(year=1, month=1, day=1)
+TML_MAX_DATE = datetime(year=2018, month=12, day=31)
+TML_IMAGE = '/static/images/times.jpg'
+
 TIMES_TITLE = "Times"
 TIMES_DESCRIPTION = "Newspaper archive, 1785-2010"
-TIMES_ES_INDEX='times'
-TIMES_ES_DOCTYPE='article'
-TIMES_DATA='/mnt/times'
-TIMES_MIN_DATE=datetime(year=1785, month=1, day=1)
-TIMES_MAX_DATE=datetime(year=2010, month=12, day=31)
-TIMES_IMAGE='/static/images/times.jpg'
+TIMES_ES_INDEX = 'times'
+TIMES_ES_DOCTYPE = 'article'
+TIMES_DATA = '/mnt/times'
+TIMES_MIN_DATE = datetime(year=1785, month=1, day=1)
+TIMES_MAX_DATE = datetime(year=2010, month=12, day=31)
+TIMES_IMAGE = '/static/images/times.jpg'
 
 DUTCHBANK_TITLE = "Dutch Banking"
 DUTCHBANK_DESCRIPTION = "Annual reports of Dutch finanical institutes"
-DUTCHBANK_ES_INDEX='dutchbank'
-DUTCHBANK_ES_DOCTYPE='article'
-DUTCHBANK_DATA='/mnt/dutchbank'
-DUTCHBANK_MIN_DATE=datetime(year=1785, month=1, day=1)
-DUTCHBANK_MAX_DATE=datetime(year=2010, month=12, day=31)
-DUTCHBANK_IMAGE='/static/images/dutchbanking.jpg'
+DUTCHBANK_ES_INDEX = 'dutchbank'
+DUTCHBANK_ES_DOCTYPE = 'article'
+DUTCHBANK_DATA = '/mnt/dutchbank'
+DUTCHBANK_MIN_DATE = datetime(year=1785, month=1, day=1)
+DUTCHBANK_MAX_DATE = datetime(year=2010, month=12, day=31)
+DUTCHBANK_IMAGE = '/static/images/dutchbanking.jpg'
 DUTCHBANK_MAP = {
     'AA':       'ABN AMRO',
     'AB':       'Amsterdamsche Bank',
@@ -102,5 +120,5 @@ DUTCHBANK_MAP = {
     'TRIODOS':  'Triodos Bank',
 }
 
-DUTCHNEWSPAPERS_IMAGE='/static/images/dutchnewspapers.jpg'
-JEWISH_INSCRIPTIONS_IMAGE='/static/images/jewish_inscriptions.jpg'
+
+JEWISH_INSCRIPTIONS_IMAGE = '/static/images/jewish_inscriptions.jpg'
