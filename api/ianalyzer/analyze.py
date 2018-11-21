fa7bdbdca07e38e94d08e410675507309d6c7ac5
BeritJanssen <berit.janssen@gmail.com>
2018-11-21 10:12:29 +0100

    added word similarities to analyze.py

--- api/ianalyzer/analyze.py
+++ api/ianalyzer/analyze.py
@@ -1,3 +1,7 @@
+
+import os
+import pickle
+
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 
@@ -7,3 +11,83 @@ def make_wordcloud_data(list_of_content):
     words = cv.get_feature_names()
     output = [{'key': word, 'doc_count': int(counts[i])+1} for i, word in enumerate(words)]
     return output
+
+
+def get_diachronic_contexts(query_term, number_similar=10):
+    complete, binned = load_data()
+    word_list = find_n_most_similar(
+        complete['svd_ppmi'],
+        complete['transformer'],
+        query_term,
+        number_similar)
+    out_list = []
+    for time_bin in binned:
+        this_dict = similarity_with_top_terms(
+            time_bin['svd_ppmi'],
+            time_bin['transformer'],
+            query_term,
+            word_list)
+        this_dict['time_point'] = np.mean(
+            [time_bin['start_year'], time_bin['end_year']])
+        out_list.append(this_dict)
+    return out_list
+
+
+def load_data(directory=DIRECTORY, complete=COMPLETE_FN, binned=BINNED_FN):
+    with open(directory+complete, "rb") as f:
+        complete = pickle.load(f)
+    with open(directory+binned, "rb") as f:
+        binned = pickle.load(f)
+    return complete, binned
+
+
+def find_n_most_similar(matrix, transformer, query_term, n):
+    """given a matrix of svd_ppmi values 
+    and the transformer (i.e., sklearn CountVectorizer),
+    determine which n terms match the given query term best
+    """
+    index = next(
+        (i for i, a in enumerate(transformer.get_feature_names())
+         if a == query_term), None)
+    if not(index):
+        print("query term not found")
+        return None
+    vec = matrix[:, index]
+    similarities = cosine_similarity(matrix, vec)
+    sorted_sim = np.sort(similarities)
+    most_similar_indices = np.where(similarities >= sorted_sim[-n])
+    output_terms = [
+        transformer.get_feature_names()[index]
+        for index in most_similar_indices[0]
+    ]
+    return output_terms
+
+
+def similarity_with_top_terms(matrix, transformer, query_term, word_list):
+    """given a matrix of svd_ppmi values,
+    the transformer (i.e., sklearn CountVectorizer), and a word list
+    of the terms matching the query term best over the whole corpus,
+    determine the similarity for each time interval
+    """
+    query_index = next(
+            (i for i, a in enumerate(transformer.get_feature_names())
+             if a == query_term), None)
+    query_vec = matrix[:, query_index]
+    out = {}
+    for term in word_list:
+        index = next(
+            (i for i, a in enumerate(transformer.get_feature_names())
+             if a == term), None)
+        if not index:
+            value = None
+        else:
+            value = cosine_similarity(matrix[:, index], query_vec)
+        out[term] = value
+    return out
+
+
+def cosine_similarity(array1, array2):
+    dot = array2.dot(array1)
+    vec_norm = np.linalg.norm(array1)
+    mat_norm = np.linalg.norm(array2)
+    return dot / (vec_norm * mat_norm)
