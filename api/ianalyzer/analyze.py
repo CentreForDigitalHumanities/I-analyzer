d7e19303e614961e4212e35199e5e7e75f66889c
BeritJanssen <berit.janssen@gmail.com>
2018-11-29 10:45:30 +0100

    checking for empty content in word cloud analysis

--- api/ianalyzer/analyze.py
+++ api/ianalyzer/analyze.py
@@ -2,6 +2,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 
 def make_wordcloud_data(list_of_content):
+    for content in list_of_content:
+        if content != '':
+            list_of_content.remove(content)
     cv = CountVectorizer(stop_words="english", max_features=50)
     counts = cv.fit_transform(list_of_content).toarray().ravel()
     words = cv.get_feature_names()
