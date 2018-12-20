b2e22e14f17bb01195264faed3ca6f53b1dfa242
Jelte van Boheemen <j.vanboheemen@gmail.com>
2018-12-20 14:25:05 +0100

    remove unused import that causes error

--- web-ui/src/app/visualization/related-words.component.ts
+++ web-ui/src/app/visualization/related-words.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 
 import { ManualService, SearchService } from '../services/index';
-import { ExecSyncOptionsWithBufferEncoding } from 'child_process';
 @Component({
     selector: 'ia-related-words',
     templateUrl: './related-words.component.html',
@@ -21,13 +20,13 @@ export class RelatedWordsComponent implements OnChanges {
     @Input() corpusName: string;
     public zoomedInData; // data requested when clicking on a time interval
     // colour-blind friendly colorPalette retrieved from colorbrewer2.org
-    public colorPalette = ['#a6611a','#dfc27d','#80cdc1','#018571','#543005','#bf812d','#f6e8c3','#f5f5f5','#c7eae5','#35978f','#003c30']
+    public colorPalette = ['#a6611a', '#dfc27d', '#80cdc1', '#018571', '#543005', '#bf812d', '#f6e8c3', '#f5f5f5', '#c7eae5', '#35978f', '#003c30']
     public chartOptions = {
         elements: {
             line: {
                 tension: 0, // disables bezier curves
             }
-        }, 
+        },
         scales: {
             yAxes: [{
                 scaleLabel: {
@@ -38,11 +37,11 @@ export class RelatedWordsComponent implements OnChanges {
             xAxes: [],
         }
     }
-    
+
     constructor(private manualService: ManualService, private searchService: SearchService) { }
 
     ngOnChanges() {
-        this.searchData.datasets.map( (d, index) => {
+        this.searchData.datasets.map((d, index) => {
             d.fill = false;
             d.borderColor = this.colorPalette[index];
         })
@@ -54,19 +53,20 @@ export class RelatedWordsComponent implements OnChanges {
 
     zoomTimeInterval(event) {
         this.searchService.getRelatedWordsTimeInterval(
-            this.queryText, 
+            this.queryText,
             this.corpusName,
             this.searchData.labels[event.element._index])
-             .then(results => {
+            .then(results => {
                 this.zoomedInData = results['graphData'];
                 this.zoomedInData.datasets
-                 .sort((a, b) => { return b.data[0] - a.data[0] })
-                 .map((d, index) => {
-                    d.backgroundColor = this.colorPalette[index];
-                })
+                    .sort((a, b) => { return b.data[0] - a.data[0] })
+                    .map((d, index) => {
+                        d.backgroundColor = this.colorPalette[index];
+                    })
                 // hide grid lines as we only have one data point on x axis
-                this.chartOptions.scales.xAxes.push({gridLines: {
-                        display:false
+                this.chartOptions.scales.xAxes.push({
+                    gridLines: {
+                        display: false
                     }
                 })
             })
