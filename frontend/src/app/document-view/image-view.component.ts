2afb6f7c3abaae893cc144d2031717cf5987c519
BeritJanssen <berit.janssen@gmail.com>
2019-09-25 14:49:35 +0200

    Revert "calling showImage via ViewChild"
    This reverts commit c58c8cb476927b6e6e92dec764fc248d22f5a4a4.

--- frontend/src/app/document-view/image-view.component.ts
+++ frontend/src/app/document-view/image-view.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { FoundDocument } from '../models';
-import { ImageViewerComponent } from 'ng2-image-viewer';
 
 @Component({
   selector: 'ia-image-view',
@@ -14,14 +13,11 @@ export class ImageViewComponent implements OnChanges {
     @Input() public allowDownload: boolean;
     @Input() public document: FoundDocument;
 
-    @ViewChild('app-image-viewer') private imageViewer: ImageViewerComponent;
-
     public downloadPath: string; // optional: downloadable content may differ from displayed content
     
     constructor() { }
 
     ngOnChanges() {
         this.downloadPath = this.document.fieldValues['image_path'];
-        this.imageViewer.showImage();
     }
 }
