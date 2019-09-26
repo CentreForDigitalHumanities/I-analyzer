a8a3dfacba6a865f1b151daf5b65c7bc0e574e9b
BeritJanssen <berit.janssen@gmail.com>
2019-09-26 10:53:19 +0200

    fixed range filter test

--- frontend/src/app/filter/range-filter.component.spec.ts
+++ frontend/src/app/filter/range-filter.component.spec.ts
@@ -2,20 +2,34 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RangeFilterComponent } from './range-filter.component';
 
+import { commonTestBed } from '../common-test-bed';
+import { RangeFilterData } from '../models';
+
 describe('RangeFilterComponent', () => {
   let component: RangeFilterComponent;
   let fixture: ComponentFixture<RangeFilterComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RangeFilterComponent ]
-    })
-    .compileComponents();
+    commonTestBed().testingModule.compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RangeFilterComponent);
     component = fixture.componentInstance;
+    let mockRangeData = {
+        filterType: 'RangeFilter',
+        min: 1984,
+        max: 1984
+    } as RangeFilterData;
+    let data = { 
+        fieldName: 'year',
+        description: 'Orwellian',
+        useAsFilter: false,
+        defaultData: mockRangeData,
+        currentData: mockRangeData
+    }
+    component.filter = data;
+    component.data = data;
     fixture.detectChanges();
   });
 
