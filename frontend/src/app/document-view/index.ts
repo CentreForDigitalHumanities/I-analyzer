460edb3000ccddd6eb615dd8b9eb28ed0c88b3ba
BeritJanssen <berit.janssen@gmail.com>
2019-10-24 12:12:35 +0200

    dependencies for ng2-image-viewer and material-icons removed

---
+++ frontend/src/app/document-view/index.ts
@@ -0,0 +1,5 @@
+export * from './document-view.component';
+export * from './image-navigation.component';
+export * from './image-view.component';
+export * from './scan-image.component';
+export * from './scan-pdf.component';
