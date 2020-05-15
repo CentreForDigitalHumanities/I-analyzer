db2d1a33f745ce96f48fcb670ad88cfad2d5c451
Alex Hebing <a.h.j.hebing@uu.nl>
2020-05-15 09:51:15 +0200

    Add corpus definitions for peace portal

---
+++ backend/corpora/peaceportal/epidat.py
@@ -0,0 +1,158 @@
+import re
+from copy import copy
+from flask import current_app
+
+from addcorpus.extract import XML, Constant, HTML
+from addcorpus.corpus import Field
+from corpora.peaceportal.peaceportal import PeacePortal, categorize_material
+
+
+class Epidat(PeacePortal):
+
+    data_directory = current_app.config['PEACEPORTAL_EPIDAT_DATA']
+    es_index = current_app.config['PEACEPORTAL_EPIDAT_ES_INDEX']
+
+    def __init__(self):
+        self.source_database.extractor = Constant(
+            value='Epidat (Steinheim Institute)'
+        )
+
+        self._id.extractor = XML(
+            tag=['teiHeader', 'fileDesc', 'sourceDesc',
+                 'msDesc', 'msIdentifier', 'idno'],
+            multiple=False,
+            toplevel=False,
+            flatten=True
+        )
+
+        self.url.extractor = HTML(
+            tag=['teiHeader', 'fileDesc', 'publicationStmt', 'idno'],
+            multiple=False,
+            toplevel=False,
+            flatten=True,
+            attribute_filter={
+                'attribute': 'type',
+                'value': 'url'
+            }
+        )
+
+        self.year.extractor = XML(
+            tag=['teiHeader', 'fileDesc', 'sourceDesc', 'msDesc',
+                 'history', 'origin', 'origDate', 'date'],
+            toplevel=False,
+            transform=lambda x: re.search('[1-2]{0,1}[0-9]{3}', x)[0],
+        )
+
+        self.transcription.extractor = XML(
+            tag=['text', 'body', 'div'],
+            toplevel=False,
+            multiple=False,
+            flatten=True,
+            transform=lambda x: ' '.join(x.split()),
+            transform_soup_func=extract_transcript
+        )
+
+        self.names.extractor = XML(
+            tag=['teiHeader', 'profileDesc',
+                 'particDesc', 'listPerson', 'person'],
+            flatten=True,
+            multiple=True,
+            toplevel=False,
+        )
+
+        self.sex.extractor = XML(
+            tag=['teiHeader', 'profileDesc',
+                 'particDesc', 'listPerson', 'person'],
+            attribute='sex',
+            multiple=True,
+            toplevel=False,
+            transform=lambda x: ['M' if s ==
+                                 '1' else 'F' if s == '2' else None for s in x]
+        )
+
+        self.country.extractor = XML(
+            tag=['teiHeader', 'fileDesc', 'sourceDesc', 'msDesc',
+                 'history', 'origin', 'origPlace', 'country'],
+            toplevel=False,
+            transform_soup_func=extract_country,
+            flatten=True,
+        )
+
+        self.provenance.extractor = XML(
+            tag=['teiHeader', 'fileDesc', 'sourceDesc', 'msDesc',
+                 'history', 'origin', 'origPlace', 'country', 'region'],
+            toplevel=False,
+            flatten=True
+        )
+
+        self.material.extractor = XML(
+            tag=['teiHeader', 'fileDesc', 'sourceDesc', 'msDesc', 'physDesc',
+                 'objectDesc', 'supportDesc', 'support', 'p', 'material'],
+            toplevel=False,
+            flatten=True,
+            transform=lambda x: categorize_material(x)
+        )
+
+        self.material_details.extractor = XML(
+            tag=['teiHeader', 'fileDesc', 'sourceDesc', 'msDesc', 'physDesc',
+                 'objectDesc', 'supportDesc', 'support', 'p', 'material'],
+            toplevel=False,
+            flatten=True
+        )
+
+        self.language.extractor = XML(
+            tag=['teiHeader', 'profileDesc', 'langUsage', 'language'],
+            toplevel=False,
+            multiple=True,
+            transform=lambda x: [''.join(s.split()) for s in x]
+        )
+
+        self.commentary.extractor = XML(
+            tag=['text', 'body'],
+            toplevel=False,
+            transform_soup_func=extract_commentary,
+            flatten=True
+        )
+
+
+def extract_transcript(soup):
+    '''
+    Helper function to ensure correct extraction of the transcripts.
+    Note that there are multiple formats in which these are stored,
+    but the text that we need is always in the `<ab>` children of
+    `['text', 'body', 'div']` (where div has `type=edition`, this is always the first one).
+    '''
+    if not soup:
+        return
+    return soup.find_all('ab')
+
+
+def extract_commentary(soup):
+    '''
+    Helper function to extract commentary from the <body> tag.
+    '''
+    incl_header = soup.find('div', {'subtype': 'Endkommentar'})
+    if incl_header:
+        return incl_header.find('p')
+    # if there is no endcommentary, return the first commentary we find, or None
+    commentary = soup.find('div', {'type': 'commentary'})
+    if commentary:
+        return commentary.find('p')
+    return None
+
+
+def extract_country(soup):
+    '''
+    Helper function to extract country.
+    This is needed because the output of `flatten` would otherwise include the text contents
+    of the `<region>`.
+    '''
+    if not soup:
+        return
+    cloned_soup = copy(soup)
+    region = cloned_soup.find('region')
+    region.extract()
+    return cloned_soup
+
+
+
---
+++ backend/corpora/peaceportal/peaceportal.py
@@ -0,0 +1,225 @@
+import os
+import os.path as op
+import logging
+from datetime import datetime
+
+from addcorpus.corpus import XMLCorpus, Field
+from addcorpus.extract import XML, Constant
+from addcorpus.filters import MultipleChoiceFilter, RangeFilter
+
+
+class PeacePortal(XMLCorpus):
+    '''
+    Base class for corpora in the PEACE portal.
+
+    This supplies the frontend with the information it needs.
+    Child corpora should only provide extractors for each field.
+    Consequently, create indices (with alias 'peaceportal') from
+    the corpora specific definitions, and point the application
+    to this base corpus.
+    '''
+
+    title = "PEACE Portal"
+    description = "A collection of inscriptions on Jewish burial sites"
+    min_date = datetime(year=769, month=1, day=1)
+    max_date = datetime(year=2020, month=12, day=31)
+    visualize = []
+    es_index = 'peaceportal'
+    # fields below are required by code but not actually used
+    image = 'bogus'
+    data_directory = 'bogus'
+
+    # Data overrides from .common.XMLCorpus
+    tag_toplevel = ''
+    tag_entry = 'TEI'
+
+    # New data members
+    non_xml_msg = 'Skipping non-XML file {}'
+    non_match_msg = 'Skipping XML file with nonmatching name {}'
+
+    def sources(self, start, end):
+        logger = logging.getLogger(__name__)
+        for directory, _, filenames in os.walk(self.data_directory):
+            for filename in filenames:
+                name, extension = op.splitext(filename)
+                full_path = op.join(directory, filename)
+                if extension != '.xml':
+                    logger.debug(self.non_xml_msg.format(full_path))
+                    continue
+                yield full_path
+
+    source_database = Field(
+        name='source_database',
+        display_name='Source database',
+        description='The database a record originates from.',
+        es_mapping={'type': 'keyword'},
+        search_filter=MultipleChoiceFilter(
+            description='Search only within these databases.',
+            option_count=2,
+        ),
+        csv_core=True
+    )
+
+    _id = Field(
+        name='id',
+        display_name='ID',
+        description='ID of the inscription entry.',
+        csv_core=True,
+        es_mapping={'type': 'keyword'}
+    )
+
+    url = Field(
+        name='url',
+        display_name='URL',
+        description='URL of the inscription entry.',
+        es_mapping={'type': 'keyword'}
+    )
+
+    year = Field(
+        name='year',
+        display_name='Year',
+        description='Year of origin of the inscription.',
+        es_mapping={'type': 'integer'},
+        search_filter=RangeFilter(
+            description='Restrict the years from which search results will be returned.',
+            lower=min_date.year,
+            upper=max_date.year,
+        ),
+        csv_core=True,
+        sortable=True,
+        visualization_type='term_frequency',
+        visualization_sort='key',
+        results_overview=True
+    )
+
+    transcription = Field(
+        name='transcription',
+        es_mapping={'type': 'text'},
+        display_name='Transcription',
+        description='Text content of the inscription.',
+        search_field_core=True,
+        results_overview=True,
+        display_type='text_content'
+    )
+
+    # A string with all the names occuring in the source
+    names = Field(
+        name='names',
+        es_mapping={'type': 'text'},
+        display_name='Names',
+        description='Names of the buried persons.',
+        search_field_core=True
+    )
+
+    # Should be an array with potentially multiple values from these: 'M', 'F', or None.
+    sex = Field(
+        name='sex',
+        display_name='Sex',
+        description='Gender(s) of the buried person(s). None if the sex is unknown.',
+        es_mapping={'type': 'keyword'},
+        search_filter=MultipleChoiceFilter(
+            description='Search only within these genders.',
+            option_count=3,
+        ),
+        csv_core=True
+    )
+
+    country = Field(
+        name='country',
+        display_name='Country',
+        description='Country where the inscription was found.',
+        es_mapping={'type': 'keyword'},
+        search_filter=MultipleChoiceFilter(
+            description='Search only within these countries.',
+            option_count=4
+        ),
+        visualization_type='term_frequency',
+        results_overview=True
+    )
+
+    provenance = Field(
+        name='provenance',
+        display_name='Provenance',
+        description='Description of the location where the inscription was found.',
+        es_mapping={'type': 'keyword'},
+        search_filter=MultipleChoiceFilter(
+            description='Search only within these provenances.',
+            option_count=23
+        ),
+        visualization_type='term_frequency',
+        results_overview=True
+    )
+
+    material = Field(
+        name='material',
+        display_name='Material',
+        description='Type of material the inscription is written on.',
+        es_mapping={'type': 'keyword'},
+        search_filter=MultipleChoiceFilter(
+            description='Search only within these material types.',
+            option_count=36
+        ),
+        visualization_type='term_frequency'
+    )
+
+    material_details = Field(
+        name='material_details',
+        display_name='Material details',
+        description='Details about the material the inscription is written on.',
+        es_mapping={'type': 'text'},
+    )
+
+    language = Field(
+        name='language',
+        display_name='Language',
+        description='Language written on the inscription.',
+        es_mapping={'type': 'keyword'},
+        search_filter=MultipleChoiceFilter(
+            description='Search only within these languages.',
+            option_count=3
+        ),
+        csv_core=True,
+        visualization_type='term_frequency'
+    )
+
+    commentary = Field(
+        name='commentary',
+        es_mapping={'type': 'text'},
+        display_name='Commentary',
+        description='Extra comments, questions or remarks on this inscription.',
+        search_field_core=True,
+    )
+
+    fields = [
+        _id,
+        url,
+        year,
+        transcription,
+        names,
+        sex,
+        country,
+        provenance,
+        material,
+        material_details,
+        language,
+        commentary,
+        source_database
+    ]
+
+
+def categorize_material(text):
+    '''
+    Helper function to (significantly) reduce the material field to a set of categories.
+    Returns a list of categories, i.e. those that appear in `text`.
+    '''
+    categories = ['Sandstein', 'Kalkstein', 'Stein', 'Stone', 'Granit', 'Kunststein',
+                  'Lavatuff', 'Marmor', 'Kalk', 'Syenit', 'Labrador', 'Basalt', 'Beton',
+                  'Glas', 'Labrador', 'Rosenquarz', 'Gabbro', 'Diorit']
+    result = []
+    ltext = text.lower()
+    for c in categories:
+        if c.lower() in ltext:
+            result.append(c)
+    if len(result) == 0:
+        result.append(text)
+    return result
