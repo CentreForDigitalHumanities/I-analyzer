52f601a9f2f8db43a6bb59d624dc29ba241d4748
BeritJanssen <berit.janssen@gmail.com>
2020-07-14 16:23:12 +0200

    replacing primeng imports, cleaning up scss

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { MenuItem } from 'primeng/primeng';
+import { MenuItem } from 'primeng/api';
 import { User } from '../models/index';
 import { CorpusService } from '../services/index';
 import { ConfigService, UserService } from '../services/index';
