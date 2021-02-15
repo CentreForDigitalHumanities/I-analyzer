8431d7fed5968852a5cd5985969dee7c69979cf8
BeritJanssen <berit.janssen@gmail.com>
2021-02-15 14:20:03 +0100

    setting fiji_separate index name to alias

--- backend/corpora/peaceportal/fiji_separate.py
+++ backend/corpora/peaceportal/fiji_separate.py
@@ -3,8 +3,7 @@ from corpora.peaceportal.peaceportal import PeacePortal
 
 class FIJISEPARATE(PeacePortal):
 
-    es_index = current_app.config['PEACEPORTAL_FIJI_ES_INDEX']
-    es_alias = current_app.config['FIJI_ALIAS']
+    es_index = current_app.config['FIJI_ALIAS']
 
     # all fields listed here will be ignored if they are
     # in the PeacePortal base class definition. Ideal for excluding
