678e27cb6140f70ac566566a0c4d3bd77d589c03
BeritJanssen <berit.janssen@gmail.com>
2021-06-09 16:52:40 +0200

    extracting debates via Metadata extractor

--- backend/corpora/parliament/norway.py
+++ backend/corpora/parliament/norway.py
@@ -1,9 +1,10 @@
 from glob import glob
 from os.path import basename
+from datetime import datetime
 
 from flask import current_app
 
-from addcorpus.extract import XML, Constant
+from addcorpus.extract import Constant, Metadata
 from corpora.parliament.parliament import Parliament
 
 
@@ -28,12 +29,14 @@ class ParliamentNorway(Parliament):
 
     def sources(self, start, end):
         for txt_file in glob('{}/*.txt'.format(self.data_directory)):
-            yield txt_file, {'year': '_'.split(basename(txt_file))[1]}
+            yield txt_file, {'date': datetime(year='_'.split(basename(txt_file))[1], month=1, day=1)}
 
     def source2dicts(self, source):
-        with open(source, 'r') as f:
+        txt_file = source[0]
+        out_dict = source[1]
+        with open(txt_file, 'r') as f:
             text = f.read()
-        yield {'debate': text}
+        yield out_dict.update({'debate': text})
 
     def __init__(self):
         self.country.extractor = Constant(
@@ -42,12 +45,6 @@ class ParliamentNorway(Parliament):
 
         self.country.search_filter = None
 
-        self.date.extractor = XML(
-            tag='date',
-            attribute='format'
-        )
+        self.date.extractor = Metadata('date')
 
-        self.debate.extractor = XML(
-            tag='debates',
-            flatten=True
-        )
+        self.debate.extractor = Metadata('debate')
