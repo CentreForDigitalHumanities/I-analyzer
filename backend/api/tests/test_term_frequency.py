6812e44742ce6dd2781c4c9fdc329bd7f8d6c404
lukavdplas <l.p.vanderplas@uu.nl>
2022-03-23 17:51:16 +0100

    add tests for histogram and timeline

--- backend/api/tests/test_term_frequency.py
+++ backend/api/tests/test_term_frequency.py
@@ -1,6 +1,7 @@
 import api.analyze as analyze
 import pytest
 
+TOTAL_DOCS_IN_MOCK_CORPUS = 3
 TOTAL_WORDS_IN_MOCK_CORPUS = 67
 
 def test_extract_data_for_term_frequency(test_app):
@@ -59,7 +60,7 @@ def test_total_docs_and_tokens(test_app, test_es_client):
     query = make_query()
     highlight_specs, aggregators = analyze.extract_data_for_term_frequency('mock-corpus', ['content'])
     doc_count, token_count = analyze.get_total_docs_and_tokens(test_es_client, query, 'mock-corpus', aggregators)
-    assert doc_count == 3
+    assert doc_count == TOTAL_DOCS_IN_MOCK_CORPUS
     assert token_count == TOTAL_WORDS_IN_MOCK_CORPUS
 
 def test_term_frequency(test_app, test_es_client):
@@ -71,7 +72,7 @@ def test_term_frequency(test_app, test_es_client):
     match_count, doc_count, token_count = analyze.get_term_frequency(query, 'mock-corpus', 100)
 
     assert match_count == 2
-    assert doc_count == 3
+    assert doc_count == TOTAL_DOCS_IN_MOCK_CORPUS
     assert token_count == None
 
     ## search in content (includes token count)
@@ -79,8 +80,60 @@ def test_term_frequency(test_app, test_es_client):
     match_count, doc_count, token_count = analyze.get_term_frequency(query, 'mock-corpus', 100)
 
     assert match_count == 1
-    assert doc_count == 3
+    assert doc_count == TOTAL_DOCS_IN_MOCK_CORPUS
     assert token_count == TOTAL_WORDS_IN_MOCK_CORPUS
+
+def test_histogram_term_frequency(test_app, test_es_client):
+    cases = [
+        {
+            'genre': 'Children',
+            'matches': 2,
+            'tokens': 21
+        }, {
+            'genre': 'Romance',
+            'matches': 2,
+            'tokens': 23,
+        }, {
+            'genre': 'Science fiction',
+            'matches': 1,
+            'tokens': 23
+        }
+    ]
+
+    for case in cases:
+        query = make_query(query_text='of', search_in_fields=['content'])
+        result = analyze.get_aggregate_term_frequency(query, 'mock-corpus', 'genre', case['genre'])
+
+        assert result == {
+            'key': case['genre'],
+            'match_count': case['matches'],
+            'doc_count': 1,
+            'token_count': case['tokens']
+        }
+
+def test_timeline_term_frequency(test_app, test_es_client):
+    cases = [
+        {
+            'min_date': '1800-01-01',
+            'max_date': '1850-01-01',
+            'matches': 3,
+            'tokens': 46,
+            'doc': 2,
+        }
+    ]
+
+    for case in cases:
+        query = make_query(query_text='of', search_in_fields=['content'])
+        result = analyze.get_date_term_frequency(query, 'mock-corpus', 'date', case['min_date'], case['max_date'])
+
+        assert result == {
+            'key': case['min_date'],
+            'key_as_string': case['min_date'],
+            'match_count': case['matches'],
+            'doc_count': 2,
+            'token_count': case['tokens']
+        }
+
 
 def make_query(query_text=None, search_in_fields=None):
     query = {
