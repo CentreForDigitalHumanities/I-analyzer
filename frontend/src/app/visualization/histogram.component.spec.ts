dcb0eecba8da3feb69d7a68b76537556ba24d9f9
BeritJanssen <berit.janssen@gmail.com>
2022-04-20 10:20:30 +0200

    use commonTestBed to add dependencies to frontend tests

--- frontend/src/app/visualization/histogram.component.spec.ts
+++ frontend/src/app/visualization/histogram.component.spec.ts
@@ -1,11 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
 
-import { ApiService, ApiRetryService, ElasticSearchService, LogService, QueryService, SearchService, UserService, DialogService } from '../services/index';
-import { ApiServiceMock } from '../../mock-data/api';
-import { ElasticSearchServiceMock } from '../../mock-data/elastic-search';
-import { UserServiceMock } from '../../mock-data/user';
-import { DialogServiceMock } from '../../mock-data/dialog';
+import { commonTestBed } from '../common-test-bed';
+
 import { HistogramComponent } from './histogram.component';
 
 describe('HistogramCompoment', () => {
@@ -13,28 +9,7 @@ describe('HistogramCompoment', () => {
   let fixture: ComponentFixture<HistogramComponent>;
 
   beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-        imports: [ FormsModule ],
-        declarations: [ HistogramComponent ],
-        providers: [ 
-            {
-    
-                provide: ApiService, useValue: new ApiServiceMock()
-            },
-            ApiRetryService,
-            {
-                provide: ElasticSearchService, useValue: new ElasticSearchServiceMock()
-            },
-            LogService,
-            QueryService,
-            SearchService,
-            {
-                provide: UserService, useValue: new UserServiceMock()
-            },
-            { provide: DialogService, useClass: DialogServiceMock },
-        ],
-    })
-    .compileComponents();
+    commonTestBed().testingModule.compileComponents();
   }));
 
   beforeEach(() => {
