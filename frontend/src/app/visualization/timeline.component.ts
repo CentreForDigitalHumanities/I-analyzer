5e7ec28b22ae56c9a9c944336ec62d9344e77170
lukavdplas <l.p.vanderplas@uu.nl>
2022-04-21 10:11:10 +0200

    update comment

--- frontend/src/app/visualization/timeline.component.ts
+++ frontend/src/app/visualization/timeline.component.ts
@@ -158,7 +158,8 @@ export class TimelineComponent extends BarChartComponent<DateResult> implements 
         // zoom limits
         options.plugins.zoom.limits = {
             xAxis: {
-                // use numeric values as zoom plugin does not accept string
+                // convert dates to numeric rather than string here,
+                // as zoom plugin does not accept strings
                 min: xMin.valueOf(),
                 max: xMax.valueOf(),
             }
