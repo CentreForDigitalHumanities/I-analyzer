84eed086d1fceba0f8593c1d59e78868a6d9c226
Meesch <meesstip@gmail.com>
2022-04-28 15:07:16 +0200

    add uk corpus test

--- backend/corpora/parliament/tests/test_uk.py
+++ backend/corpora/parliament/tests/test_uk.py
@@ -5,31 +5,28 @@ from datetime import datetime
 target_docs = [
     {
         'country': 'United Kingdom',
-        'date': '1939-07-26',
+        'date': '1872-02-06',
         'house': 'House of Commons',
-        'debate_title': 'Motor Passenger Vehicles',
-        'debate_id': '311003',
-        'speech': "1. asked the Minister of Transport whether, especially in view of the recent omnibus accident near Whitby, in which three Hull persons were killed and 37 injured, he will inquire into the inspection of the brakes and mechanical efficiency generally when this class of vehicle is licensed to carry passengers.",
-        'id': '1839045',
-        'speaker': 'Lieut.-Commander Kenworthy',
-        'column': '1561',
+        'debate_title': 'NEW WRITS DURING THE RECESS.',
+        'speech': "acquainted the House, —that he had issued Warrants for New Writs, for Truro, v. Hon. John Cranch Walker Vivian, Under Secretary to the Eight hon. Edward Cardwell; for Plymouth, Sir Robert Porrett Collier, knight, one of the Justices of the Court of Common Pleas; Dover, George Jessel, esquire, Solicitor General; York County (West Riding, Northern Division), Sir Francis Crossley, baronet, deceased; Limerick City, Francis William Russell, esquire, deceased; Galway County, Eight hon. William Henry Gregory, Governor and Commander in Chief of the Island of Ceylon and its dependencies; Kerry, Eight hon. Valentine Augustus Browne, commonly called Viscount Castlerosse, now Earl of Kenmare.",
+        'id': 'guldi_c19_365565',
+        'speaker': 'Mr. SPEAKER',
+        'sequence': '365565'
     },
     {
         'country': 'United Kingdom',
-        'date': '1939-07-26',
+        'date': '2020-01-14',
         'house': 'House of Commons',
-        'debate_title': 'Motor Passenger Vehicles',
-        'debate_id': '311003',
-        'speech': ' '.join([
-            'I have been asked to reply.',
-            'Two years ago a circular was sent by the Ministry of Transport to licensing authorities setting out the constructional requirements for motor omnibus and motor coaches recommended by a Departmental Committee which had considered the subject.',
-            'These recommendations have it is believed been generally observed by manufacturers in the construction of new vehicles, and have been imposed by many licensing authorities as conditions precedent to the issue of licences to ply for hire.',
-            'My hon.',
-            'Friend has at present no power either to insist on the adoption of these requirements by licensing authorities or to control their methods of inspecting vehicles presented for licensing.',
-            ]),
-        'id': '1839046',
-        'speaker': 'Mr. Whiteley (Lord Of The Treasury)',
-        'column': '1561',
+        'debate_title': 'House of Commons debate on 14/01/2020',
+        'debate_id': 'debates2020-01-14c',
+        'speech': "What steps his Department is taking to ensure that legal aid is accessible to people who need it.",
+        'id': 'uk.org.publicwhip/debate/2020-01-14c.865.4',
+        'speaker': 'Sarah Dines',
+        'speaker_id': 'uk.org.publicwhip/person/25877',
+        'speech_type': 'Start Question',
+        'topic': 'The Secretary of State was asked—',
+        'subtopic': 'Legal Aid Access',
+        'sequence': '0'
     }
 ]
 
