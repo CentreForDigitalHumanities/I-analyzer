5b300fe28d85d0db2d5d7f642f308d7ea40e0c15
Meesch <meesstip@gmail.com>
2022-04-29 15:12:56 +0200

    updated test - not final

--- backend/api/tests/test_wordcloud.py
+++ backend/api/tests/test_wordcloud.py
@@ -7,28 +7,81 @@ def test_wordcloud():
     # simplified version of elasticsearch output
     documents = [
         { '_source': {
-            'content': 'I-analyzer is great!'
+            'content': 'You will rejoice to hear that no disaster has accompanied the commencement of an enterprise which you have regarded with such evil forebodings.'
         }, '_id' : 'id1'},
         { '_source': {
-            'content': 'I love to analyze in I-analyzer.'
+            'content': 'It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.'
         }, '_id' : 'id2'},
         { '_source': {
-            'content': 'I love I-analyzer.'
-        }, '_id' : 'id3'},
-        { '_source': {
-            'content': 'I could analyze all day.'
-        }, '_id' : 'id4' }
+            'content': 'Alice in Wonderland","Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do.'
+        }, '_id' : 'id3'}
     ]
 
     target_unfiltered = [
-        { 'key':  'I', 'doc_count': 4 },
-        { 'key': 'analyzer', 'doc_count': 3 },
+        { 'key': 'of', 'doc_count': 5 },
+        { 'key': 'a', 'doc_count': 4 },
+        { 'key': 'to', 'doc_count': 3 },
+        { 'key': 'you', 'doc_count': 2 },
+        { 'key': 'the', 'doc_count': 2 },
+        { 'key': 'that', 'doc_count': 2 },
+        { 'key': 'in', 'doc_count': 2 },
+        { 'key': 'with', 'doc_count': 1 },
+        { 'key': 'will', 'doc_count': 1 },
+        { 'key': 'wife', 'doc_count': 1 },
+        { 'key': 'which', 'doc_count': 1 },
+        { 'key': 'was', 'doc_count': 1 },
+        { 'key': 'want', 'doc_count': 1 },
+        { 'key': 'very', 'doc_count': 1 },
+        { 'key': 'universally', 'doc_count': 1 },
+        { 'key': 'truth', 'doc_count': 1 },
+        { 'key': 'tired', 'doc_count': 1 },
+        { 'key': 'such', 'doc_count': 1 },
+        { 'key': 'sitting', 'doc_count': 1 },
+        { 'key': 'sister', 'doc_count': 1 },
+        { 'key': 'single', 'doc_count': 1 },
+        { 'key': 'rejoice', 'doc_count': 1 },
+        { 'key': 'regarded', 'doc_count': 1 },
+        { 'key': 'possession', 'doc_count': 1 },
+        { 'key': 'on', 'doc_count': 1 },
+        { 'key': 'nothing', 'doc_count': 1 },
+        { 'key': 'no', 'doc_count': 1 },
+        { 'key': 'must', 'doc_count': 1 },
+        { 'key': 'man', 'doc_count': 1 },
+        { 'key': 'it', 'doc_count': 1 },
         { 'key': 'is', 'doc_count': 1 },
-        { 'key': 'great', 'doc_count': 1 },
-        { 'key': 'love', 'doc_count': 2 },
-        { 'key': 'to', 'doc_count': 1 },
-        { 'key': 'analyze', 'doc_count': 2 }
+        { 'key': 'her', 'doc_count': 1 },
+        { 'key': 'hear', 'doc_count': 1 },
+        { 'key': 'having', 'doc_count': 1 },
+        { 'key': 'have', 'doc_count': 1 },
+        { 'key': 'has', 'doc_count': 1 },
+        { 'key': 'good', 'doc_count': 1 },
+        { 'key': 'get', 'doc_count': 1 },
+        { 'key': 'fortune', 'doc_count': 1 },
+        { 'key': 'forebodings', 'doc_count': 1 },
+        { 'key': 'evil', 'doc_count': 1 },
+        { 'key': 'enterprise', 'doc_count': 1 },
+        { 'key': 'do', 'doc_count': 1 },
+        { 'key': 'disaster', 'doc_count': 1 },
+        { 'key': 'commencemen', 'doc_count': 1 }, 
+        { 'key': 'by', 'doc_count': 1 },
+        { 'key': 'beginning', 'doc_count': 1 },
+        { 'key': 'be', 'doc_count': 1 },
+        { 'key': 'bank', 'doc_count': 1 },
+        { 'key': 'and', 'doc_count': 1 },
+        { 'key': 'an', 'doc_count': 1 },
+        { 'key': 'alice', 'doc_count': 1 },
+        { 'key': 'acknowledge', 'doc_count': 1 }, 
+        { 'key': 'accompanied', 'doc_count': 1 }
     ]
+    # target_unfiltered = [
+    #     { 'key':  'I', 'doc_count': 4 },
+    #     { 'key': 'analyzer', 'doc_count': 3 },
+    #     { 'key': 'is', 'doc_count': 1 },
+    #     { 'key': 'great', 'doc_count': 1 },
+    #     { 'key': 'love', 'doc_count': 2 },
+    #     { 'key': 'to', 'doc_count': 1 },
+    #     { 'key': 'analyze', 'doc_count': 2 }
+    # ]
 
     output = analyze.make_wordcloud_data(documents, 'content', 'mock-corpus')
 
