777f876f89cd0af7d4a912f78085f3359a3f64a9
lukavdplas <l.p.vanderplas@uu.nl>
2022-06-16 17:17:32 +0200

    remove length multifield for keyword fields

--- backend/corpora/parliament/utils/constants.py
+++ backend/corpora/parliament/utils/constants.py
@@ -4,13 +4,7 @@ MIN_DATE = datetime(year=1800, month=1, day=1)
 MAX_DATE = datetime(year=2021, month=12, day=31)
 
 BASIC_KEYWORD_MAPPING = {
-    'type': 'keyword',
-    'fields': {
-        'length': {
-            'type': 'token_count',
-            'analyzer': 'standard'
-        }
-    }
+    'type': 'keyword'
 }
 
 BASIC_TEXT_MAPPING = {
