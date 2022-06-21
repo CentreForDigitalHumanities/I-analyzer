287addb14ee91a8002efbc02265076227230f765
lukavdplas <l.p.vanderplas@uu.nl>
2022-06-21 14:27:46 +0200

    remove obsolete import

--- backend/api/tests/test_query.py
+++ backend/api/tests/test_query.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 import api.query as query
 from es.search import search, hits
-import pytest
-from addcorpus.load_corpus import load_corpus
 
 def test_date_manipulation(basic_query):
     assert query.get_filters(basic_query) == []
