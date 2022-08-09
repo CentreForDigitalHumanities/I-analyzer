d0d4ae711ae085d2ed76537fdc06f8dc9dc18eb6
lukavdplas <l.p.vanderplas@uu.nl>
2022-08-09 11:44:10 +0200

    update sweden corpus

--- backend/corpora/parliament/sweden-old.py
+++ backend/corpora/parliament/sweden-old.py
@@ -41,18 +41,6 @@ class ParliamentSwedenOld(Parliament, CSVCorpus):
     data_directory = current_app.config['PP_SWEDEN_OLD_DATA']
     es_index = current_app.config['PP_SWEDEN_OLD_INDEX']
 
-    # TODO: remove es_settings after #771)
-    es_settings = current_app.config['PP_ES_SETTINGS']
-    es_settings['analysis']['filter'] = {
-        "stopwords": {
-          "type": "stop",
-          "stopwords": "_swedish_"
-        },
-        "stemmer": {
-            "type": "stemmer",
-            "language": "swedish"
-        }
-    }
 
     document_context = constants.document_context()
     document_context['context_fields'] = ['chamber', 'date_earliest', 'date_latest']
