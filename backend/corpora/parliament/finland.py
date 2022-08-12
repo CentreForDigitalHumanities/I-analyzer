89ba1cfdc971d2c849e97a2f8da65648e63999ec
lukavdplas <lukavdplas@gmail.com>
2022-08-12 13:17:47 +0200

    add more fields

--- backend/corpora/parliament/finland.py
+++ backend/corpora/parliament/finland.py
@@ -15,26 +15,26 @@ def extract_party_data(node):
     return {
         'name': node['xml:id'],
         'role': node['role'],
-        'id': '#party.' + node['xml:id']
+        'id': node['xml:id']
     }
 
-def get_all_party_data(soup):
+def extract_all_party_data(soup):
     parties_list = soup.find('listOrg')
     party_data = map(extract_party_data, parties_list.find_all('org'))
     return {
-        party['id']: party for party in party_data
+        '#party.' + party['id']: party for party in party_data
     }
 
-def extract_person_data(person):
-    id = person['xml:id']
-    surname = person.persName.surname.text.strip()
-    forename = person.persName.forename.text.strip()
+def extract_person_data(node):
+    id = node['xml:id']
+    surname = node.persName.surname.text.strip()
+    forename = node.persName.forename.text.strip()
     name = ' '.join([forename, surname])
-    role = person.persName.roleName.text.strip() if person.persName.roleName else None
-    gender = person.sex.text.strip() if person.sex else None
-    party_id = person.affliation['ref'] if person.affliation else None
+    role = node.persName.roleName.text.strip() if node.persName.roleName else None
+    gender = node.sex.text.strip() if node.sex else None
+    party_id = node.affliation['ref'] if node.affliation else None
     party = party_id.split('.', maxsplit=1)[1] if party_id else None
-    birth_date = person.birth['when'] if person.birth else None
+    birth_date = node.birth['when'] if node.birth else None
     birth_year = int(birth_date[:4]) if birth_date else None
 
     return {
@@ -47,15 +47,28 @@ def extract_person_data(person):
         'birth_year': birth_year,
     }
 
-def get_people_data(soup):
-    person_tags = soup.find_all('person')
-    person_data = map(extract_person_data, person_tags)
+def extract_people_data(soup):
+    person_nodes = soup.find_all('person')
+    person_data = map(extract_person_data, person_nodes)
     return {
         '#' + person['id']: person for person in person_data
     }
 
-def person_attribute_transform_func(attribute):
-    get_attribute = lambda who, people : people[who][attribute]
+def extract_role_data(soup):
+    role_nodes = soup.find('encodingDesc').find_all('category')
+
+    # return dict that maps IDs to terms
+    # data contains duplicate role IDs
+    # go through data in reverse order
+    # so earlier (more general) terms overwrite later (more specific) ones
+
+    return {
+        node['xml:id']: node.find('term').text.strip()
+        for node in reversed(role_nodes)
+    }
+
+def metadata_attribute_transform_func(attribute):
+    get_attribute = lambda which, collection : collection[which][attribute]
     return lambda values: get_attribute(*values)
 
 def person_attribute_extractor(attribute):
@@ -64,13 +77,42 @@ def person_attribute_extractor(attribute):
     return Combined(
         XML(attribute='who'),
         Metadata('persons'),
-        transform = person_attribute_transform_func(attribute),
+        transform = metadata_attribute_transform_func(attribute),
     )
 
-def speech_metadata(speech):
-    """Gets the `note` sibling to the speech."""
+def party_attribute_extractor(attribute):
+    """Extractor that finds the speaker's party and party's
+    attributes defined in extract_party_data()'"""
 
-    return speech.parent.find('note')
+    return Combined(
+        person_attribute_extractor('party_id'),
+        Metadata('parties'),
+        transform = metadata_attribute_transform_func(attribute),
+    )
+
+def format_role(values):
+    id, roles = values
+    if id:
+        clean_id = id.replace('#', '')
+        return roles[clean_id]
+
+def speech_metadata(speech_node):
+    """Gets the `note` sibling to the speech."""
+    return speech_node.find_previous_sibling('note')
+
+def find_topic(speech_node):
+    return speech_node.parent.find_previous_sibling('head')
+
+def find_debate_node(speech_node):
+    return speech_node.find_parent('TEI')
+
+def find_debate_title(speech_node):
+    debate_node = find_debate_node(speech_node)
+    return debate_node.teiHeader.find('title')
+
+def find_date(speech_node):
+    debate_node = find_debate_node(speech_node)
+    return debate_node.teiHeader.find('date')
 
 class ParliamentFinland(Parliament, XMLCorpus):
     title = 'People and Parliament (Finland)'
@@ -84,10 +126,17 @@ class ParliamentFinland(Parliament, XMLCorpus):
 
             with open(xml_file) as infile:
                 soup = BeautifulSoup(infile, features = 'xml')
-            party_data = get_all_party_data(soup)
-            person_data = get_people_data(soup)
+            role_data = extract_role_data(soup)
+            party_data = extract_all_party_data(soup)
+            person_data = extract_people_data(soup)
 
-            yield xml_file, {'parties': party_data, 'persons': person_data}
+            metadata = {
+                'roles': role_data,
+                'parties': party_data,
+                'persons': person_data
+            }
+
+            yield xml_file, metadata
 
     language = 'finnish'
     image = 'finland.jpg'
@@ -98,14 +147,39 @@ class ParliamentFinland(Parliament, XMLCorpus):
     country = field_defaults.country()
     country.extractor = Constant('Finland')
 
+    date = field_defaults.date()
+    date.extractor = XML(
+        transform_soup_func = find_date,
+        attribute = 'when'
+    )
+
+    debate_id = field_defaults.debate_id()
+    debate_id.extractor = XML(
+        transform_soup_func = find_debate_node,
+        attribute = 'xml:id'
+    )
+
+    debate_title = field_defaults.debate_title()
+    debate_title.extractor = XML(
+        transform_soup_func = find_debate_title,
+        transform = str.strip,
+    )
+
     party = field_defaults.party()
-    party.extractor = person_attribute_extractor('party')
+    party.extractor = party_attribute_extractor('name')
 
     party_id = field_defaults.party_id()
-    party_id.extractor = person_attribute_extractor('party_id')
+    party_id.extractor = party_attribute_extractor('id')
+
+    party_role = field_defaults.party_role()
+    party_role.extractor = party_attribute_extractor('role')
 
     role = field_defaults.parliamentary_role()
-    role.extractor = person_attribute_extractor('role')
+    role.extractor = Combined(
+        XML(attribute = 'ana'),
+        Metadata('roles'),
+        transform = format_role,
+    )
 
     speaker = field_defaults.speaker()
     speaker.extractor = person_attribute_extractor('name')
@@ -122,10 +196,21 @@ class ParliamentFinland(Parliament, XMLCorpus):
     speech = field_defaults.speech()
     speech.extractor = XML(transform = str.strip)
 
+    speech_id = field_defaults.speech_id()
+    speech_id.extractor = XML(
+        attribute = 'xml:id'
+    )
+
     speech_type = field_defaults.speech_type()
     speech_type.extractor = XML(
         transform_soup_func = speech_metadata,
         attribute = 'speechType'
+    )
+
+    topic = field_defaults.topic()
+    topic.extractor = XML(
+        transform_soup_func = find_topic,
+        transform = str.strip,
     )
 
     url = field_defaults.url()
@@ -134,14 +219,24 @@ class ParliamentFinland(Parliament, XMLCorpus):
         attribute = 'link'
     )
 
+    sequence = field_defaults.sequence()
+    sequence.extractor = XML(
+        attribute = 'xml:id',
+        transform = lambda value: value.split('_')[-1]
+    )
+
     def __init__(self):
         self.fields = [
             self.country,
-            self.party_id, self.party,
+            self.date,
+            self.debate_id, self.debate_title,
+            self.party, self.party_id, self.party_role,
             self.role,
             self.speaker,
             self.speaker_id, self.speaker_gender, self.speaker_birth_year,
             self.speech,
-            self.speech_type,
+            self.speech_id, self.speech_type,
+            self.topic,
             self.url,
+            self.sequence,
         ]
