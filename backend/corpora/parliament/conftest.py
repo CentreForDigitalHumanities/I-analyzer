eff9d984c67ee7793fae4feb35de0dfe972bedc8
lukavdplas <lukavdplas@gmail.com>
2022-08-18 10:46:34 +0200

    fix test config

--- backend/corpora/parliament/conftest.py
+++ backend/corpora/parliament/conftest.py
@@ -63,6 +63,12 @@ class UnittestConfig:
     PP_SWEDEN_OLD_INDEX = 'parliament-sweden-old'
     PP_SWEDEN_OLD_DATA = os.path.join(here, 'tests', 'data', 'sweden-old')
     PP_SWEDEN_OLD_IMAGE = 'sweden-old.jpg'
+    PP_FINLAND_INDEX = 'parliament-finland'
+    PP_FINLAND_DATA = os.path.join(here, 'tests', 'data', 'finland')
+    PP_NORWAY_INDEX = 'parliament-norway'
+    PP_NORWAY_DATA = os.path.join(here, 'tests', 'data', 'norway')
+    PP_NORWAY_NEW_INDEX = 'parliament-norway-new'
+    PP_NORWAY_NEW_DATA = os.path.join(here, 'tests', 'data', 'norway-new')
 
     SAML_FOLDER = "saml"
     SAML_SOLISID_KEY = "uuShortID"
