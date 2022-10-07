4c5ad08d9c57058ce87c283019c8237a95ea149b
BeritJanssen <berit.janssen@gmail.com>
2022-10-07 11:24:58 +0200

    set dummy variables to production environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
     production: true,
     appName: 'Something completely different',
+    aboutPage: 'people-parliament.md',
+    apiUrl: '/api',
+    adminUrl: '/admin',
+    wordModelsUrl: '/wordmodels/'
 };
