e6745cb837e69f1bd6368679974b492ffc53ad9f
Sheean Spoel <s.j.j.spoel@uu.nl>
2022-10-07 11:37:50 +0200

    Stub for configuring different variants

---
+++ frontend/src/environments/variants/people-and-parliament.ts
@@ -0,0 +1,7 @@
+export const peopleAndParliament = {
+    appName: 'People & Parliament',
+    aboutPage: 'people-and-parliament',
+    apiUrl: '/api',
+    adminUrl: '/admin',
+    wordModelsUrl: '/wordmodels/'
+};
