bf63dbb7501623965881031a2c316a8b5f50389e
BeritJanssen <berit.janssen@gmail.com>
2022-10-07 11:33:45 +0200

    fix: add isLoading to AboutComponent

--- frontend/src/app/about/about.component.ts
+++ frontend/src/app/about/about.component.ts
@@ -11,16 +11,19 @@ import { DialogService } from '../services';
 export class AboutComponent implements OnInit {
     public appName: string;
     public aboutHtml: SafeHtml;
+    public isLoading = false;
 
     constructor(private dialogService: DialogService) { }
 
     ngOnInit() {
+        this.isLoading = true;
         this.appName = environment.appName;
         this.fetchData();
     }
 
     async fetchData() {
         this.aboutHtml = await this.dialogService.getAboutPage(environment.aboutPage);
+        this.isLoading = false;
     }
 
 }
