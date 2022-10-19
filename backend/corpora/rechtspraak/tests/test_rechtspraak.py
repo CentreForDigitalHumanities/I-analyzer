88c6b318b33fdad8fe65278b6b3ceb158a0c774c
Jelte van Boheemen <j.vanboheemen@uu.nl>
2022-10-19 11:13:38 +0200

    Remove unpack test

--- backend/corpora/rechtspraak/tests/test_rechtspraak.py
+++ backend/corpora/rechtspraak/tests/test_rechtspraak.py
@@ -1,9 +1,5 @@
 from operator import itemgetter
 
-def test_rechtspraak_unpack(test_corpus):
-    assert test_corpus
-    test_corpus.unpack(how='sample', per_year=1)
-
 
 def test_rechtspraak_sources(test_corpus):
     s = test_corpus.sources()
