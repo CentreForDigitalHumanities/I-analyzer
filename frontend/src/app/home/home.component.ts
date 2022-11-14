39a36a13509d81f53988272453768558faf66e31
lukavdplas <lukavdplas@gmail.com>
2022-11-14 14:52:38 +0100

    show loading spinner for corpora

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -11,11 +11,21 @@ import { CorpusService } from '../services/index';
 export class HomeComponent implements OnInit {
     public items: Corpus[];
 
+    isLoading = false;
+
     constructor(private corpusService: CorpusService, private title: Title) {
         this.title.setTitle('Home');
     }
 
     ngOnInit() {
-        this.corpusService.get(true).then((items) => this.items = items);
+        this.showLoading(
+            this.corpusService.get(true)
+            .then((items) => this.items = items)
+        );
+    }
+
+    showLoading(promise: Promise<any>) {
+        this.isLoading = true;
+        return promise.then(result => {this.isLoading = false; return result; });
     }
 }
