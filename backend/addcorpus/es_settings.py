da70a507218009e54273b54eb85154e7873a27be
lukavdplas <lukavdplas@gmail.com>
2022-11-30 17:15:56 +0100

    more generic es_settings function

--- backend/addcorpus/es_settings.py
+++ backend/addcorpus/es_settings.py
@@ -42,19 +42,67 @@ def get_nltk_stopwords(language):
     else:
         raise NotImplementedError('language {} has no nltk stopwords list'.format(language))
 
-def parliament_es_settings(language):
-    stopwords = get_nltk_stopwords(language)
 
-    settings = SETTINGS
-    settings['analysis']['filter'] = {
-        "stopwords": {
-            "type": "stop",
-            "stopwords": stopwords
-        },
-        "stemmer": {
-            "type": "stemmer",
-            "language": language
-        }
+def es_settings(language = None, stopword_analyzer = False, stemming_analyzer = False):
+    '''
+    Make elasticsearch settings json for a corpus index. Options:
+    - `language`: string with the language of the corpus. Must be specified if you want to use stopword or stemming analysers.
+    - `stopword_analyzer`: define an analyser that removes stopwords.
+    - `stemming_analyzer`: define an analyser that removes stopwords and performs stemming.
+    '''
+
+    settings = {
+        'index': {'number_of_replicas': 0}
     }
 
+    if stopword_analyzer or stemming_analyzer:
+        settings["analysis"] = {
+            "analyzer": {},
+            "char_filter":{ "number_filter": number_filter() },
+            'filter': {
+                "stopwords": make_stopword_filter(language)
+            }
+        }
+
+        if stopword_analyzer:
+            settings["analysis"]['analyzer']['clean'] = make_stopword_analyzer()
+
+        if stemming_analyzer:
+            settings['analysis']['filter']['stemmer'] = make_stemmer_filter(language)
+            settings["analysis"]['analyzer']['stemmed'] = make_stemmed_analyzer()
+
     return settings
+
+def number_filter():
+    return {
+        "type":"pattern_replace",
+        "pattern":"\\d+",
+        "replacement":""
+    }
+
+def make_stopword_filter(language):
+    stopwords = get_nltk_stopwords(language)
+    return {
+        "type": "stop",
+        "stopwords": stopwords
+    }
+
+def make_stopword_analyzer():
+    return {
+        "tokenizer": "standard",
+        "char_filter": ["number_filter"],
+        "filter": ["lowercase", "stopwords"]
+    }
+
+def make_stemmer_filter(language):
+    return {
+        "type": "stemmer",
+        "language": language
+    }
+
+def make_stemmed_analyzer():
+    return {
+        "tokenizer": "standard",
+        "char_filter": ["number_filter"],
+        "filter": ["lowercase", "stopwords", "stemmer"]
+    }
