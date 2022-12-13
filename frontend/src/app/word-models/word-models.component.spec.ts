f36a60f781fe984e1dba07ebe676d9bddbe214c5
lukavdplas <lukavdplas@gmail.com>
2022-12-13 11:41:44 +0100

    updat word model spec

--- frontend/src/app/word-models/word-models.component.spec.ts
+++ frontend/src/app/word-models/word-models.component.spec.ts
@@ -17,7 +17,7 @@ describe('WordModelsComponent', () => {
         fixture.detectChanges();
     });
 
-    // it('should create', () => {
-    //     expect(component).toBeTruthy();
-    // });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
