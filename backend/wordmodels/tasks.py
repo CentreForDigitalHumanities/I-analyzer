a59e9d5a6120fae73505b14faf9b642ff75617c3
lukavdplas <lukavdplas@gmail.com>
2022-12-15 14:01:04 +0100

    remove stray print statement

--- backend/wordmodels/tasks.py
+++ backend/wordmodels/tasks.py
@@ -3,5 +3,4 @@ from ianalyzer import celery_app
 
 @celery_app.task()
 def get_2d_context_results(terms, corpus, neighbours):
-    print(terms, corpus, neighbours)
     return visualisations.get_2d_contexts_over_time(terms, corpus, neighbours)
