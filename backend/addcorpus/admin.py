1c77a25e33c7dc28ee3945ed0311d6ba74e252c9
lukavdplas <lukavdplas@gmail.com>
2023-01-11 15:39:18 +0100

    add corpus model

--- backend/addcorpus/admin.py
+++ backend/addcorpus/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Corpus
 
-# Register your models here.
+admin.site.register(Corpus)
--- backend/addcorpus/models.py
+++ backend/addcorpus/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import CustomUser
+from django.contrib.auth.models import Group
+from django.conf import settings
 
-# Create your models here.
+class Corpus(models.Model):
+    name = models.CharField(max_length=settings.MAX_LENGTH_NAME, unique=True)
+    description = models.CharField(max_length=settings.MAX_LENGTH_DESCRIPTION, null=True)
+    groups = models.ManyToManyField(Group, related_name='corpora', blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Corpora'
+
+    def __str__(self):
+        return self.name
