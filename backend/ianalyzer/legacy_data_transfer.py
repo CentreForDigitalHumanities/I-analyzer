a2278babfebd0b4f10f3ee9afee51808a3d32155
lukavdplas <lukavdplas@gmail.com>
2023-01-13 12:03:02 +0100

    move data migration files

--- backend/ianalyzer/legacy_data_transfer.py
+++ backend/ianalyzer/legacy_data_transfer.py
@@ -1,13 +1,11 @@
 import csv
 import os
-import django.contrib.auth.hashers as hashers
 import base64
 from django.contrib.auth.models import Group
 from users.models import CustomUser
 from django.db import connection
 
 _here = os.path.abspath(os.path.dirname(__file__))
-legacy_test_data_dir = os.path.join(_here, 'legacy_test_data')
 
 def adapt_password_encoding(flask_encoded):
     '''Adapt encoded password hash from flask to django format'''
@@ -89,63 +87,3 @@ def import_and_save_users(directory):
     for row in data:
         save_legacy_user(row)
 
-
-def test_legacy_data_import():
-    user_data = import_table_data(legacy_test_data_dir, 'user')
-
-    assert len(user_data) == 1
-
-    user = user_data[0]
-    expected_user =  {
-        'id': '1',
-        'username': 'admin',
-        'password': 'password',
-        'email': 'admin@ianalyzer.nl',
-        'active': '1',
-        'authenticated': '1',
-        'download_limit': '10000',
-        'role_id': '2',
-        'saml': '0'
-    }
-
-    for key in expected_user:
-        if key == 'password':
-            encoded = adapt_password_encoding(user['password'])
-            assert hashers.check_password(expected_user['password'], encoded)
-        else:
-            assert user[key] == expected_user[key]
-
-def test_roles_import():
-    role_data = import_table_data(legacy_test_data_dir, 'role')
-
-    assert len(role_data) == 2
-
-    role = role_data[0]
-    expected_role = {
-        'id': '1',
-        'name': 'basic',
-        'description': 'corpora for public access'
-    }
-
-    assert role == expected_role
-
-def test_save_groups(db):
-    import_and_save_groups(legacy_test_data_dir)
-
-    groups = Group.objects.all()
-    assert len(groups) == 2
-
-def test_save_legacy_user(db):
-    import_and_save_groups(legacy_test_data_dir)
-    import_and_save_users(legacy_test_data_dir)
-
-    users = CustomUser.objects.all()
-
-    assert len(users) == 1
-
-    user = users[0]
-    assert user.username == 'admin'
-    assert user.email == 'admin@ianalyzer.nl'
-    print(user.groups)
-    print(Group.objects.filter(name='admin'))
-    assert user.groups == Group.objects.filter(name='admin')
