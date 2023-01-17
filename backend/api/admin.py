9e50879c5629f5392726fe156dcf3ea1a460ec6f
lukavdplas <lukavdplas@gmail.com>
2023-01-17 11:38:34 +0100

    make some fields readonly in admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Query, Download
 
-admin.site.register(Query)
-admin.site.register(Download)
+class QueryAdmin(admin.ModelAdmin):
+    readonly_fields = ['query_json']
+
+admin.site.register(Query, QueryAdmin)
+
+class DownloadAdmin(admin.ModelAdmin):
+    readonly_fields = ['parameters']
+
+admin.site.register(Download, DownloadAdmin)
