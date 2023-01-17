5fabcf643575422f555c943cb8d89bbeb4dd3ba4
lukavdplas <lukavdplas@gmail.com>
2023-01-17 15:57:19 +0100

    fix load_corpus unit tests

--- backend/addcorpus/tests/test_corpusimport.py
+++ backend/addcorpus/tests/test_corpusimport.py
@@ -1,40 +1,50 @@
-from os import mkdir, rmdir
-from os.path import abspath
-from shutil import copy
-
+import os
 import pytest
-
 from addcorpus import load_corpus
-from django.conf import settings
 
-def test_key_error(session, monkeypatch):
+def test_key_error(db, settings):
     ''' Verify that exception is correctly raised
     - in case the config.CORPORA variable is empty
     '''
+
+    settings.CORPORA = {}
+    settings.CORPUS_DEFINITIONS = {}
+
     with pytest.raises(KeyError) as e:
-        monkeypatch.setitem(settings, 'CORPORA', {})
         load_corpus.load_all_corpora()
         settings.CORPUS_DEFINITIONS['times']
 
-
-def test_import_error(test_app, monkeypatch):
+def test_import_error(db, settings):
     ''' Verify that exceptions is correctly raised
     - in case the file path in config.CORPORA is faulty
     '''
+
+    settings.CORPORA = {'times': '/somewhere/times/times.py'}
+    settings.CORPUS_DEFINITIONS = {}
+
     with pytest.raises(FileNotFoundError) as e:
-        monkeypatch.setitem(
-            test_app.config, 'CORPORA', {'times': '/somewhere/times/times.py'}
-            )
         load_corpus.load_corpus('times')
 
 
-def test_import_from_anywhere(session, db, test_app, admin_role, monkeypatch, tmpdir):
+mock_corpus_definition = '''
+class Times():
+    title = "Times"
+    description = "Newspaper archive, 1785-2010"
+    fields = []
+'''
+
+def test_import_from_anywhere(db, settings, tmpdir, admin_group):
     ''' Verify that the corpus definition
     can live anywhere in the file system
     '''
     testdir = tmpdir.mkdir('/testdir')
-    copy(str(abspath('corpora/times/times.py')), str(testdir))
+
+    with open(os.path.join(testdir, 'times.py'), 'w') as f:
+        f.write(mock_corpus_definition)
     path_testfile = str(testdir)+'/times.py'
-    monkeypatch.setitem(settings, 'CORPORA', {'times': path_testfile})
+
+    settings.CORPORA = {'times': path_testfile}
+    settings.CORPUS_DEFINITIONS = {}
+
     load_corpus.load_all_corpora()
     assert 'times' in settings.CORPUS_DEFINITIONS
