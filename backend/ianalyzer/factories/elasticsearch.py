4bbe07c54b58b0549e770b41a21923bcae76eb2d
BeritJanssen <berit.janssen@gmail.com>
2023-01-19 14:22:23 +0100

    REVERT: ES client timeout=1

--- backend/ianalyzer/factories/elasticsearch.py
+++ backend/ianalyzer/factories/elasticsearch.py
@@ -15,7 +15,7 @@ def elasticsearch(corpus_name):
             'scheme': 'http'
     }
     kwargs = {
-        'request_timeout': 30, 'max_retries': 10, 'retry_on_timeout': True,
+       'max_retries': 10, 'retry_on_timeout': True, 'timeout': 1
     }
     if server_config.get('certs_location') and server_config.get('api_key'):
         # settings to connect via SSL are present
