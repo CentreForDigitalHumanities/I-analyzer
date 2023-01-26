97635dd77e956ea9e209d2a82328b097adb7a333
lukavdplas <lukavdplas@gmail.com>
2023-01-26 13:40:25 +0100

    update deprecated get_feature_names

--- backend/visualization/wordcloud.py
+++ backend/visualization/wordcloud.py
@@ -18,7 +18,7 @@ def make_wordcloud_data(documents, field, corpus):
     cv = CountVectorizer(max_features=100, max_df=0.7, token_pattern=r'(?u)\b[^0-9\s]{3,30}\b', stop_words=nltk_stopwords)
     cvtexts = cv.fit_transform(texts)
     counts = cvtexts.sum(axis=0).A1
-    words = list(cv.get_feature_names())
+    words = list(cv.get_feature_names_out())
     freq_distribution = Counter(dict(zip(words, counts)))
     output = [{'key': word, 'doc_count': int(freq_distribution[word])} for word in words]
     return output
