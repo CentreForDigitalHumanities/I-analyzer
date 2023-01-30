f0e6cf9819b8d4bec513614c889608d2bd0869b0
lukavdplas <lukavdplas@gmail.com>
2023-01-30 13:47:23 +0100

    more generic corpus permission

--- backend/addcorpus/permissions.py
+++ backend/addcorpus/permissions.py
@@ -2,14 +2,36 @@ from rest_framework import permissions
 from addcorpus.load_corpus import load_corpus
 from rest_framework.exceptions import NotFound
 
+def corpus_name_from_request(request):
+        '''
+        Extract the corpus name from a request
+        '''
+
+        # corpus name can be in kwargs, query params or data json...
+        kwargs = request.parser_context['kwargs']
+        query_params = request.query_params
+        data_json = request.data
+
+        # ... stored as 'corpus' or 'corpus_name'
+        possible_corpus_specifications = (
+            data.get('corpus', None) or data.get('corpus_name', None)
+            for data in [kwargs, query_params, data_json]
+        )
+
+        # take the first value that is not None
+        corpus = next(
+            (corpus_name for corpus_name in possible_corpus_specifications if corpus_name),
+            None
+        )
+
+        return corpus
+
 class CorpusAccessPermission(permissions.BasePermission):
     message = 'You do not have permission to access this corpus'
 
     def has_permission(self, request, view):
         user = request.user
-        kwargs = request.parser_context['kwargs']
-        data = request.data
-        corpus = kwargs.get('corpus', None) or data.get('corpus', None) or data.get('corpus_name', None)
+        corpus = corpus_name_from_request(request)
 
         # check if the corpus exists
         try:
@@ -17,4 +39,5 @@ class CorpusAccessPermission(permissions.BasePermission):
         except:
             raise NotFound('Corpus does not exist')
 
+        # check if the user has access
         return user.has_access(corpus)
