3b4cad6378458d9a849df028bec192813a801dac
lukavdplas <lukavdplas@gmail.com>
2023-01-30 14:50:45 +0100

    add test with finished download

--- backend/download/tests/test_download_views.py
+++ backend/download/tests/test_download_views.py
@@ -1,5 +1,10 @@
 from rest_framework import status
 import pytest
+import os
+import csv
+from download import create_csv
+from download.models import Download
+import io
 
 @pytest.mark.xfail(reason='view not implemented')
 def test_direct_download_view(authenticated_client, mock_corpus):
@@ -84,18 +89,46 @@ def test_full_data_download_view(authenticated_client, mock_corpus, term_frequen
     )
     assert status.is_success(response.status_code)
 
-@pytest.mark.xfail(reason='view not implemented')
-def test_download_history_view(authenticated_client):
+def test_empty_download_history_view(authenticated_client):
     response = authenticated_client.get(
         '/api/download/'
     )
 
     assert status.is_success(response.status_code)
+    assert response.data == []
 
 @pytest.fixture()
-def finished_download(corpus_user):
-    # TODO: save CSV, store in database and return ID
-    return '0'
+def finished_download(corpus_user, csv_directory, mock_corpus, select_small_mock_corpus):
+    filepath = os.path.join(csv_directory, mock_corpus + '.csv')
+    download = Download.objects.create(download_type='search_results', corpus=mock_corpus, parameters={}, user=corpus_user)
+
+    with open(filepath, 'w') as outfile:
+        writer = csv.DictWriter(outfile,
+            fieldnames=['content', 'date', 'genre', 'query', 'title'],
+            **create_csv.SEARCH_RESULTS_DIALECT)
+        writer.writeheader()
+        writer.writerow({
+            'content': "You will rejoice to hear...",
+            'date': '1818-01-01',
+            'genre': 'Science fiction',
+            'query': mock_corpus,
+            'title': 'Frankenstein, or, the Modern Prometheus'
+        })
+
+    _, filename = os.path.split(filepath)
+    download.complete(filename)
+    return id
+
+def test_download_history_view(authenticated_client, finished_download, mock_corpus):
+    response = authenticated_client.get(
+        '/api/download/'
+    )
+
+    assert status.is_success(response.status_code)
+    assert len(response.data) == 1
+    download = next(d for d in response.data)
+    assert download['corpus'] == mock_corpus
+    assert download['status'] == 'done'
 
 @pytest.mark.xfail(reason='view not implemented')
 def test_csv_download_view(authenticated_client, finished_download):
