238166ebe41a30a1f96ef94e541f4816eedf0a38
Meesch <meesstip@gmail.com>
2023-02-02 17:35:40 +0100

    update test

--- backend/visualization/tests/test_celery.py
+++ backend/visualization/tests/test_celery.py
@@ -1,7 +1,6 @@
 from rest_framework.status import is_success
 import pytest
 
-@pytest.mark.celery(result_backend='redis://')
 def test_add(celery_worker, client):
     response = client.get('/api/visualization/add')
     assert is_success(response.status_code)
