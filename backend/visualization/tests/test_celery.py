0fa32aa2abe290242c0c3a9f7bceb64f1e9316a7
Jelte van Boheemen <j.vanboheemen@uu.nl>
2023-02-09 15:59:09 +0100

    Fix celery authentication in test

--- backend/visualization/tests/test_celery.py
+++ backend/visualization/tests/test_celery.py
@@ -1,7 +1,7 @@
 from rest_framework.status import is_success
-import pytest
 
-def test_add(celery_worker, client):
-    response = client.get('/api/visualization/add')
+
+def test_add(celery_worker, auth_client):
+    response = auth_client.get('/api/visualization/add')
     assert is_success(response.status_code)
     assert response.data == {'result': 3}
