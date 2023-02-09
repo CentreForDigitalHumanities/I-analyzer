3fee47adda20282273784bfe1fb10797dbf90df9
Jelte van Boheemen <j.vanboheemen@uu.nl>
2023-02-09 11:15:10 +0100

    Refactor corpus access tests

--- backend/users/conftest.py
+++ backend/users/conftest.py
@@ -1,5 +1,6 @@
 import pytest
-
+from django.contrib.auth.models import Group
+from addcorpus.models import Corpus
 
 @pytest.fixture
 def user_credentials():
@@ -13,3 +14,25 @@ def admin_credentials():
     return {'username': 'admin',
             'password': 'admin',
             'email': 'admin@ianalyzer.com'}
+
+
+@pytest.fixture
+def group_with_access():
+    nice = Group.objects.create(name='nice-users')
+    yield nice
+    nice.delete()
+
+
+@pytest.fixture
+def group_without_access():
+    naughty = Group.objects.create(name='naughty-users')
+    yield naughty
+    naughty.delete()
+
+
+@pytest.fixture
+def test_corpus(group_with_access):
+    corpus = Corpus.objects.create(name='test-corpus')
+    corpus.groups.add(group_with_access)
+    corpus.save()
+    yield corpus
