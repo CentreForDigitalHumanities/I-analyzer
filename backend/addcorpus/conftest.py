2c7f3c256c15cf708c7d97f25af43508b0ead2ba
lukavdplas <lukavdplas@gmail.com>
2023-02-27 13:06:29 +0100

    update corpus access tests

--- backend/addcorpus/conftest.py
+++ backend/addcorpus/conftest.py
@@ -6,10 +6,9 @@ from addcorpus.load_corpus import load_all_corpora
 from addcorpus.models import Corpus
 
 @pytest.fixture()
-def mock_corpus_group(db, mock_corpus):
+def group_with_access(db, mock_corpus):
     '''Create a group with access to the mock corpus'''
     group = Group.objects.create(name='nice-users')
-    load_all_corpora()
     corpus = Corpus.objects.get(name=mock_corpus)
     corpus.groups.add(group)
     corpus.save()
@@ -24,12 +23,13 @@ def mock_corpus_settings(settings):
     }
 
 @pytest.fixture()
-def mock_corpus(mock_corpus_settings):
+def mock_corpus(mock_corpus_settings, db):
+    load_all_corpora()
     return 'mock-csv-corpus'
 
 @pytest.fixture()
-def mock_corpus_user(db, mock_corpus_group):
+def mock_corpus_user(db, group_with_access):
     user = CustomUser.objects.create(username='mock-user', password='secret')
-    user.groups.add(mock_corpus_group)
+    user.groups.add(group_with_access)
     user.save()
     return user
