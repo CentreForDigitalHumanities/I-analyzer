c528bda8f8316fdea1f96f059cdc416ca8a540f5
BeritJanssen <berit.janssen@gmail.com>
2023-03-01 15:07:41 +0100

    showReset for different conditions

--- frontend/src/app/visualization/barchart/term-comparison-editor/term-comparison-editor.component.ts
+++ frontend/src/app/visualization/barchart/term-comparison-editor/term-comparison-editor.component.ts
@@ -11,6 +11,7 @@ export class TermComparisonEditorComponent implements OnChanges {
     @Input() termLimit = 10;
 
     queries: string[] = [];
+    public showReset = false;
 
     @Output() queriesChanged = new EventEmitter<string[]>();
     @Output() clearQueries = new EventEmitter<void>();
@@ -21,22 +22,27 @@ export class TermComparisonEditorComponent implements OnChanges {
 
     ngOnChanges(changes: SimpleChanges): void {
         this.queries = this.initialValue;
+        if (this.queries.length > 1) {
+            this.showReset = true;
+        }
     }
 
     confirmQueries() {
         this.queriesChanged.emit(this.queries);
+        this.showReset = true;
     }
 
     signalClearQueries() {
         this.queries = this.initialValue;
         this.clearQueries.emit();
+        this.showReset = false;
     }
 
 
     get disableConfirm(): boolean {
         if (!this.queries || !this.queries.length) {
- return false;
-}
+            return false;
+        }
         return this.queries.length >= this.termLimit;
     }
 }
