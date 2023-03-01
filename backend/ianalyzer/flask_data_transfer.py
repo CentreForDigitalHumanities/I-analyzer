2319dd9d67c26f75d075c418d4b38f94792fc522
Jelte van Boheemen <j.vanboheemen@uu.nl>
2023-03-01 16:17:57 +0100

    Add allauth emailadresses on Flask data transfer

--- backend/ianalyzer/flask_data_transfer.py
+++ backend/ianalyzer/flask_data_transfer.py
@@ -10,6 +10,8 @@ from download.models import Download
 import json
 from django.conf import settings
 import warnings
+from allauth.account.models import EmailAddress
+
 
 def adapt_password_encoding(flask_encoded):
     '''Adapt encoded password hash from flask to django format'''
@@ -19,6 +21,7 @@ def adapt_password_encoding(flask_encoded):
     hashed = base64.b64encode(raw_hash).decode('ascii').strip()
     rest = '$'.join([iteration, salt, hashed])
     return f'{alg}_{hash_alg}${rest}'
+
 
 flask_table_columns = {
     'user': [
@@ -38,9 +41,11 @@ flask_table_columns = {
     ]
 }
 
+
 def extract_row_data(values, table):
     columns = flask_table_columns[table]
-    return { col: value for col, value in zip(columns, values) }
+    return {col: value for col, value in zip(columns, values)}
+
 
 def import_table_data(directory, table):
     '''
@@ -62,6 +67,7 @@ def import_table_data(directory, table):
         data = [extract_row_data(row, table) for row in reader]
     return data
 
+
 def save_flask_group(row):
     '''
     Save a Group based on a datarow from the flask SQL data
@@ -74,23 +80,24 @@ def save_flask_group(row):
     they don't do anything.
     '''
 
-    group = Group(id = row['id'], name = row['name'])
+    group = Group(id=row['id'], name=row['name'])
     group.save()
+
 
 def save_flask_user(row):
     'Save a User based on a datarow from the flask SQL data'
 
     user = CustomUser(
-        id = row['id'],
-        username = row['username'],
-        password = 'nonsense', #we will set the password below
-        email = row['email'],
-        download_limit = row['download_limit'],
-        saml = row['saml'],
+        id=row['id'],
+        username=row['username'],
+        password='nonsense',  # we will set the password below
+        email=row['email'],
+        download_limit=row['download_limit'],
+        saml=row['saml'],
     )
     user.save()
 
-    group = Group.objects.get(id = row['role_id'])
+    group = Group.objects.get(id=row['role_id'])
     user.groups.add(group)
 
     if group.name == 'admin':
@@ -106,29 +113,37 @@ def save_flask_user(row):
             [password_hash, row['id']]
         )
 
+    # add an Allauth verified email address
+    EmailAddress.objects.create(
+        user=user, email=user.email, verified=True, primary=True)
+
+
 def save_flask_corpus(row):
     corpus = Corpus(**row)
     corpus.save()
 
+
 def save_flask_corpus_role(row):
-    corpus = Corpus.objects.get(id = row['corpus_id'])
-    group = Group.objects.get(id = row['role_id'])
+    corpus = Corpus.objects.get(id=row['corpus_id'])
+    group = Group.objects.get(id=row['role_id'])
     corpus.groups.add(group)
+
 
 def null_to_none(value):
     '''return None if the value is `'\\N'`, i.e. null'''
     return value if value != '\\N' else None
 
+
 def save_flask_query(row):
     query = Query(
-        id = row['id'],
-        query_json = json.loads(row['query']),
-        corpus = Corpus.objects.get(name = row['corpus_name']),
-        user = CustomUser.objects.get(id = row['userID']),
-        completed = null_to_none(row['completed']),
-        aborted = null_to_none(row['aborted']),
-        transferred = null_to_none(row['transferred']),
-        total_results = null_to_none(row['total_results'])
+        id=row['id'],
+        query_json=json.loads(row['query']),
+        corpus=Corpus.objects.get(name=row['corpus_name']),
+        user=CustomUser.objects.get(id=row['userID']),
+        completed=null_to_none(row['completed']),
+        aborted=null_to_none(row['aborted']),
+        transferred=null_to_none(row['transferred']),
+        total_results=null_to_none(row['total_results'])
     )
     query.save()
 
@@ -136,15 +151,16 @@ def save_flask_query(row):
     query.started = row['started']
     query.save()
 
+
 def save_flask_download(row):
     download = Download(
-        id = row['id'],
-        completed = null_to_none(row['completed']),
-        download_type = row['download_type'],
-        corpus = Corpus.objects.get(name = row['corpus_name']),
-        user = CustomUser.objects.get(id = row['user_id']),
-        parameters = json.loads(row['parameters']),
-        filename = os.path.relpath(row['filename'], settings.CSV_FILES_PATH),
+        id=row['id'],
+        completed=null_to_none(row['completed']),
+        download_type=row['download_type'],
+        corpus=Corpus.objects.get(name=row['corpus_name']),
+        user=CustomUser.objects.get(id=row['user_id']),
+        parameters=json.loads(row['parameters']),
+        filename=os.path.relpath(row['filename'], settings.CSV_FILES_PATH),
     )
     download.save()
 
@@ -156,6 +172,7 @@ def save_flask_download(row):
 def import_and_save_table(directory, flask_table_name, save_function, **kwargs):
     for row in import_table_data(directory, flask_table_name):
         save_function(row, **kwargs)
+
 
 def import_and_save_all_data(directory):
 
--- backend/ianalyzer/tests/test_flask_data_transfer.py
+++ backend/ianalyzer/tests/test_flask_data_transfer.py
@@ -1,13 +1,15 @@
 import os
+from datetime import datetime
+
 import django.contrib.auth.hashers as hashers
-from django.contrib.auth.models import Group
-from users.models import CustomUser
+import pytest
 from addcorpus.models import Corpus
+from allauth.account.models import EmailAddress
 from api.models import Query
+from django.contrib.auth.models import Group
 from download.models import Download
 from ianalyzer.flask_data_transfer import *
-import pytest
-from datetime import datetime, timezone
+from users.models import CustomUser
 
 _here = os.path.abspath(os.path.dirname(__file__))
 flask_test_data_dir = os.path.join(_here, 'flask_test_data')
@@ -19,7 +21,7 @@ def test_legacy_data_import():
     assert len(user_data) == 1
 
     user = user_data[0]
-    expected_user =  {
+    expected_user = {
         'id': '1',
         'username': 'admin',
         'password': 'password',
@@ -38,6 +40,7 @@ def test_legacy_data_import():
         else:
             assert user[key] == expected_user[key]
 
+
 def test_roles_import():
     role_data = import_table_data(flask_test_data_dir, 'role')
 
@@ -52,11 +55,13 @@ def test_roles_import():
 
     assert role == expected_role
 
+
 def test_save_groups(db):
     import_and_save_table(flask_test_data_dir, 'role', save_flask_group)
 
     groups = Group.objects.all()
     assert len(groups) == 2
+
 
 def test_save_legacy_user(db):
     import_and_save_table(flask_test_data_dir, 'role', save_flask_group)
@@ -68,24 +73,33 @@ def test_save_legacy_user(db):
     user = users[0]
     assert user.username == 'admin'
     assert user.email == 'admin@ianalyzer.nl'
-    assert list(user.groups.all()) == [Group.objects.get(name='admin')]
+    assert list(user.groups.all()) == [Group.objects.get(
+        name='basic'), Group.objects.get(name='admin')]
+
+    allauth_email = EmailAddress.objects.get(user=user)
+    assert allauth_email.email == user.email
+    assert allauth_email.verified
+
 
 def test_save_corpora(db):
     import_and_save_table(flask_test_data_dir, 'role', save_flask_group)
     import_and_save_table(flask_test_data_dir, 'corpus', save_flask_corpus)
-    import_and_save_table(flask_test_data_dir, 'corpora_roles', save_flask_corpus_role)
+    import_and_save_table(flask_test_data_dir,
+                          'corpora_roles', save_flask_corpus_role)
 
     corpora = Corpus.objects.all()
     assert len(corpora) == 13
 
-    corpus = Corpus.objects.get(id = '13')
+    corpus = Corpus.objects.get(id='13')
     assert corpus.name == 'parliament-ireland'
     assert corpus.description == 'Speeches from the Dáil Éireann and Seanad Éireann'
     assert list(corpus.groups.all()) == list(Group.objects.all())
 
+
 def dates_match(datetime1, datetime2):
     '''To avoid timezone issues, just check the dates to compare to datetime objects'''
     return datetime1.date() == datetime2.date()
+
 
 @pytest.mark.filterwarnings(
     'ignore:DateTimeField .* received a naive datetime (.*) while time zone support is active'
@@ -96,18 +110,20 @@ def test_save_queries(db):
     queries = Query.objects.all()
     assert len(queries) == 10
 
-    query = Query.objects.get(id = '507')
+    query = Query.objects.get(id='507')
 
-    assert query.query_json == {"queryText": "", "filters": [], "sortBy": "date", "sortAscending": False}
+    assert query.query_json == {
+        "queryText": "", "filters": [], "sortBy": "date", "sortAscending": False}
 
     assert dates_match(query.started,
-        datetime(year=2022, month=12, day=7, hour=14, minute=18, second=6))
-    assert query.completed == None
+                       datetime(year=2022, month=12, day=7, hour=14, minute=18, second=6))
+    assert query.completed is None
     assert query.total_results == 7915
-    assert query.aborted == False
+    assert query.aborted is False
     assert query.transferred == 0
-    assert query.user == CustomUser.objects.get(username = 'admin')
-    assert query.corpus == Corpus.objects.get(name = 'parliament-ireland')
+    assert query.user == CustomUser.objects.get(username='admin')
+    assert query.corpus == Corpus.objects.get(name='parliament-ireland')
+
 
 @pytest.mark.filterwarnings(
     'ignore:DateTimeField .* received a naive datetime (.*) while time zone support is active'
@@ -118,11 +134,11 @@ def test_save_downloads(db):
     downloads = Download.objects.all()
     assert len(downloads) == 10
 
-    download = Download.objects.get(id = '49')
+    download = Download.objects.get(id='49')
     assert dates_match(download.started,
-        datetime(year=2022, month=11, day=21, hour=10, minute=59, second=26))
+                       datetime(year=2022, month=11, day=21, hour=10, minute=59, second=26))
     assert dates_match(download.completed,
-        datetime(year=2022, month=11, day=21, hour=10, minute=59, second=27))
+                       datetime(year=2022, month=11, day=21, hour=10, minute=59, second=27))
     assert download.corpus == Corpus.objects.get(name='parliament-uk')
     assert download.user == CustomUser.objects.get(id='1')
     assert download.parameters == {
@@ -130,12 +146,13 @@ def test_save_downloads(db):
         "es_query": {
             "query": {"bool": {"must": {"match_all": {}}, "filter": []}},
             "sort": [{"date": "desc"}]},
-            "fields": ["date", "speech", "id", "sequence", "speaker"],
+        "fields": ["date", "speech", "id", "sequence", "speaker"],
         "route": "/search/parliament-uk"
     }
 
     _, filename = os.path.split(download.filename)
     assert filename == 'parliament-uk.csv'
+
 
 def test_no_data_to_import(db):
     '''Assert that a missing directory or missing files will raise a warning
