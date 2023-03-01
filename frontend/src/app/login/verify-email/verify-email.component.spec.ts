5fc35f90d78690b97a84bd19c5a45f6ec10860ea
Jelte van Boheemen <j.vanboheemen@uu.nl>
2023-03-01 16:52:32 +0100

    Fix VerifyEmailComponent tests

--- frontend/src/app/login/verify-email/verify-email.component.spec.ts
+++ frontend/src/app/login/verify-email/verify-email.component.spec.ts
@@ -1,25 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApiService } from '../../services/api.service';
+import { ApiServiceMock } from '../../../mock-data/api';
+import { SessionService } from '../../services/session.service';
 
 import { VerifyEmailComponent } from './verify-email.component';
 
 describe('VerifyEmailComponent', () => {
-  let component: VerifyEmailComponent;
-  let fixture: ComponentFixture<VerifyEmailComponent>;
+    let component: VerifyEmailComponent;
+    let fixture: ComponentFixture<VerifyEmailComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ VerifyEmailComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [VerifyEmailComponent],
+            imports: [RouterTestingModule],
+            providers: [
+                SessionService,
+                { provide: ApiService, useValue: new ApiServiceMock() },
+            ],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(VerifyEmailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(VerifyEmailComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
