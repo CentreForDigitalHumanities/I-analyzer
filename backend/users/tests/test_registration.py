c986ed905be93b64f2e723cf04acc095d6820808
Jelte van Boheemen <j.vanboheemen@uu.nl>
2023-03-01 15:40:40 +0100

    Full backend test for registration

--- backend/users/tests/test_registration.py
+++ backend/users/tests/test_registration.py
@@ -1,7 +1,9 @@
 from django.core import mail
+import re
+from allauth.account.models import EmailAddress
 
 
-def test_register(client, db, django_user_model):
+def test_register_verification(client, db, django_user_model):
     creds = {
         'username': 'ThomasCromwell',
         'password1': 'annaregina',
@@ -11,14 +13,33 @@ def test_register(client, db, django_user_model):
     response = client.post('/users/registration/', creds)
     assert response
 
+    # Check if user was registered
     db_user = django_user_model.objects.get(
         username=creds['username'])
     assert db_user
 
+    # Check if registration mail was sent
     box = mail.outbox
     assert len(box) == 1
-    assert box[0].to[0] == creds['email']
+    verify_mail = box[0]
+    assert verify_mail.to[0] == creds['email']
 
+    key = re.search(r'account-confirm-email\/(.+)\/',
+                    verify_mail.body).group(1)
+    assert key
 
-def test_verify(client):
-    assert True
+    # Check key information
+    key_info = client.post('/users/registration/key-info/', {'key': key})
+    assert key_info.status_code == 200
+    assert key_info.data.get('username') == creds.get('username')
+
+    # Check verification
+    verify_response = client.post(
+        '/users/registration/verify-email/', {'key': key})
+    assert verify_response.status_code == 200
+
+    # Check if email address was verified
+    allauth_email = EmailAddress.objects.get(user=db_user)
+    assert allauth_email.email == creds.get('email')
+    assert allauth_email.verified is True
+    assert allauth_email.primary is True
