38d41d6d7c93c46475ce7e227b8306e841ef6be6
lukavdplas <lukavdplas@gmail.com>
2023-03-09 18:32:36 +0100

    make sort utils file

--- frontend/src/app/utils/sort.ts
+++ frontend/src/app/utils/sort.ts
@@ -1,2 +1,7 @@
+import { Corpus, SortBy } from '../models';
+
+export const sortByDefault = (corpus: Corpus): SortBy =>
+    corpus.fields.find(field => field.primarySort) || 'relevance';
+
 export const sortDirectionFromBoolean = (sortAscending: boolean): 'asc'|'desc' =>
     sortAscending ? 'asc' : 'desc';
