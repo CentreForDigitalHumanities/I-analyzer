d5373b90d0b320e0ea757b77f856c76587e91a14
lukavdplas <lukavdplas@gmail.com>
2023-03-13 12:02:12 +0100

    querymodel -> esquery conversion: search fields

--- backend/api/query_model_to_es_query.py
+++ backend/api/query_model_to_es_query.py
@@ -3,5 +3,16 @@
 from visualization import query
 
 def query_model_to_es_query(query_model):
-    es_query = query.set_query_text(query.MATCH_ALL, query_model['queryText'])
+    es_query = query.set_query_text(query.MATCH_ALL, get_query_text(query_model))
+
+    search_fields = get_search_fields(query_model)
+    if search_fields:
+        es_query = query.set_search_fields(es_query, search_fields)
+
     return es_query
+
+def get_query_text(query_model):
+    return query_model['queryText']
+
+def get_search_fields(query_model):
+    return query_model.get('fields', None)
