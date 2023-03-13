b90a89569b55f9b60cdb2fcf5536dedf75027bcc
lukavdplas <lukavdplas@gmail.com>
2023-03-13 12:17:25 +0100

    querymodel -> esquery conversion: highlight

--- backend/api/query_model_to_es_query.py
+++ backend/api/query_model_to_es_query.py
@@ -13,10 +13,14 @@ def query_model_to_es_query(query_model):
     if sort_by:
         es_query = query.set_sort(es_query, sort_by, sort_direction)
 
+    highlight = get_highlight(query_model)
+    if highlight:
+        es_query = query.set_highlight(es_query, highlight)
+
     return es_query
 
 def get_query_text(query_model):
-    return query_model['queryText']
+    return query_model.get('queryText', None)
 
 def get_search_fields(query_model):
     return query_model.get('fields', None)
@@ -25,4 +29,7 @@ def get_sort(query_model):
     sort_by = query_model.get('sortBy', None)
     direction = 'asc' if query_model.get('sortAscending', True) else 'desc'
     return sort_by, direction
+
+def get_highlight(query_model):
+    return query_model.get('highlight', None)
 
