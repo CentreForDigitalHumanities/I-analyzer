0afe39735f57a1590c6109be87fd5b3110dd41b5
lukavdplas <lukavdplas@gmail.com>
2023-04-28 12:49:04 +0200

    show filtered corpora in overview

--- frontend/src/app/corpus-selection/corpus-selection.component.ts
+++ frontend/src/app/corpus-selection/corpus-selection.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Corpus } from '../models/corpus';
+import * as _ from 'lodash';
 
 
 @Component({
@@ -11,7 +12,17 @@ export class CorpusSelectionComponent implements OnInit {
     @Input()
     public items: Corpus[];
 
+    filteredItems: Corpus[];
+
     constructor() { }
+
+    get displayItems(): Corpus[] {
+        if (_.isUndefined(this.filteredItems)) {
+            return this.items;
+        } else {
+            return this.filteredItems;
+        }
+    }
 
     ngOnInit() {
     }
