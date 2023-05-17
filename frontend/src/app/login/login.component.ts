1ae36ff90da122cb84511845a22f17fa6ede5369
Jelte van Boheemen <j.vanboheemen@uu.nl>
2023-05-17 10:43:12 +0200

    Error messages login not verified & server error

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
+import { takeUntil, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
+import { HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 @Component({
@@ -13,16 +14,17 @@ import { environment } from '../../environments/environment';
     styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
+    public static activated = false;
     public username: string;
     public password: string;
     public isLoading: boolean;
     public isWrong: boolean;
+    public isNotVerified: boolean;
     public hasError: boolean;
 
-    private returnUrl: string;
-
-    public static activated = false;
     public showSolis: boolean;
+
+    private returnUrl: string;
 
     private destroy$ = new Subject<boolean>();
 
@@ -68,6 +70,13 @@ export class LoginComponent implements OnInit, OnDestroy {
         this.isLoading = true;
         this.authService
             .login(this.username, this.password)
+            .pipe(
+                tap(() => {
+                    this.isWrong = undefined;
+                    this.isNotVerified = undefined;
+                    this.hasError = undefined;
+                })
+            )
             .subscribe(this.onLoginSuccess, this.onLoginFail);
     }
 
@@ -80,8 +89,22 @@ export class LoginComponent implements OnInit, OnDestroy {
     }
 
     private onLoginSuccess = () => this.router.navigateByUrl(this.returnUrl);
-    private onLoginFail = () => {
+    private onLoginFail = (error: HttpErrorResponse) => {
         this.isLoading = false;
-        this.isWrong = true;
+        if (this.notVerified(error)) {
+            this.isNotVerified = true;
+        } else if (this.isServerError(error)) {
+            this.hasError = true;
+        } else {
+            this.isWrong = true;
+        }
     };
+
+    private notVerified = (err: HttpErrorResponse) =>
+        err.status === 400 &&
+        err.error?.non_field_errors &&
+        err.error?.non_field_errors[0] === this.authService.notVerifiedMsg;
+
+    private isServerError = (err: HttpErrorResponse) =>
+        err.status >= 500 && err.status <= 599;
 }
