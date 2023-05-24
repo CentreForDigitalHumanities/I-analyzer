3c5c2ff51005d5ff7569561940c5a0db96110188
BeritJanssen <berit.janssen@gmail.com>
2023-05-24 11:30:49 +0200

    override djangosaml2.LogoutView to make post csrf_exempt

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 from allauth.account.models import EmailConfirmationHMAC, EmailConfirmation
 from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND
 from rest_framework.exceptions import APIException
 from rest_framework.permissions import AllowAny
+
+from djangosaml2.views import LogoutView
 
 
 def redirect_confirm(request, key):
@@ -31,3 +34,10 @@ class KeyInfoView(APIView):
                             'email': info.email_address.email})
         except Exception as e:
             raise APIException(e)
+
+
+class SamlLogoutView(LogoutView):
+
+    @csrf_exempt
+    def post(self, request, *args, **wkargs):
+        super().post(request, *args, **kwargs)
