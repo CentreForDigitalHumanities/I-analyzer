af37a2f3a1c610e3ed775858558caeaae5e2b2ac
Luka van der Plas <43678097+lukavdplas@users.noreply.github.com>
2023-06-07 14:26:13 +0200

    Update word models mock corpus docstring
    Co-authored-by: Berit <berit.janssen@gmail.com>

--- backend/wordmodels/tests/mock-corpus/mock_corpus.py
+++ backend/wordmodels/tests/mock-corpus/mock_corpus.py
@@ -6,7 +6,7 @@ from os.path import abspath, dirname, join
 here = abspath(dirname(__file__))
 
 class WordmodelsMockCorpus(Corpus):
-    title = "Mock corpus with SVD_PPMI models"
+    title = "Mock corpus with word models represented as Keyed Vectors"
     description = "Mock corpus for testing word models, saved as gensim Keyed Vectors"
     es_index = 'nothing'
     min_date = datetime.datetime(year=1810, month=1, day=1)
