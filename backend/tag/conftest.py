d94630756771955aba20b06af76661c7613cac86
lukavdplas <lukavdplas@gmail.com>
2023-06-12 17:22:40 +0200

    add mock corpus

--- backend/tag/conftest.py
+++ backend/tag/conftest.py
@@ -1,8 +1,7 @@
 import pytest
-from addcorpus.models import Corpus
+from addcorpus.load_corpus import load_all_corpora
 
 @pytest.fixture()
 def mock_corpus(db):
-    name = 'mock-corpus'
-    Corpus.objects.create(name=name)
-    return name
+    load_all_corpora()
+    return 'tagging-mock-corpus'
