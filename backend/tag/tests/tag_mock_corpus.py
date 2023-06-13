e9c6d282a1f168bb9dc1245456400d56eec3a853
lukavdplas <lukavdplas@gmail.com>
2023-06-13 15:42:01 +0200

    test searching with tag filter

--- backend/tag/tests/tag_mock_corpus.py
+++ backend/tag/tests/tag_mock_corpus.py
@@ -7,6 +7,7 @@ from addcorpus.es_mappings import keyword_mapping, main_content_mapping
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+
 class TaggingMockCorpus(CSVCorpus):
     title = 'Tagging Mock Corpus'
     description = 'Mock corpus for tagging'
@@ -14,10 +15,15 @@ class TaggingMockCorpus(CSVCorpus):
     min_date = datetime.datetime(year=1, month=1, day=1)
     max_date = datetime.datetime(year=2022, month=12, day=31)
     image = 'nothing.jpeg'
-    data_directory = os.path.join(here, 'csv_example')
+    data_directory = os.path.join(here, 'data')
+
+    def sources(self, start, end):
+        return (
+            (os.path.join(self.data_directory, file), {})
+            for file in os.listdir(self.data_directory)
+        )
     languages = ['en']
     category = 'book'
-
 
     fields = [
         Field(
--- backend/tag/tests/test_tag_filter.py
+++ backend/tag/tests/test_tag_filter.py
@@ -1,5 +1,14 @@
-from tag.filter import tag_document_ids
+from tag.filter import tag_document_ids, tag_filter
+from es import search
+
 
 def test_tag_document_ids(mock_corpus, auth_user_tag, tagged_documents):
     _, docs = tagged_documents
     assert len(tag_document_ids(auth_user_tag, mock_corpus)) == 3
+
+
+def test_tag_search(mock_corpus, index_mock_corpus, auth_user_tag, tagged_documents):
+    filter = tag_filter(auth_user_tag.id, mock_corpus)
+    query = {'query': filter}
+    results = search.search(mock_corpus, query)
+    assert search.total_hits(results) == len(tagged_documents)
