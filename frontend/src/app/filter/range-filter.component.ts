3814842963416b00e69a9ca09c13e68762f72762
lukavdplas <lukavdplas@gmail.com>
2023-06-28 18:29:20 +0200

    fix range filter crashing page

--- frontend/src/app/filter/range-filter.component.ts
+++ frontend/src/app/filter/range-filter.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { RangeFilterData, RangeFilter } from '../models';
 import { BaseFilterComponent } from './base-filter.component';
+import { map, tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ia-range-filter',
@@ -12,9 +14,16 @@ export class RangeFilterComponent extends BaseFilterComponent<RangeFilterData> {
     min: number;
     max: number;
 
+    data$: Observable<[number, number]>;
+
     onFilterSet(filter: RangeFilter): void {
         this.min = filter.defaultData.min;
         this.max = filter.defaultData.max;
+
+        this.data$ = this.filter?.data.asObservable().pipe(
+            tap(data => console.log(data)),
+            map(this.getDisplayData)
+        );
     }
 
     getDisplayData(filterData: RangeFilterData): [number, number] {
