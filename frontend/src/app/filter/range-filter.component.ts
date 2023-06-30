0dfbf762074b4621a0dfbc80498b309861828c3e
lukavdplas <lukavdplas@gmail.com>
2023-06-30 11:32:40 +0200

    cleaner logic from filter data to template

--- frontend/src/app/filter/range-filter.component.ts
+++ frontend/src/app/filter/range-filter.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 import { RangeFilterData, RangeFilter } from '../models';
 import { BaseFilterComponent } from './base-filter.component';
-import { map, tap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'ia-range-filter',
@@ -14,20 +12,9 @@ export class RangeFilterComponent extends BaseFilterComponent<RangeFilterData> {
     min: number;
     max: number;
 
-    data$: Observable<[number, number]>;
-
     onFilterSet(filter: RangeFilter): void {
         this.min = filter.defaultData.min;
         this.max = filter.defaultData.max;
-
-        this.data$ = this.filter?.data.asObservable().pipe(
-            tap(data => console.log(data)),
-            map(this.getDisplayData)
-        );
-    }
-
-    getDisplayData(filterData: RangeFilterData): [number, number] {
-        return [filterData.min, filterData.max];
     }
 
     getFilterData(value: [number, number]): RangeFilterData {
