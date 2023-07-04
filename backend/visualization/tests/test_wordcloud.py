dee440b8bfbd65051a2c36cf7ce2cf3be6ec75f2
lukavdplas <lukavdplas@gmail.com>
2023-07-04 17:20:23 +0200

    adjust wordcloud tests, add test for stopword removal

--- backend/visualization/tests/test_wordcloud.py
+++ backend/visualization/tests/test_wordcloud.py
@@ -4,135 +4,133 @@ import pytest
 import visualization.query as query
 from datetime import datetime
 
+
 def make_filtered_query():
-        empty_query = {
-            "query": {
-                "bool": {
-                    "filter": []
-                }
+    empty_query = {
+        "query": {
+            "bool": {
+                "filter": []
             }
         }
-        datefilter = query.make_date_filter(max_date = datetime(year = 1820, month=12, day=31))
-        return query.add_filter(empty_query, datefilter)
-
-
-def test_wordcloud(small_mock_corpus, index_small_mock_corpus):
-    query = {
-        "query": {
-            "match_all": {}
-        },
     }
+    datefilter = query.make_date_filter(
+        max_date=datetime(year=1820, month=12, day=31))
+    return query.add_filter(empty_query, datefilter)
 
+
+@pytest.fixture()
+def small_mock_corpus_complete_wordcloud(small_mock_corpus, index_small_mock_corpus):
     result = search.search(
-        corpus = small_mock_corpus,
-        query_model = query,
-        size = 10
+        corpus=small_mock_corpus,
+        query_model=query.MATCH_ALL,
+        size=10
     )
 
     documents = search.hits(result)
+    return wordcloud.make_wordcloud_data(documents, 'content', small_mock_corpus)
+
+
+def test_wordcloud(small_mock_corpus, small_mock_corpus_complete_wordcloud):
 
     target_unfiltered = [
-        { 'key': 'the', 'doc_count': 2 },
-        { 'key': 'you', 'doc_count': 2 },
-        { 'key': 'that', 'doc_count': 2 },
-        { 'key': 'with', 'doc_count': 1 },
-        { 'key': 'will', 'doc_count': 1 },
-        { 'key': 'wife', 'doc_count': 1 },
-        { 'key': 'which', 'doc_count': 1 },
-        { 'key': 'was', 'doc_count': 1 },
-        { 'key': 'want', 'doc_count': 1 },
-        { 'key': 'very', 'doc_count': 1 },
-        { 'key': 'universally', 'doc_count': 1 },
-        { 'key': 'truth', 'doc_count': 1 },
-        { 'key': 'tired', 'doc_count': 1 },
-        { 'key': 'such', 'doc_count': 1 },
-        { 'key': 'sitting', 'doc_count': 1 },
-        { 'key': 'sister', 'doc_count': 1 },
-        { 'key': 'single', 'doc_count': 1 },
-        { 'key': 'rejoice', 'doc_count': 1 },
-        { 'key': 'regarded', 'doc_count': 1 },
-        { 'key': 'possession', 'doc_count': 1 },
-        { 'key': 'nothing', 'doc_count': 1 },
-        { 'key': 'must', 'doc_count': 1 },
-        { 'key': 'man', 'doc_count': 1 },
-        { 'key': 'her', 'doc_count': 1 },
-        { 'key': 'hear', 'doc_count': 1 },
-        { 'key': 'having', 'doc_count': 1 },
-        { 'key': 'have', 'doc_count': 1 },
-        { 'key': 'has', 'doc_count': 1 },
-        { 'key': 'good', 'doc_count': 1 },
-        { 'key': 'get', 'doc_count': 1 },
-        { 'key': 'fortune', 'doc_count': 1 },
-        { 'key': 'forebodings', 'doc_count': 1 },
-        { 'key': 'evil', 'doc_count': 1 },
-        { 'key': 'enterprise', 'doc_count': 1 },
-        { 'key': 'disaster', 'doc_count': 1 },
-        { 'key': 'commencement', 'doc_count': 1 },
-        { 'key': 'beginning', 'doc_count': 1 },
-        { 'key': 'bank', 'doc_count': 1 },
-        { 'key': 'and', 'doc_count': 1 },
-        { 'key': 'alice', 'doc_count': 1 },
-        { 'key': 'acknowledged', 'doc_count': 1 },
-        { 'key': 'accompanied', 'doc_count': 1 }
+        {'key': 'wife', 'doc_count': 1},
+        {'key': 'universally', 'doc_count': 1},
+        {'key': 'truth', 'doc_count': 1},
+        {'key': 'tired', 'doc_count': 1},
+        {'key': 'sitting', 'doc_count': 1},
+        {'key': 'sister', 'doc_count': 1},
+        {'key': 'single', 'doc_count': 1},
+        {'key': 'rejoice', 'doc_count': 1},
+        {'key': 'regarded', 'doc_count': 1},
+        {'key': 'possession', 'doc_count': 1},
+        {'key': 'nothing', 'doc_count': 1},
+        {'key': 'man', 'doc_count': 1},
+        {'key': 'hear', 'doc_count': 1},
+        {'key': 'having', 'doc_count': 1},
+        {'key': 'good', 'doc_count': 1},
+        {'key': 'fortune', 'doc_count': 1},
+        {'key': 'forebodings', 'doc_count': 1},
+        {'key': 'evil', 'doc_count': 1},
+        {'key': 'enterprise', 'doc_count': 1},
+        {'key': 'disaster', 'doc_count': 1},
+        {'key': 'commencement', 'doc_count': 1},
+        {'key': 'beginning', 'doc_count': 1},
+        {'key': 'bank', 'doc_count': 1},
+        {'key': 'alice', 'doc_count': 1},
+        {'key': 'acknowledged', 'doc_count': 1},
+        {'key': 'accompanied', 'doc_count': 1}
     ]
 
-    output = wordcloud.make_wordcloud_data(documents, 'content', small_mock_corpus)
     for item in target_unfiltered:
         term = item['key']
         doc_count = item['doc_count']
-        match = next(hit for hit in output if hit['key'] == term)
+        match = next(
+            hit for hit in small_mock_corpus_complete_wordcloud if hit['key'] == term)
         assert match
         assert doc_count == match['doc_count']
+
 
 def test_wordcloud_filtered(small_mock_corpus, es_client, index_small_mock_corpus):
     """Test the word cloud on a query with date filter"""
 
     filtered_query = make_filtered_query()
 
-    target_filtered = [
-        {'key': 'accompanied', 'doc_count': 1},
-        {'key': 'acknowledged', 'doc_count': 1},
-        {'key': 'commencement', 'doc_count': 1},
-        {'key': 'disaster', 'doc_count': 1},
-        {'key': 'enterprise', 'doc_count': 1},
-        {'key': 'evil', 'doc_count': 1},
-        {'key': 'forebodings', 'doc_count': 1},
-        {'key': 'fortune', 'doc_count': 1},
-        {'key': 'good', 'doc_count': 1},
-        {'key': 'has', 'doc_count': 1},
-        {'key': 'have', 'doc_count': 1},
-        {'key': 'hear', 'doc_count': 1},
-        {'key': 'man', 'doc_count': 1},
-        {'key': 'must', 'doc_count': 1},
-        {'key': 'possession', 'doc_count': 1},
-        {'key': 'regarded', 'doc_count': 1},
-        {'key': 'rejoice', 'doc_count': 1},
-        {'key': 'single', 'doc_count': 1},
-        {'key': 'such', 'doc_count': 1},
-        {'key': 'the', 'doc_count': 1},
-        {'key': 'truth', 'doc_count': 1},
-        {'key': 'universally', 'doc_count': 1},
-        {'key': 'want', 'doc_count': 1},
-        {'key': 'which', 'doc_count': 1},
-        {'key': 'wife', 'doc_count': 1},
-        {'key': 'will', 'doc_count': 1},
-        {'key': 'with', 'doc_count': 1},
-        {'key': 'you', 'doc_count': 2}
-    ]
-
     result = search.search(
-        corpus = small_mock_corpus,
-        query_model = filtered_query,
-        size = 10,
-        client = es_client
+        corpus=small_mock_corpus,
+        query_model=filtered_query,
+        size=10,
+        client=es_client
     )
 
     documents = search.hits(result)
-    output = wordcloud.make_wordcloud_data(documents, 'content', small_mock_corpus)
+    output = wordcloud.make_wordcloud_data(
+        documents, 'content', small_mock_corpus)
 
-    for item in target_filtered:
-        term = item['key']
-        doc_count = item['doc_count']
-        match = next(hit for hit in output if hit['key'] == term)
-        assert match
-        assert doc_count == match['doc_count']
+    # from frankenstein + pride & prejudice
+    words_to_include = ['accompanied', 'acknowledged', 'truth', 'universally']
+    words_to_exclude = ['alice', 'beginning']  # from alice in wonderland
+
+    def occurs_in_results(word): return any(
+        item['key'] == word for item in output)
+
+    for word in words_to_include:
+        assert occurs_in_results(word)
+
+    for word in words_to_exclude:
+        assert not occurs_in_results(word)
+
+def test_wordcloud_counts(small_mock_corpus):
+    '''
+    Each non-stopword only occurs once in the mock corpus data, so
+    this test uses some fake texts for counting.
+    '''
+
+    texts = [
+        'Some words',
+        'Even more!',
+        'Words, words, words...',
+        'More words! More!',
+        'That should be enough.',
+    ]
+    docs = [
+        {'_source': {'content': text}}
+        for text in texts
+    ]
+
+    results = wordcloud.make_wordcloud_data(docs, 'content', small_mock_corpus)
+
+    counts = {
+        item['key']: item['doc_count']
+        for item in results
+    }
+
+    assert counts['words'] == 5
+    assert counts['more'] == 3
+
+def test_wordcloud_filters_stopwords(small_mock_corpus, small_mock_corpus_complete_wordcloud):
+    stopwords = ['the', 'and', 'of']
+
+    for stopword in stopwords:
+        match = any(
+            item['key'] == stopword for item in small_mock_corpus_complete_wordcloud)
+        assert not match
