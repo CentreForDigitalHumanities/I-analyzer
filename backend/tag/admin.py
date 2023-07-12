b4d6f46009f750c03b6b2bfe57e83e32c0c2dbe8
lukavdplas <lukavdplas@gmail.com>
2023-07-12 14:52:26 +0200

    add tag models to admin

--- backend/tag/admin.py
+++ backend/tag/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from tag.models import Tag, TaggedDocument
 
 # Register your models here.
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'user']
+
+class TaggedDocumentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'corpus', 'doc_id']
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(TaggedDocument, TaggedDocumentAdmin)
