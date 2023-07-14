fbc0f83be071921ccff40c174e2a927e682b4625
lukavdplas <lukavdplas@gmail.com>
2023-07-14 12:45:25 +0200

    emit events in tag-select

--- frontend/src/app/tag/tag-select/tag-select.component.ts
+++ frontend/src/app/tag/tag-select/tag-select.component.ts
@@ -1,25 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { TagService } from '../../services/tag.service';
 import { Observable } from 'rxjs';
 import { Tag } from '../../models';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+import * as _ from 'lodash';
+
 
 @Component({
     selector: 'ia-tag-select',
     templateUrl: './tag-select.component.html',
     styleUrls: ['./tag-select.component.scss']
 })
-export class TagSelectComponent implements OnInit {
+export class TagSelectComponent {
+    @Input() exclude: Tag[];
+    @Output() selection = new EventEmitter<number>();
+    @Output() cancel = new EventEmitter<void>();
+
+    @ViewChild('tagSelect') tagSelect: ElementRef;
+
     tags$: Observable<Tag[]>;
 
     faCheck = faCheck;
     faTimes = faTimes;
 
+
     constructor(private tagService: TagService) {
         this.tags$ = this.tagService.tags$;
     }
 
-    ngOnInit(): void {
+    get selectedTagId(): number {
+        const option = this.tagSelect.nativeElement.selectedOptions[0];
+        return parseInt(option.value, 10);
     }
 
+    filterTags(tags: Tag[], exclude?: Tag[]) {
+        return _.differenceBy(tags, exclude || [], 'name');
+    }
+
+    confirm() {
+        this.selection.emit(this.selectedTagId);
+    }
 }
