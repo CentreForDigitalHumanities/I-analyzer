ca823c5212770a00dcc276281c2e473336e8f83e
lukavdplas <lukavdplas@gmail.com>
2023-07-19 11:49:30 +0200

    add notifications to search history toggle

--- frontend/src/app/settings/search-history-settings/toggle-search-history/toggle-search-history.component.ts
+++ frontend/src/app/settings/search-history-settings/toggle-search-history/toggle-search-history.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { AuthService, NotificationService } from '../../../services';
 import { map } from 'rxjs/operators';
-import { AuthService } from '../../services';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'ia-toggle-search-history',
@@ -11,7 +11,7 @@ import { AuthService } from '../../services';
 export class ToggleSearchHistoryComponent {
     searchHistoryEnabled$: Observable<boolean>;
 
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService, private notificationService: NotificationService) {
         this.searchHistoryEnabled$ = this.authService.currentUser$.pipe(
             map(user => user?.enableSearchHistory)
         );
@@ -19,7 +19,14 @@ export class ToggleSearchHistoryComponent {
 
     emitChange(setting: boolean) {
         const data = { enableSearchHistory: setting };
-        this.authService.updateSettings(data).subscribe();
+        const succesMessage = `Search history will ${setting ? '' : 'not '} be saved from now on`;
+        this.authService.updateSettings(data).subscribe(
+            res => this.notificationService.showMessage(succesMessage, 'success'),
+            err => this.notificationService.showMessage(
+                'An error occured while trying to save your search history setting',
+                'danger'
+            ),
+        );
     }
 
 }
