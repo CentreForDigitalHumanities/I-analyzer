1f5aac7cd3aad0498a9ef73d714d726c0936a3dc
Meesch <meesstip@gmail.com>
2023-07-20 17:53:36 +0200

    prelim implement new highlights for document view

--- frontend/src/app/document-view/document-view.component.ts
+++ frontend/src/app/document-view/document-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges } from '@angular/core';
 
-import { CorpusField, FoundDocument, Corpus } from '../models/index';
+import { CorpusField, FoundDocument, Corpus, QueryModel } from '../models/index';
 
 
 @Component({
@@ -22,7 +22,7 @@ export class DocumentViewComponent implements OnChanges {
     public document: FoundDocument;
 
     @Input()
-    public query: string;
+    public queryModel: QueryModel;
 
     @Input()
     public corpus: Corpus;
@@ -51,4 +51,37 @@ export class DocumentViewComponent implements OnChanges {
     isUrlField(field: CorpusField) {
         return field.name === 'url' || field.name.startsWith('url_');
     }
+
+    /**
+     * Checks if user has selected fields in the queryModel and whether current field is among them
+     * Used to check which fields need to be highlighted
+     */
+    selectedFieldsContain(field: CorpusField) {
+        if (this.queryModel && this.queryModel.searchFields && this.queryModel.searchFields.includes(field)) {
+            return true;
+        } else if (this.queryModel && !this.queryModel.searchFields) {
+            return true;  // if there are no selected fields, return true for all fields
+        } else {
+            return false;
+        }
+    }
+
+    stripTags(htmlString: string){
+        const parseHTML= new DOMParser().parseFromString(htmlString, 'text/html');
+        return parseHTML.body.textContent || '';
+      }
+
+    highlightedInnerHtml(field: CorpusField) {
+        let highlighted = this.document.fieldValues[field.name];
+        if (this.document.highlight && this.document.highlight.hasOwnProperty(field.name) &&
+            this.selectedFieldsContain(field)) {
+            for (let highlight of this.document.highlight[field.name]) {
+                const stripped_highlight = this.stripTags(highlight);
+                highlighted = highlighted.replace(stripped_highlight, highlight);
+            }
+            return highlighted;
+            } else {
+                return this.document.fieldValues[field.name];
+            }
+        }
 }
