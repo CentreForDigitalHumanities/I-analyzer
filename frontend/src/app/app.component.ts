20a11d93896a331f8cf4dc69b8fd1bee67c323c6
BeritJanssen <berit.janssen@gmail.com>
2023-07-20 13:35:18 +0200

    remove header and footer in case of iframe display

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
 
+import { environment } from '../environments/environment';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+    public iframe: boolean
+
     constructor(private authService: AuthService) {
         this.authService.setInitialAuth();
+        this.iframe = environment.runInIFrame;
     }
 }
 
