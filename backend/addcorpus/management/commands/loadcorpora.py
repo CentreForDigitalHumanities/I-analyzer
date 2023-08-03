e81f0b51d91dbbc2b187840077fd22deda102475
Luka van der Plas <lukavdplas@gmail.com>
2023-08-03 12:24:12 +0200

    add documentation on loadcorpora command

--- backend/addcorpus/management/commands/loadcorpora.py
+++ backend/addcorpus/management/commands/loadcorpora.py
@@ -4,9 +4,17 @@ from addcorpus.save_corpus import load_and_save_all_corpora
 class Command(BaseCommand):
     help = '''
     Load all python corpus definitions (configured in settings)
-    into the database
+    into the database.
+
+    This command will add any newly added corpora to the database,
+    and update any existing corpora.
+
+    If a corpus is removed from settings or cannot be successfully
+    loaded, it will not be removed from the database, to prevent data
+    loss.
     '''
 
     def handle(self, *args, **kwargs):
         load_and_save_all_corpora()
+
         self.stdout.write('Finished loading corpora')
