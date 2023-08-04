12102dc374c71ded9c6059b367bd24f32a7417b6
Luka van der Plas <lukavdplas@gmail.com>
2023-08-04 09:03:41 +0200

    add required properties to model test

--- backend/addcorpus/tests/test_models.py
+++ backend/addcorpus/tests/test_models.py
@@ -1,7 +1,14 @@
 from addcorpus.models import Corpus
+from datetime import datetime
 
 def test_corpus_model(db):
-    corpus = Corpus(name = 'test_corpus', description = 'test')
+    corpus = Corpus(
+        name = 'test_corpus',
+        description = 'test',
+        languages = ['en'],
+        max_date = datetime(1800, 1, 1),
+        min_date = datetime(1900, 1, 1,),
+    )
     corpus.save()
 
     assert Corpus.objects.filter(name = corpus.name)
