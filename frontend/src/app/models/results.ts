9a187a6dd8ffbcd7648d496cc6bc8cdde11d9770
Luka van der Plas <lukavdplas@gmail.com>
2023-11-01 14:24:00 +0100

    enable next/prev document buttons

--- frontend/src/app/models/results.ts
+++ frontend/src/app/models/results.ts
@@ -7,6 +7,7 @@ import { FoundDocument } from './found-document';
 import { SearchService } from '../services';
 import { SearchResults } from './search-results';
 import { CorpusField } from './corpus';
+import * as _ from 'lodash';
 
 abstract class Results<Parameters, Result> {
     parameters$: BehaviorSubject<Parameters>;
@@ -47,14 +48,36 @@ export class DocumentPage {
         public documents: FoundDocument[],
         public total: number,
         public fields?: CorpusField[]
-    ) { }
+    ) {
+        this.documents.forEach((d, i) => d.position = i + 1);
+    }
 
     focus(document: FoundDocument) {
         this.focus$.next(document);
     }
 
+    focusNext() {
+        this.focusShift(1);
+    }
+
+    focusPrevious() {
+        this.focusShift(-1);
+    }
+
     blur() {
         this.focus$.next(undefined);
+    }
+
+    private focusShift(shift: number) {
+        const document = this.focus$.value;
+        if (document) {
+            this.focusPosition(document.position + shift);
+        }
+    }
+
+    private focusPosition(position: number) {
+        const index = _.clamp(position - 1, 0, this.documents.length - 1);
+        this.focus(this.documents[index]);
     }
 }
 
