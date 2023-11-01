9c3d211e3a773718f926c8ff4c3c0bf2f84fdaf0
Luka van der Plas <lukavdplas@gmail.com>
2023-11-01 14:43:31 +0100

    show page and context link

--- frontend/src/app/document/document-popup/document-popup.component.ts
+++ frontend/src/app/document/document-popup/document-popup.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { DocumentPage } from '../../models/results';
 import { filter } from 'rxjs/operators';
 import * as _ from 'lodash';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faBookOpen, faLink } from '@fortawesome/free-solid-svg-icons';
+import { FoundDocument, QueryModel } from '../../models';
 
 @Component({
     selector: 'ia-document-popup',
@@ -11,11 +12,30 @@ import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 })
 export class DocumentPopupComponent implements OnChanges {
     @Input() page: DocumentPage;
+    @Input() queryModel: QueryModel;
 
     visible = true;
 
     faArrowLeft = faArrowLeft;
     faArrowRight = faArrowRight;
+    linkIcon = faLink;
+    contextIcon = faBookOpen;
+
+    get documentPageLink(): string[] {
+        if (this.focusedDocument) {
+            return ['/document', this.focusedDocument.corpus.name, this.focusedDocument.id];
+        }
+    }
+
+    get contextDisplayName(): string {
+        if (this.focusedDocument.corpus.documentContext) {
+            return this.focusedDocument.corpus.documentContext.displayName;
+        }
+    }
+
+    private get focusedDocument(): FoundDocument {
+        return this.page?.focus$.value;
+    }
 
     ngOnChanges(changes: SimpleChanges): void {
         if (changes.page) {
