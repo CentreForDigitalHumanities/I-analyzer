9f23a3b88a4c4157ad35a3b146dccd7b927f902e
Luka van der Plas <lukavdplas@gmail.com>
2023-11-08 15:56:12 +0100

    polish styling and keyboard interactions

--- frontend/src/app/tag/tag.module.ts
+++ frontend/src/app/tag/tag.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { TagSelectComponent } from './tag-select/tag-select.component';
 import { DocumentTagsComponent } from './document-tags/document-tags.component';
-import { DropdownModule } from 'primeng/dropdown';
 
 
 
 @NgModule({
     declarations: [DocumentTagsComponent, TagSelectComponent],
-    imports: [SharedModule, DropdownModule],
+    imports: [SharedModule],
     exports: [DocumentTagsComponent],
 })
 export class TagModule {}
