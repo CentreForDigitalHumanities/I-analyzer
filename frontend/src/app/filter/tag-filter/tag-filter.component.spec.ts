d73226f1797a25d415c8b4b6e886389ef4451b2e
Luka van der Plas <lukavdplas@gmail.com>
2023-11-09 14:32:08 +0100

    generate tag filter component

---
+++ frontend/src/app/filter/tag-filter/tag-filter.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { TagFilterComponent } from './tag-filter.component';
+import { commonTestBed } from 'src/app/common-test-bed';
+
+describe('TagFilterComponent', () => {
+    let component: TagFilterComponent;
+    let fixture: ComponentFixture<TagFilterComponent>;
+
+    beforeEach(waitForAsync(() => {
+        commonTestBed().testingModule.compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TagFilterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
