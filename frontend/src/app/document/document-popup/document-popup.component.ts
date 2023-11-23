267441cd4a8058af36377e2e1de46b6f2ebe47b4
Luka van der Plas <lukavdplas@gmail.com>
2023-11-23 12:22:19 +0100

    fix leaky subscription

--- frontend/src/app/document/document-popup/document-popup.component.ts
+++ frontend/src/app/document/document-popup/document-popup.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { DocumentPage, DocumentView } from '../../models/document-page';
-import { filter } from 'rxjs/operators';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
+import { DocumentFocus, DocumentPage, DocumentView } from '../../models/document-page';
+import { filter, takeUntil } from 'rxjs/operators';
 import * as _ from 'lodash';
 import { faArrowLeft, faArrowRight, faBookOpen, faLink } from '@fortawesome/free-solid-svg-icons';
 import { FoundDocument, QueryModel } from '../../models';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'ia-document-popup',
     templateUrl: './document-popup.component.html',
     styleUrls: ['./document-popup.component.scss']
 })
-export class DocumentPopupComponent implements OnChanges {
+export class DocumentPopupComponent implements OnChanges, OnDestroy {
     @Input() page: DocumentPage;
     @Input() queryModel: QueryModel;
 
@@ -23,6 +24,8 @@ export class DocumentPopupComponent implements OnChanges {
     faArrowRight = faArrowRight;
     linkIcon = faLink;
     contextIcon = faBookOpen;
+
+    private refresh$ = new Subject<void>();
 
     get documentPageLink(): string[] {
         if (this.document) {
@@ -38,13 +41,27 @@ export class DocumentPopupComponent implements OnChanges {
 
     ngOnChanges(changes: SimpleChanges): void {
         if (changes.page) {
+            this.refresh$.next();
+
             this.page.focus$.pipe(
-                filter(focus => !!focus),
-            ).subscribe(focus => {
-                this.document = focus.document;
-                this.view = focus.view;
-                this.visible = true;
-            });
+                takeUntil(this.refresh$),
+            ).subscribe(this.focusUpdate.bind(this));
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.refresh$.next();
+        this.refresh$.complete();
+    }
+
+    focusUpdate(focus?: DocumentFocus): void {
+        if (focus) {
+            this.document = focus.document;
+            this.view = focus.view;
+            this.visible = true;
+        } else {
+            this.document = undefined;
+            this.visible = false;
         }
     }
 }
