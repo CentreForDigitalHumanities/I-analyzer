4917aa51c67636c11ca8f01dcedbdbe122f1756d
Luka van der Plas <lukavdplas@gmail.com>
2023-11-25 11:54:06 +0100

    render vega chart

--- frontend/src/app/visualization/vega/vega.component.ts
+++ frontend/src/app/visualization/vega/vega.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { View, ViewOptions, parse } from 'vega';
 
 @Component({
     selector: 'ia-vega',
@@ -10,6 +11,21 @@ export class VegaComponent implements OnInit {
     constructor() { }
 
     ngOnInit(): void {
+        const request = fetch('https://vega.github.io/vega/examples/bar-chart.vg.json')
+            .then(res => res.json())
+            .then(spec => this.render(spec));
     }
 
+    render(spec): Promise<View> {
+        const options: ViewOptions = {
+            renderer:  'svg',  // renderer (canvas or svg)
+            container: '#chart',   // parent DOM container
+            hover:     true       // enable hover processing
+        };
+        const view = new View(
+            parse(spec),
+            options
+        );
+        return view.runAsync();
+    }
 }
