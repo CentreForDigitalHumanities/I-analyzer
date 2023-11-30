6b47da709f301fba9a7445a393db8b9a7970a9ec
BeritJanssen <berit.janssen@gmail.com>
2023-11-30 11:43:24 +0100

    close #1343: save user before adding group

--- backend/users/saml.py
+++ backend/users/saml.py
@@ -5,10 +5,11 @@ from django.conf import settings
 class CustomSaml2Backend(Saml2Backend):
     def get_or_create_user(self, *args, **kwargs):
         user, created = super().get_or_create_user(*args, **kwargs)
-        user.saml = True
 
         saml_group = saml_user_group()
-        if saml_group:
+        if created and saml_group:
+            user.saml = True
+            user.save()
             user.groups.add(saml_group)
 
         return user, created
