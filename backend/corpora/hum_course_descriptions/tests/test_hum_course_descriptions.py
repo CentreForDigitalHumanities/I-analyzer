b21e5f47a1efc7d909ff15bda6581678a91d7a42
Luka van der Plas <lukavdplas@gmail.com>
2023-12-04 17:26:57 +0100

    set up import test

--- backend/corpora/hum_course_descriptions/tests/test_hum_course_descriptions.py
+++ backend/corpora/hum_course_descriptions/tests/test_hum_course_descriptions.py
@@ -3,6 +3,7 @@ import pytest
 
 from corpora.utils_test import corpus_from_api
 from addcorpus.save_corpus import load_and_save_all_corpora
+from addcorpus.load_corpus import load_corpus_definition
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,8 +12,16 @@ def hum_course_descriptions_settings(settings, db):
     settings.CORPORA = {
         'hum_course_descriptions': os.path.join(here, '../hum_course_descriptions.py')
     }
-    settings.HUM_COURSE_DESCRIPTIONS_DATA = ''
+    settings.HUM_COURSE_DESCRIPTIONS_DATA = os.path.join(here, 'data')
 
 def test_hum_course_descriptions_model(hum_course_descriptions_settings, db, admin_client):
     corpus = corpus_from_api(admin_client)
     assert corpus['title'] == 'Humanities Course Descriptions'
+
+def test_hum_course_descriptions_extraction(hum_course_descriptions_settings):
+    load_and_save_all_corpora()
+    corpus = load_corpus_definition('hum_course_descriptions')
+    assert corpus
+
+    docs = corpus.documents()
+    assert len(docs) == 1
