4301b89f0f2277c12733a017bf717aa2d396a6bc
Luka van der Plas <lukavdplas@gmail.com>
2023-12-04 18:23:13 +0100

    draft data extraction

--- backend/corpora/hum_course_descriptions/hum_course_descriptions.py
+++ backend/corpora/hum_course_descriptions/hum_course_descriptions.py
@@ -5,8 +5,20 @@ from django.conf import settings
 from addcorpus.corpus import FieldDefinition
 from addcorpus.xlsx import XLSXCorpusDefinition
 from addcorpus.es_settings import es_settings
-from addcorpus.es_mappings import text_mapping, main_content_mapping, keyword_mapping
-from addcorpus.extract import CSV
+from addcorpus.es_mappings import text_mapping, main_content_mapping, keyword_mapping, int_mapping
+from addcorpus.extract import CSV, Combined
+
+def filter_label(label):
+    def get_content_with_label(data):
+        labels, contents = data
+        get_label = lambda pair: pair[0]
+        get_content = lambda pair: pair[1]
+        has_label = lambda pair: get_label(pair) == label
+        filtered = filter(has_label, zip(labels, contents))
+        filtered_content = map(get_content, filtered)
+        return '\n'.join(filtered_content)
+
+    return get_content_with_label
 
 class HumCourseDescriptions(XLSXCorpusDefinition):
     title = 'Humanities Course Descriptions'
@@ -31,6 +43,62 @@ class HumCourseDescriptions(XLSXCorpusDefinition):
         FieldDefinition(
             name='id',
             display_name='Course ID',
-            extractor=CSV('CURSUS')
-        )
+            extractor=CSV('CURSUS'),
+            es_mapping=keyword_mapping(),
+        ),
+        FieldDefinition(
+            name='academic_year',
+            display_name='Academic year',
+            extractor=CSV('COLLEGEJAAR'),
+            es_mapping=int_mapping(),
+        ),
+        FieldDefinition(
+            name='name',
+            display_name='Name',
+            extractor=CSV('KORTE_NAAM_NL'),
+            es_mapping=text_mapping(),
+        ),
+        FieldDefinition(
+            name='type',
+            display_name='Course type',
+            extractor=CSV('CURSUSTYPE'),
+            es_mapping=keyword_mapping(),
+        ),
+        FieldDefinition(
+            name='department',
+            display_name='Coordinating department',
+            extractor=CSV('COORDINEREND_ONDERDEEL'),
+            es_mapping=keyword_mapping(),
+        ),
+        FieldDefinition(
+            name='description',
+            display_name='Description',
+            extractor=CSV('OMSCHRIJVING'),
+            es_mapping=keyword_mapping(enable_full_text_search=True),
+        ),
+        FieldDefinition(
+            name='faculty',
+            display_name='Faculty',
+            extractor=CSV('FACULTEIT'),
+            es_mapping=keyword_mapping(),
+        ),
+        FieldDefinition(
+            name='goal',
+            display_name='Course goal',
+            extractor=Combined(
+                CSV('LABEL', multiple=True),
+                CSV('INHOUD', multiple=True),
+                transform=filter_label('DOEL')
+            ),
+            es_mapping=main_content_mapping(),
+        ),
+        FieldDefinition(
+            name='content',
+            display_name='Course content',
+            extractor=Combined(
+                CSV('LABEL', multiple=True),
+                CSV('INHOUD', multiple=True),
+                transform=filter_label('INHOUD')
+            ),
+        ),
     ]
