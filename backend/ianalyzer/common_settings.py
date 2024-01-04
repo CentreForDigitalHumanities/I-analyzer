519fc2ce2c9377fd61e7ef6efe959466d08d8913
Jelte van Boheemen <j.vanboheemen@uu.nl>
2024-01-04 13:39:18 +0100

    Update dj-rest-auth configuration
    Resolves #1366

--- backend/ianalyzer/common_settings.py
+++ backend/ianalyzer/common_settings.py
@@ -125,7 +125,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
 AUTH_USER_MODEL = 'users.CustomUser'
 
-REST_AUTH = {
+REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'users.serializers.CustomUserDetailsSerializer',
     'REGISTER_SERIALIZER': 'users.serializers.CustomRegistrationSerializer',
 }
