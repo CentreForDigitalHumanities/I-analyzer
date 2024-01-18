402eabfae956478d930e6c897d6e29714ddc658b
Luka van der Plas <lukavdplas@mailfence.com>
2024-01-18 16:34:32 +0100

    update QueryParamManager clarity

--- frontend/src/app/utils/routing.ts
+++ frontend/src/app/utils/routing.ts
@@ -3,50 +3,50 @@ import * as _ from 'lodash';
 import { Observable, Observer } from 'rxjs';
 import { distinct, filter, map, take, takeUntil, withLatestFrom } from 'rxjs/operators';
 
-const isDistinct = ([a, b]) => !_.isEqual(a, b);
+const areDistinct = ([a, b]) => !_.isEqual(a, b);
 
 const mergeCurrentIntoPushing = ([current, pushing]) =>
     [current, _.extend(current, pushing)];
 
 export class QueryParamManager<State> {
     constructor(
+        /** signals when the model's state updates */
         private stateUpdate$: Observable<State>,
+        /** signals when the route parameters have changed */
         private paramUpdate$: Observable<Params>,
+        /** observer to which you can push changes to the route parameters */
         private paramsObserver$: Observer<Params>,
+        /** function converting the model state to parameters */
         private paramsFromState: (stateUpdate: State) => Params,
+        /** function updating the model state from parameters */
         private updateStateFromParams: (params: Params) => void,
+        /** signals when the model is destroyed */
         private complete$: Observable<void>,
+        /** keys in the route parameters that the model syncs with */
         private relevantKeys: string[],
-        resetOnComplete = true,
     ) {
         this.subscribeRouteToState();
         this.subscribeStateToRoute();
-
-        if (resetOnComplete) {
-            this.resetOnComplete();
-        }
+        this.resetOnComplete();
     }
 
     /** subscribe to state update and send updates to the route */
     private subscribeRouteToState() {
-        // observable of parameters pushed by state updates
-        const paramsFromState$ = this.stateUpdate$.pipe(
+        const paramPushes$ = this.stateUpdate$.pipe(
             takeUntil(this.complete$),
+
+            // convert state to parameters
             map(this.paramsFromState),
-        );
 
-
-
-        const paramPushes$ = paramsFromState$.pipe(
             // merge the data pushed by the state  update with the latest route data
             withLatestFrom(this.paramUpdate$),
             map(mergeCurrentIntoPushing),
 
-            // filter cases where the updated data is distinct from the current data
-            filter(isDistinct),
+            // only emit if the new data is distinct from the current data
+            filter(areDistinct),
 
-            // select the updated data
-            map(_.last)
+            // output new new data
+            map(_.last),
         );
 
         paramPushes$.subscribe(params => this.paramsObserver$.next(params));
@@ -55,7 +55,7 @@ export class QueryParamManager<State> {
     /** subscribe to route updates and send updates to the state */
     private subscribeStateToRoute() {
         const stateIsOutdated = ([params, state]) =>
-            isDistinct([params, this.paramsFromState(state)]);
+            areDistinct([params, this.paramsFromState(state)]);
 
         const statePushes$ = this.paramUpdate$.pipe(
             takeUntil(this.complete$),
@@ -70,28 +70,35 @@ export class QueryParamManager<State> {
             withLatestFrom(this.stateUpdate$),
             filter(stateIsOutdated),
 
-            // take the parameters
-            map(_.first)
+            // output only the state update
+            map(_.first),
         );
 
         statePushes$.subscribe(params => this.updateStateFromParams(params));
     }
 
     private resetOnComplete() {
-        const nullParams = Object.assign({}, ...this.relevantKeys.map(f => ({ [f]: null })));
-
         this.complete$.pipe(
             // only need to do this once
             take(1),
 
             // signal null parameters
-            map(() => nullParams),
+            map(() => this.nullParams()),
 
-            // merge null parameters into latests parameters
+            // merge null parameters into latest parameters
             withLatestFrom(this.paramUpdate$),
             map(mergeCurrentIntoPushing),
         ).subscribe(params =>
             this.paramsObserver$.next(params)
         );
+    }
+
+    /**
+     * object with null value for each relevant parameters
+     *
+     * used to reset paramets on completion
+     */
+    private nullParams() {
+        return Object.assign({}, ...this.relevantKeys.map(f => ({ [f]: null })));
     }
 };
