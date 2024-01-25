0eca684f0498acb9908b7a72766fc6011cf36281
Luka van der Plas <lukavdplas@mailfence.com>
2024-01-25 14:53:59 +0100

    fix url pattern

--- backend/tag/urls.py
+++ backend/tag/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from .views import DocumentTagsView
 
 urlpatterns = [
-    re_path(r'^document_tags/(?P<corpus>[A-Za-z\-_]+)/(?P<doc_id>).+$', DocumentTagsView.as_view())
+    re_path(r'^document_tags/(?P<corpus>[A-Za-z\-_]+)/(?P<doc_id>.+)$', DocumentTagsView.as_view())
 ]
