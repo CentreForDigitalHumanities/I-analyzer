6c2aeb74213852a256effcbab1c01ef98f9740d1
Luka van der Plas <lukavdplas@mailfence.com>
2024-02-15 16:52:01 +0100

    remove spec files

--- frontend/src/app/dropdown/dropdown-item.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DropdownItemDirective } from './dropdown-item.directive';
-
-describe('DropdownItemDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DropdownItemDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- frontend/src/app/dropdown/dropdown-menu.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DropdownMenuDirective } from './dropdown-menu.directive';
-
-describe('DropdownMenuDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DropdownMenuDirective();
-    expect(directive).toBeTruthy();
-  });
-});
