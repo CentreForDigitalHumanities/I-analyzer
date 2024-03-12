9ba287cce209435869a93c0d04cf96b415236662
Luka van der Plas <lukavdplas@mailfence.com>
2024-03-12 15:47:26 +0100

    fix sort direction reset on page load

--- frontend/src/app/models/page-results.ts
+++ frontend/src/app/models/page-results.ts
@@ -77,13 +77,22 @@ export class PageResults extends Results<PageResultsParameters, DocumentPage> {
         );
     }
 
+    /**
+     * change the field by which results are sorted, and reset the sort direction and page
+     */
     setSortBy(value: SortBy) {
-        this.setParams({
-            sort: [value, 'desc'],
-            from: 0,
-        });
+        // check if the new value is actually different to avoid resetting the direction
+        // for nothing
+        const currentValue = this.state$.value.sort[0];
+        if (value?.name !== currentValue?.name) {
+            this.setParams({
+                sort: [value, 'desc'],
+                from: 0,
+            });
+        }
     }
 
+    /** change the sort direction and reset the page */
     setSortDirection(value: SortDirection) {
         const [sortBy, _] = this.state$.value.sort;
         this.setParams({
