b4656c7bc524be0fd021df1ef5afb1498d998ab9
Luka van der Plas <lukavdplas@mailfence.com>
2024-03-20 14:49:43 +0100

    import options

--- backend/addcorpus/json_corpora/import_json.py
+++ backend/addcorpus/json_corpora/import_json.py
@@ -21,10 +21,13 @@ def import_configuration(data: Dict) -> CorpusConfiguration:
     description = get_path(data, 'meta', 'description')
     category = get_path(data, 'meta', 'category')
     es_index = get_path(data, 'name')
-    image = 'missing.png'
+    image = 'missing.png' # TODO: fix this
     languages = get_path(data, 'meta', 'languages')
     min_date = parse_date(get_path(data, 'meta', 'date_range', 'min'))
     max_date = parse_date(get_path(data, 'meta', 'date_range', 'max'))
+    default_sort = get_path(data, 'options', 'default_sort') or {}
+    language_field = get_path(data, 'options', 'language_field') or ''
+    document_context = get_path(data, 'options', 'document_context') or {}
     return CorpusConfiguration(
         title=title,
         description=description,
@@ -34,7 +37,11 @@ def import_configuration(data: Dict) -> CorpusConfiguration:
         languages=languages,
         min_date=min_date,
         max_date=max_date,
+        default_sort=default_sort,
+        language_field=language_field,
+        document_context=document_context,
     )
 
 def parse_date(date: str):
     return datetime.strptime(date, '%Y-%m-%d').date()
+
