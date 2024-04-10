3d35c040083c82998d7ab196d69071bdc8fc0be4
Jelte van Boheemen <j.vanboheemen@uu.nl>
2024-04-10 17:03:40 +0200

    Expand reader function for json corpora

--- backend/addcorpus/readers/reader.py
+++ backend/addcorpus/readers/reader.py
@@ -1,10 +1,23 @@
-from addcorpus.models import Corpus
-from ianalyzer_readers.readers.core import Reader
+import glob
 
+from addcorpus.json_corpora.utils import get_path
+from addcorpus.models import Corpus, Field
 from addcorpus.python_corpora.load_corpus import load_corpus_definition
+from ianalyzer_readers.extract import CSV
+from ianalyzer_readers.readers.core import Field as ReaderField
+from ianalyzer_readers.readers.core import Reader
+from ianalyzer_readers.readers.csv import CSVReader
 
 
-def make_reader(corpus: Corpus) -> Reader:
+def make_reader_field(corpus_field: Field) -> ReaderField:
+    col = get_path(corpus_field.extract_options, 'column')
+    return ReaderField(
+        name=corpus_field.name,
+        extractor=CSV(column=col)
+    )
+
+
+def make_reader(corpus: Corpus, data_directory: str = None) -> Reader:
     '''
     From a corpus, returns a Reader object that allows source extraction
 
@@ -13,4 +26,13 @@ def make_reader(corpus: Corpus) -> Reader:
     '''
     if corpus.has_python_definition:
         return load_corpus_definition(corpus.name)
-    raise NotImplementedError('Only supports python corpora')
+
+    reader = CSVReader()
+    reader.delimiter = get_path(
+        corpus.source_data, 'options', 'delimiter') or ','
+
+    reader.fields = [make_reader_field(f)
+                     for f in corpus.configuration.fields.all()]
+    reader.sources = lambda _: glob.glob(f'{data_directory}/**/*.csv')
+
+    return reader
