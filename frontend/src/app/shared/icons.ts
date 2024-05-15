e41db24310ea37e4c562b35a148c6610261efddc
Luka van der Plas <lukavdplas@mailfence.com>
2024-05-15 16:47:43 +0200

    add corpus definitions link

--- frontend/src/app/shared/icons.ts
+++ frontend/src/app/shared/icons.ts
@@ -7,7 +7,7 @@ import {
     faAngleDown, faAngleUp, faArrowLeft, faArrowRight, faAt, faBook, faBookOpen, faChartColumn,
     faCheck, faChevronDown, faChevronLeft, faChevronRight, faCog, faCogs, faDatabase, faDiagramProject,
     faDownload, faEdit, faEnvelope, faEye, faFilter, faHistory, faImage, faInfo, faInfoCircle, faLink, faList, faLock,
-    faMinus, faPalette, faPlus, faQuestionCircle, faSearch, faSearchMinus, faSearchPlus, faSignOut,
+    faMinus, faPalette, faPencil, faPlus, faQuestionCircle, faSearch, faSearchMinus, faSearchPlus, faSignOut,
     faSortAlphaAsc, faSortAlphaDesc, faSortNumericAsc, faSortNumericDesc, faSquare,
     faTable, faTags, faTimes, faTrashCan, faUndo, faUser
 } from '@fortawesome/free-solid-svg-icons';
@@ -52,7 +52,7 @@ export const actionIcons: Icons = {
     add: faPlus,
     remove: faTimes,
     delete: faTrashCan,
-    edit: faEdit,
+    edit: faPencil,
     view: faEye,
 };
 
