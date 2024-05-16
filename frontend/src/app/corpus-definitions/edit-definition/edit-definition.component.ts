c9a634039eeda40a196451f1a82ce0199d3954c4
Luka van der Plas <lukavdplas@mailfence.com>
2024-05-16 11:53:45 +0200

    use router state in edit definition component

--- frontend/src/app/corpus-definitions/edit-definition/edit-definition.component.ts
+++ frontend/src/app/corpus-definitions/edit-definition/edit-definition.component.ts
@@ -1,18 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { actionIcons } from '../../shared/icons';
+import { Observable, combineLatest } from 'rxjs';
+import { APICorpusDefinition } from '../../models/corpus-definition';
+import { ApiService } from '../../services';
+import { ActivatedRoute } from '@angular/router';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'ia-edit-definition',
     templateUrl: './edit-definition.component.html',
     styleUrls: ['./edit-definition.component.scss']
 })
-export class EditDefinitionComponent implements OnInit {
+export class EditDefinitionComponent {
 
     actionIcons = actionIcons;
 
-    constructor() { }
+    definition$: Observable<APICorpusDefinition>;
 
-    ngOnInit(): void {
+    constructor(
+        private apiService: ApiService,
+        private route: ActivatedRoute
+    ) {
+        const corpusID$ = this.route.params.pipe(
+            map(params => parseInt(params['corpusID'], 10))
+        );
+        this.definition$ = combineLatest([
+            this.apiService.corpusDefinitions(),
+            corpusID$,
+        ]).pipe(
+            tap(values => console.log(values)),
+            map(([definitions, id]) => definitions.find(def => def.id === id))
+        );
     }
 
 }
