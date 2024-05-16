ce40ca380366ef5b91e97930cdb7ea1e1b6aea0c
BeritJanssen <berit.janssen@gmail.com>
2024-05-16 15:24:56 +0200

    fix: remove duplicate word_model_path in Canada corpus definition

--- backend/corpora/parliament/canada.py
+++ backend/corpora/parliament/canada.py
@@ -26,7 +26,6 @@ class ParliamentCanada(Parliament, CSVCorpusDefinition):
     required_field = 'content'
 
     document_context = document_context(sort_field=None)
-    word_model_path = getattr(settings, 'PP_CA_WM', None)
 
     def sources(self, start, end):
         logger = logging.getLogger('indexing')
