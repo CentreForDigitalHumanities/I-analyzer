f1b4c9b85576dfc1f0f7f32cd1c59f80f9dd48d9
Luka van der Plas <lukavdplas@mailfence.com>
2024-06-05 14:49:04 +0200

    use sections for manual pages

--- frontend/src/app/manual/manual-navigation.component.ts
+++ frontend/src/app/manual/manual-navigation.component.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import {
     DialogService,
-    ManualPageMetaData,
+    ManualSection,
 } from '../services';
 import { actionIcons, navIcons } from '../shared/icons';
 
@@ -16,7 +16,7 @@ export class ManualNavigationComponent implements OnInit {
     navIcons = navIcons;
     actionIcons = actionIcons;
 
-    manifest = new Subject<ManualPageMetaData[]>();
+    manifest = new Subject<ManualSection[]>();
 
     constructor(
         private dialogService: DialogService
