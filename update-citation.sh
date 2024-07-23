053f1e4391f7bbddda374af5d77b74d3d6c50c1f
Jelte van Boheemen <j.vanboheemen@uu.nl>
2024-07-23 18:56:31 +0200

    Python instead of shell for updating citation

--- update-citation.sh
+++
@@ -1,4 +0,0 @@
-version=`grep -o '\d\+\.\d\+\.\d\+' package.json`
-today=`date +'%Y-%m-%d'`
-sed -i '' "s/^version:.*$/version: $version/g" CITATION.cff
-sed -i '' "s/^date-released:.*$/date-released: \'$today\'/g" CITATION.cff
