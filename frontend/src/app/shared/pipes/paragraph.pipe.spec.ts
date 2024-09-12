487d509fbe3d6f798ad203b574940368216d693f
BeritJanssen <berit.janssen@gmail.com>
2024-09-12 10:09:16 +0200

    add tests for paragraph pipe

--- frontend/src/app/shared/pipes/paragraph.pipe.spec.ts
+++ frontend/src/app/shared/pipes/paragraph.pipe.spec.ts
@@ -1,8 +1,24 @@
 import { ParagraphPipe } from './paragraph.pipe';
 
 describe('ParagraphPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ParagraphPipe();
-    expect(pipe).toBeTruthy();
-  });
+    it('creates an instance', () => {
+      const pipe = new ParagraphPipe();
+      expect(pipe).toBeTruthy();
+    });
+
+    it('does not alter text without linebreaks', () => {
+        const pipe = new ParagraphPipe();
+        const input = 'Some text. And some more text. And even more.';
+        const output = pipe.transform(input);
+        expect(output).toEqual(input);
+    });
+
+    it('wraps text with linebreaks in paragraph tags', () => {
+        const pipe = new ParagraphPipe();
+        const input = 'Some text.\nAnd some more text.\nAnd even more.';
+        const output = pipe.transform(input);
+        const expected = '<p>Some text.</p><p>And some more text.</p><p>And even more.</p>'
+        expect(output).toEqual(expected);
+    });
+
 });
