64bb540ec6f3f7024cff591e556d796aea8191e5
Luka van der Plas <lukavdplas@mailfence.com>
2024-11-01 13:11:45 +0100

    show index metadata

--- backend/es/admin.py
+++ backend/es/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from es import models
 
 class IndexAdmin(admin.ModelAdmin):
-    readonly_fields = ['name', 'server', 'available']
+    readonly_fields = ['name', 'server', 'available', 'aliases', 'settings',  'mappings', 'creation_date']
     list_display = ['name', 'server', 'available']
     search_fields = ['name']
     list_filter = ['server', 'available']
--- backend/es/models.py
+++ backend/es/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.conf import settings
+from typing import Optional, Dict, List
+from datetime import datetime
+
 from addcorpus import models as corpus_models
+from ianalyzer.elasticsearch import client_from_config
+
 
 class Index(models.Model):
     '''
@@ -27,3 +33,32 @@ class Index(models.Model):
             return self.name
         else:
             return f'{self.name} ({self.server})'
+
+    def settings(self) -> Optional[Dict]:
+        client = self.client()
+        if client:
+            response = client.indices.get_settings(index=self.name)
+            return response[self.name]['settings']
+
+    def mappings(self) -> Optional[Dict]:
+        client = self.client()
+        if client:
+            response = client.indices.get_mapping(index=self.name)
+            return response[self.name]['mappings']
+
+    def aliases(self) -> Optional[List[str]]:
+        client = self.client()
+        if client:
+            response = client.indices.get_alias(index=self.name)
+            return list(response[self.name]['aliases'].keys())
+
+    def creation_date(self) -> Optional[datetime]:
+        settings = self.settings()
+        if settings:
+            timestamp = int(settings['index']['creation_date'])
+            return datetime.fromtimestamp(timestamp/1000)
+
+    def client(self):
+        if self.available:
+            config = settings.SERVERS[self.server]
+            return client_from_config(config)
