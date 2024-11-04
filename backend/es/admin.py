75daa19accb69037e7be91c8c56bdcada1c90f34
Luka van der Plas <lukavdplas@mailfence.com>
2024-11-04 13:42:37 +0100

    show servers in admin

--- backend/es/admin.py
+++ backend/es/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from es import models
 
+class ServerAdmin(admin.ModelAdmin):
+    readonly_fields = ['configuration', 'can_connect']
+    list_display = ['name', 'active']
+
+    def has_add_permission(self, request):
+        # disable creating servers manually
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        # disable editing
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        # only inactive servers can be deleted
+        if obj and obj.active:
+            return False
+        return super().has_delete_permission(request, obj)
+
 class IndexAdmin(admin.ModelAdmin):
     readonly_fields = ['aliases', 'settings',  'mappings', 'creation_date']
     list_display = ['name', 'server', 'available']
@@ -12,7 +30,7 @@ class IndexAdmin(admin.ModelAdmin):
         return False
 
     def has_change_permission(self, request, obj=None):
-        # disable editing index data
+        # disable editing
         return False
 
     def has_delete_permission(self, request, obj=None):
@@ -22,4 +40,5 @@ class IndexAdmin(admin.ModelAdmin):
         return super().has_delete_permission(request, obj)
 
 
+admin.site.register(models.Server, ServerAdmin)
 admin.site.register(models.Index, IndexAdmin)
