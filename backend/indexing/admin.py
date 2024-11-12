1718402e3040ffffd1391fc34326b04d6bf305a9
Luka van der Plas <lukavdplas@mailfence.com>
2024-11-12 15:31:08 +0100

    add admin interface

--- backend/indexing/admin.py
+++ backend/indexing/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
-# Register your models here.
+from indexing import models
+
+
+class CreateIndexAdmin(admin.StackedInline):
+    model = models.CreateIndexTask
+    extra = 0
+
+
+class PopulateIndexAdmin(admin.StackedInline):
+    model = models.PopulateIndexTask
+    extra = 0
+
+
+class UpdateIndexAdmin(admin.StackedInline):
+    model = models.UpdateIndexTask
+    extra = 0
+
+
+class UpdateSettingsAdmin(admin.StackedInline):
+    model = models.UpdateSettingsTask
+    extra = 0
+
+
+class RemoveAliasAdmin(admin.StackedInline):
+    model = models.RemoveAliasTask
+    extra = 0
+
+
+class AddAliasAdmin(admin.StackedInline):
+    model = models.AddAliasTask
+    extra = 0
+
+
+class DeleteIndexAdmin(admin.StackedInline):
+    model = models.DeleteIndexTask
+    extra = 0
+
+
+
+class IndexJobAdmin(admin.ModelAdmin):
+    list_display = ['created', 'corpus']
+    list_filter = ['corpus']
+    inlines = [
+        CreateIndexAdmin,
+        PopulateIndexAdmin,
+        UpdateIndexAdmin,
+        UpdateSettingsAdmin,
+        RemoveAliasAdmin,
+        AddAliasAdmin,
+        DeleteIndexAdmin,
+    ]
+
+admin.site.register(models.IndexJob, IndexJobAdmin)
