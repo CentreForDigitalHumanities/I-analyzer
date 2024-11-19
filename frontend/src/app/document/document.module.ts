356ff07916e7e4893a6dec87911d7bd88b7eeda6
Luka van der Plas <lukavdplas@mailfence.com>
2024-11-19 16:37:41 +0100

    move document-view component

--- frontend/src/app/document/document.module.ts
+++ frontend/src/app/document/document.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
-import { DocumentViewComponent } from '../document-view/document-view.component';
+import { DocumentViewComponent } from './document-view/document-view.component';
 import { DocumentPageComponent } from '../document-page/document-page.component';
 import { ImageViewModule } from '../image-view/image-view.module';
 import { SearchRelevanceComponent } from '../search';
