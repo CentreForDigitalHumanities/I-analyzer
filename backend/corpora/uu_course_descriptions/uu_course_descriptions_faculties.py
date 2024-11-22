97aa44253523ba643d76e0155bfcc8b7980f82d6
Luka van der Plas <lukavdplas@mailfence.com>
2024-11-22 13:35:00 +0100

    add proof-of-concept subcorpora for faculties

---
+++ backend/corpora/uu_course_descriptions/uu_course_descriptions_faculties.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+
+from corpora.uu_course_descriptions.uu_course_descriptions import UUCourseDescriptions, FACULTIES
+
+def faculty_filter(faculty):
+    return lambda doc: doc['faculty'] == FACULTIES[faculty]
+
+
+class UUCourseDescriptionsGW(UUCourseDescriptions):
+    title = 'Course Descriptions: Humanities faculty'
+    description = 'Courses taught at the Humanities faculty in 2024-2025'
+    image = 'uu_gw.jpg'
+    es_index =  getattr(settings, 'UU_COURSE_DESCRIPTIONS_GW_INDEX', 'uu_course_descriptions_gw')
+
+    def source2dicts(self, source):
+        all_docs = super().source2dicts(source)
+        return filter(faculty_filter('GW'), all_docs)
+
+
+class UUCourseDescriptionsBETA(UUCourseDescriptions):
+    title = 'Course Descriptions: Science faculty'
+    description = 'Courses taught at the Faculty of Science in 2024-2025'
+    image = 'uu_beta.jpg'
+    languages = ['nl', 'en']
+    es_index =  getattr(settings, 'UU_COURSE_DESCRIPTIONS_BETA_INDEX', 'uu_course_descriptions_beta')
+
+    def source2dicts(self, source):
+        all_docs = super().source2dicts(source)
+        return filter(faculty_filter('BETA'), all_docs)
