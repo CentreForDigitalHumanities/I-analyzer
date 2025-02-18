6d72c23276d658fd45772fe1895d837668626e40
Luka van der Plas <lukavdplas@mailfence.com>
2025-02-18 18:23:23 +0100

    move highest version test, add test

--- backend/indexing/tests/test_alias.py
+++ backend/indexing/tests/test_alias.py
@@ -1,7 +1,6 @@
 from addcorpus.models import Corpus
 from indexing.create_job import create_alias_job
 from indexing.run_job import perform_indexing
-from es.versioning import highest_version_in_result
 
 
 def test_alias(db, es_alias_client):
@@ -23,16 +22,3 @@ def test_alias_with_clean(es_alias_client):
     indices = es_alias_client.indices.get(
         index='{}-*'.format(corpus.configuration.es_index))
     assert 'test-times-1' not in list(indices.keys())
-
-
-def test_highest_version_number(es_alias_client):
-    corpus = Corpus.objects.get(name='times')
-    indices = es_alias_client.indices.get(
-        index='{}-*'.format(corpus.configuration.es_index))
-    current_index = 'test-times'
-    num = highest_version_in_result(indices, current_index)
-    assert num == 2
-    current_index = "fantasy-index-name"
-    indices = es_alias_client.indices.get(index="{}-*".format(current_index))
-    num = highest_version_in_result(indices, current_index)
-    assert num == 0
