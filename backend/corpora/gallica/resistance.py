434c8083826e1f82c88e866bdea56b17f9d82eea
BeritJanssen <berit.janssen@gmail.com>
2025-03-05 16:39:05 +0100

    experimentally adjust min_date

--- backend/corpora/gallica/resistance.py
+++ backend/corpora/gallica/resistance.py
@@ -9,7 +9,7 @@ from corpora.gallica.gallica import Gallica
 class Resistance(Gallica):
     title = "Journaux clandestins de la Resistance"
     description = "Collection of underground journals during and after German occupation, 1940-1954"
-    min_date = datetime(year=1940, month=1, day=1)
+    min_date = datetime(year=1938, month=1, day=1)
     max_date = datetime(year=1960, month=12, day=31)
     publication_ids = [
         # Bulletins
