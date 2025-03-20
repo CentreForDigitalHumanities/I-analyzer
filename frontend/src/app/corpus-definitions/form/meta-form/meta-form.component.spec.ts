90adcea9955968e99edfc57c8ce0d89af5228e67
BeritJanssen <berit.janssen@gmail.com>
2025-03-20 11:21:38 +0100

    fix warnings in meta-form component unit test

--- frontend/src/app/corpus-definitions/form/meta-form/meta-form.component.spec.ts
+++ frontend/src/app/corpus-definitions/form/meta-form/meta-form.component.spec.ts
@@ -1,21 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MetaFormComponent } from './meta-form.component';
-import { CorpusDefinitionService } from 'app/corpus-definitions/corpus-definition.service';
-import { SlugifyPipe } from '@shared/pipes/slugify.pipe';
+import { ApiService } from '@services';
+import { CorpusDefinition } from '@models/corpus-definition';
+import { mockCorpusDefinition } from 'mock-data/corpus-definition';
+import { commonTestBed } from 'app/common-test-bed';
 
 describe('MetaFormComponent', () => {
     let component: MetaFormComponent;
     let fixture: ComponentFixture<MetaFormComponent>;
+    let apiService: ApiService;
 
     beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [MetaFormComponent],
-            providers: [CorpusDefinitionService, SlugifyPipe],
-        }).compileComponents();
-
+        commonTestBed().testingModule.compileComponents();
+        apiService = TestBed.inject(ApiService);
         fixture = TestBed.createComponent(MetaFormComponent);
         component = fixture.componentInstance;
+        const corpus = new CorpusDefinition(apiService);
+        corpus.setFromDefinition(mockCorpusDefinition);
+        component.corpus = corpus;
         fixture.detectChanges();
     });
 
