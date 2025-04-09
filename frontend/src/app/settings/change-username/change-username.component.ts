b5d860bf4afa4232afd7ea1e0632ccf12582cff0
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-09 13:27:30 +0200

    update name, show feedback

--- frontend/src/app/settings/change-username/change-username.component.ts
+++ frontend/src/app/settings/change-username/change-username.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@services';
 import { formIcons } from '@shared/icons';
+import { map } from 'rxjs';
 
 @Component({
     selector: 'ia-change-username',
@@ -7,7 +9,36 @@ import { formIcons } from '@shared/icons';
     styleUrl: './change-username.component.scss'
 })
 export class ChangeUsernameComponent {
+    currentName$ = this.authService.currentUser$.pipe(
+        map(user => user.name)
+    );
+
     formIcons = formIcons;
 
     invalidFeedback: string;
+    successFeedback: string;
+
+    constructor(
+        private authService: AuthService,
+    ) {}
+
+    submitName() {
+        this.invalidFeedback = undefined;
+        this.successFeedback = undefined;
+
+        this.authService.updateSettings({
+            name: 'test'
+        }).subscribe({
+            next: this.onSuccess.bind(this),
+            error: this.onRequestFail.bind(this),
+        })
+    }
+
+    private onSuccess() {
+        this.successFeedback = 'Your username has been updated.';
+    }
+
+    private onRequestFail(err) {
+        this.invalidFeedback = err.error?.username;
+    }
 }
