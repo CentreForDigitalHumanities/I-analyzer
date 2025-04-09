69bee7d3af55619dedd3907202717c54b52937a4
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-09 13:59:04 +0200

    update spec

--- frontend/src/app/settings/change-username/change-username.component.spec.ts
+++ frontend/src/app/settings/change-username/change-username.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangeUsernameComponent } from './change-username.component';
+import { SharedModule } from 'primeng/api';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '@services';
+import { AuthServiceMock } from 'mock-data/auth';
 
 describe('ChangeUsernameComponent', () => {
-  let component: ChangeUsernameComponent;
-  let fixture: ComponentFixture<ChangeUsernameComponent>;
+    let component: ChangeUsernameComponent;
+    let fixture: ComponentFixture<ChangeUsernameComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ChangeUsernameComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ChangeUsernameComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ChangeUsernameComponent],
+            imports: [
+                SharedModule,
+                ReactiveFormsModule,
+            ],
+            providers: [
+                { provide: AuthService, useClass: AuthServiceMock },
+            ],
+        })
+            .compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(ChangeUsernameComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
