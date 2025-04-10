e8a7beb0f5f0e69772368a03e042e5ff1a9f048a
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-10 11:36:40 +0200

    add celery utils

---
+++ backend/indexing/celery_utils.py
@@ -0,0 +1,21 @@
+import warnings
+
+from django.conf import settings
+from ianalyzer.celery import app
+
+
+def worker_is_active() -> bool:
+    '''
+    Check whether the app can connect to an active worker.
+    '''
+    inspector = app.control.inspect()
+    try:
+        workers = inspector.active()
+        return workers is not None and len(workers) > 0
+    except:
+        return False
+
+
+def warn_if_no_worker() -> None:
+    if not worker_is_active():
+        warnings.warn('No active celery worker.')
