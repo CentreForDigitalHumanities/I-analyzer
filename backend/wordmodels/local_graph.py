7ce4dc4c0fe295818a6b1101c84a516a3544e78d
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-16 16:15:24 +0200

    send vega doc

--- backend/wordmodels/local_graph.py
+++ backend/wordmodels/local_graph.py
@@ -11,7 +11,9 @@ def local_graph_data(corpus_name: str, query_term: str):
         {
             'start_year': wm['start_year'],
             'end_year': wm['end_year'],
-            'data': local_graph_in_timeframe(wm, query_term)
+            'graph': _graph_vega_doc(
+                *local_graph_in_timeframe(wm, query_term)
+            )
         }
         for wm in wm_list
     ]
@@ -21,10 +23,7 @@ def local_graph_in_timeframe(wm, query_term: str):
     nodes = _graph_nodes(wm, query_term)
     links = _graph_links(wm, nodes)
 
-    return {
-        'nodes': nodes,
-        'links': links
-    }
+    return nodes, links
 
 
 def _graph_nodes(wm, query_term):
@@ -68,3 +67,151 @@ def _graph_links(wm, nodes):
                     })
 
     return links
+
+
+def _graph_vega_doc(nodes, links):
+    return {
+        "$schema": "https://vega.github.io/schema/vega/v5.json",
+        "description": "A node-link diagram of neighbouring words",
+        "width": 700,
+        "height": 500,
+        "padding": 0,
+        "autosize": "none",
+
+        "signals": [
+            { "name": "cx", "update": "width / 2" },
+            { "name": "cy", "update": "height / 2" },
+            { "name": "nodeCharge", "value": -30,
+            "bind": {"input": "range", "min":-100, "max": 10, "step": 1} },
+            { "name": "linkDistance", "value": 30,
+            "bind": {"input": "range", "min": 5, "max": 100, "step": 1} },
+            { "name": "static", "value": True,
+            "bind": {"input": "checkbox"} },
+            {
+            "description": "State variable for active node fix status.",
+            "name": "fix", "value": False,
+            "on": [
+                {
+                "events": "symbol:pointerout[!event.buttons], window:pointerup",
+                "update": "false"
+                },
+                {
+                "events": "symbol:pointerover",
+                "update": "fix || true"
+                },
+                {
+                "events": "[symbol:pointerdown, window:pointerup] > window:pointermove!",
+                "update": "xy()",
+                "force": True
+                }
+            ]
+            },
+            {
+            "description": "Graph node most recently interacted with.",
+            "name": "node", "value": None,
+            "on": [
+                {
+                "events": "symbol:pointerover",
+                "update": "fix === true ? item() : node"
+                }
+            ]
+            },
+            {
+            "description": "Flag to restart Force simulation upon data changes.",
+            "name": "restart", "value": False,
+            "on": [
+                {"events": {"signal": "fix"}, "update": "fix && fix.length"}
+            ]
+            }
+        ],
+
+        "data": [
+            {
+            "name": "node-data",
+            'values': nodes,
+            "format": {"type": "json", "property": "nodes"}
+            },
+            {
+            "name": "link-data",
+            'values': links,
+            "format": {"type": "json", "property": "links"}
+            }
+        ],
+
+        "scales": [
+            {
+            "name": "color",
+            "type": "linear",
+            "domain": {"data": "node-data", "field": "similarity"},
+            "range": {"scheme": "blues"}
+            }
+        ],
+
+        "marks": [
+            {
+            "name": "nodes",
+            "type": "symbol",
+            "zindex": 1,
+
+            "from": {"data": "node-data"},
+            "on": [
+                {
+                "trigger": "fix",
+                "modify": "node",
+                "values": "fix === true ? {fx: node.x, fy: node.y} : {fx: fix[0], fy: fix[1]}"
+                },
+                {
+                "trigger": "!fix",
+                "modify": "node", "values": "{fx: null, fy: null}"
+                }
+            ],
+
+            "encode": {
+                "enter": {
+                "fill": {"scale": "color", "field": "group"},
+                "stroke": {"value": "white"}
+                },
+                "update": {
+                "size": 32,
+                "cursor": {"value": "pointer"}
+                }
+            },
+
+            "transform": [
+                {
+                "type": "force",
+                "iterations": 300,
+                "restart": {"signal": "restart"},
+                "static": {"signal": "static"},
+                "signal": "force",
+                "forces": [
+                    {"force": "center", "x": {"signal": "cx"}, "y": {"signal": "cy"}},
+                    {"force": "collide", "radius": 8},
+                    {"force": "nbody", "strength": {"signal": "nodeCharge"}},
+                    {"force": "link", "links": "link-data", "distance": {"signal": "linkDistance"}}
+                ]
+                }
+            ]
+            },
+            {
+            "type": "path",
+            "from": {"data": "link-data"},
+            "interactive": False,
+            "encode": {
+                "update": {
+                "stroke": {"value": "#ccc"},
+                "strokeWidth": {"value": 0.5}
+                }
+            },
+            "transform": [
+                {
+                "type": "linkpath",
+                "require": {"signal": "force"},
+                "shape": "line",
+                "sourceX": "datum.source.x", "sourceY": "datum.source.y",
+                "targetX": "datum.target.x", "targetY": "datum.target.y"
+                }
+            ]
+            }
+        ]
+    }
