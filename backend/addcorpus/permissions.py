a7a85fe0dc9ff0b6c71b0ef0d49bf9822a226d44
Luka van der Plas <lukavdplas@mailfence.com>
2025-04-29 15:59:29 +0200

    remove unused import

--- backend/addcorpus/permissions.py
+++ backend/addcorpus/permissions.py
@@ -1,7 +1,6 @@
 from rest_framework import permissions
 from rest_framework.exceptions import NotFound
 from rest_framework.request import Request
-from django.db.models import Q
 
 from addcorpus.models import Corpus, CorpusConfiguration
 
