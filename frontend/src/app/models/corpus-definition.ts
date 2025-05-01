efb2d82a1771ab6d195748bf5716a08cdf9d718e
Luka van der Plas <lukavdplas@mailfence.com>
2025-05-01 14:34:22 +0200

    add has_image field
    Co-Authored-By: Jelte van Boheemen <j.vanboheemen@uu.nl>

--- frontend/src/app/models/corpus-definition.ts
+++ frontend/src/app/models/corpus-definition.ts
@@ -84,10 +84,12 @@ export interface APIEditableCorpus {
     id?: number;
     active: boolean;
     definition: APICorpusDefinition;
+    has_image?: boolean;
 }
 
 export class CorpusDefinition {
     active = false;
+    hasImage = false;
     loading$ = new BehaviorSubject<boolean>(true);
 
     definition: APICorpusDefinition;
@@ -144,5 +146,6 @@ export class CorpusDefinition {
         this.active = result.active;
         this.setFromDefinition(result.definition);
         this.loading$.next(false);
+        this.hasImage = result.has_image;
     }
 }
