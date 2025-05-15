cfacadaff79ebcc7c3d812a85d0e4f364682ef88
Luka van der Plas <lukavdplas@mailfence.com>
2025-05-15 13:16:45 +0200

    add unit test

---
+++ backend/indexing/tests/test_stop.py
@@ -0,0 +1,27 @@
+from time import sleep
+
+from addcorpus.models import Corpus
+from indexing import models, create_job, run_job
+from indexing.stop_job import stop_job, is_stoppable
+
+
+def test_stop_job(transactional_db, mock_corpus, es_index_client, celery_app, celery_worker, monkeypatch):
+    # replace create function to ensure fixed execution time
+    def mock_create(task: models.CreateIndexTask):
+        sleep(60) # this is a long time, but the task will never complete
+        return task.index.name
+
+    monkeypatch.setattr(run_job, 'create', mock_create)
+
+    # start indexing job asynchronously
+    corpus = Corpus.objects.get(name=mock_corpus)
+    job =  create_job.create_indexing_job(corpus, mappings_only=True)
+    run_job.perform_indexing_async(job)
+
+    # wait for job to get started
+    sleep(0.5)
+
+    # stop
+    assert is_stoppable(job)
+    stop_job(job)
+    assert job.status() == models.TaskStatus.ABORTED
