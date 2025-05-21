4f18695359ab5c1f9e3d2da124a6db0204422944
Jelte van Boheemen <j.vanboheemen@uu.nl>
2025-05-21 16:12:29 +0200

    Delete converted csv files on donwload delete

--- backend/download/signals.py
+++ backend/download/signals.py
@@ -1,9 +1,13 @@
 import logging
+import os
+
 from django.conf import settings
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from download.convert_csv import output_path
+
 from .models import Download
-import os
+
 logger = logging.getLogger()
 
 
@@ -13,5 +17,11 @@ def after_download_delete(sender, instance, **kwargs):
         try:
             full_path = os.path.join(settings.CSV_FILES_PATH, instance.filename)
             os.remove(full_path)
+
+            converted_path = output_path(
+                settings.CSV_FILES_PATH, instance.filename)[0]
+            if os.path.exists(converted_path):
+                os.remove(converted_path)
+
         except Exception as e:
             logger.error(f"Error deleting file {instance.filename}: {e}")
