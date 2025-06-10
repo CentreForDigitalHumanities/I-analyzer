f4296c2596a3d10dd6fe5d7ca8d24edd46874450
Luka van der Plas <lukavdplas@mailfence.com>
2025-06-10 14:51:24 +0200

    update corpus, fix monkeypatch function def

--- backend/indexing/tests/test_stop.py
+++ backend/indexing/tests/test_stop.py
@@ -1,11 +1,12 @@
 from time import sleep
+from copy import copy
 
 from addcorpus.models import Corpus
 from indexing import models, create_job, run_job
 from indexing.stop_job import stop_job, is_stoppable
 
 
-def test_stop_job(transactional_db, mock_corpus, es_index_client, celery_app, celery_worker, monkeypatch):
+def test_stop_job(transactional_db, basic_mock_corpus, es_index_client, celery_app, celery_worker, monkeypatch):
     # replace create task handler to ensure fixed execution time
     # (note: because this skips creating the index, the index job will fail in a later
     # step if it is not interrupted)
@@ -13,10 +14,12 @@ def test_stop_job(transactional_db, mock_corpus, es_index_client, celery_app, ce
         sleep(60) # this is a long time, but the task will never complete
         return task.index.name
 
-    monkeypatch.setattr(run_job, 'create', mock_create)
+    mock_handlers = copy(run_job.TASK_HANDLERS)
+    mock_handlers[models.CreateIndexTask] = mock_create
+    monkeypatch.setattr(run_job, 'TASK_HANDLERS', mock_handlers)
 
     # start indexing job asynchronously
-    corpus = Corpus.objects.get(name=mock_corpus)
+    corpus = Corpus.objects.get(name=basic_mock_corpus)
     job =  create_job.create_indexing_job(corpus)
     result = run_job.perform_indexing_async(job)
 
