489b797ec0c51d21e1d536fa0c7a5991433acbe7
Luka van der Plas <lukavdplas@mailfence.com>
2025-07-04 15:07:10 +0200

    update test

--- backend/indexing/tests/test_stop.py
+++ backend/indexing/tests/test_stop.py
@@ -6,20 +6,15 @@ from indexing import models, create_job, run_job, stop_job
 
 
 def test_stop_job(transactional_db, basic_mock_corpus, es_index_client, celery_worker, monkeypatch):
-    # replace create task handler to ensure fixed execution time
-    # (note: because this skips creating the index, the index job will fail in a later
-    # step if it is not interrupted)
-    def mock_create(task: models.CreateIndexTask):
-        # this is a long time, but the task will never complete
+    # replace populate task handler to ensure fixed execution time
+    def mock_populate(task: models.PopulateIndexTask):
         for _ in range(20):
-            task.refresh_from_db()
-            if task.is_aborted():
-                return
+            stop_job.raise_if_aborted(task)
             sleep(1)
         return task.index.name
 
     mock_handlers = copy(run_job.TASK_HANDLERS)
-    mock_handlers[models.CreateIndexTask] = mock_create
+    mock_handlers[models.PopulateIndexTask] = mock_populate
     monkeypatch.setattr(run_job, 'TASK_HANDLERS', mock_handlers)
 
     # start indexing job asynchronously
