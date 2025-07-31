f75043d2ddc416ec464a0b0a4bafe1e2ae01efe3
Luka van der Plas <lukavdplas@mailfence.com>
2025-07-31 17:12:24 +0200

    add utils file to generate search field options

---
+++ frontend/src/app/utils/search-fields.ts
@@ -0,0 +1,32 @@
+import { Corpus, CorpusField } from '@models';
+import _ from 'lodash';
+
+export const searchFieldOptions = (corpus: Corpus): CorpusField[] => {
+    const searchableFields = corpus.fields.filter(field => field.searchable);
+    return _.flatMap(searchableFields, field => searchableVariants(field));
+}
+
+export const searchableVariants = (field: CorpusField): CorpusField[] => {
+    if (field.multiFields?.includes('text')) {
+        return [textVariant(field)];
+    } else if (field.multiFields?.includes('stemmed')) {
+        return [field, stemmedVariant(field)];
+    } else {
+        return [field];
+    }
+}
+
+const textVariant = (field: CorpusField): CorpusField => {
+    const variant = _.clone(field);
+    variant.name = field.name + '.text';
+    variant.multiFields = null;
+    return variant;
+}
+
+const stemmedVariant = (field: CorpusField): CorpusField => {
+    const variant = _.clone(field);
+    variant.name = field.name + '.stemmed';
+    variant.displayName = field.displayName + ' (stemmed)'
+    variant.multiFields = null;
+    return variant;
+}
