bb4a8c3813d53de5bbff3f3afd40a305e3988c9f
Luka van der Plas <lukavdplas@mailfence.com>
2025-08-01 16:12:21 +0200

    use searchFieldOptions in select-field component

--- frontend/src/app/select-field/select-field.component.ts
+++ frontend/src/app/select-field/select-field.component.ts
@@ -3,6 +3,7 @@ import * as _ from 'lodash';
 import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 import { CorpusField, QueryModel } from '@models/index';
 import { actionIcons } from '@shared/icons';
+import { searchFieldOptions } from '@utils/search-fields';
 
 @Component({
     selector: 'ia-select-field',
@@ -35,7 +36,7 @@ export class SelectFieldComponent implements OnChanges {
         } else {
             this.selectedFields = this.filterCoreFields();
         }
-        this.availableFields = this.getAvailableFields(this.corpusFields);
+        this.availableFields = searchFieldOptions(this.queryModel.corpus);
         this.optionFields = this.filterCoreFields();
     }
 
@@ -49,44 +50,6 @@ export class SelectFieldComponent implements OnChanges {
         } else {
             this.selectedFields = [];
         }
-    }
-
-    private getAvailableFields(corpusFields: CorpusField[]): CorpusField[] {
-        const availableFields = corpusFields.filter(field => field[this.filterCriterion]);
-        if (this.filterCriterion === 'searchable') {
-            return _.flatMap(availableFields, this.searchableMultiFields.bind(this)) as CorpusField[];
-        } else {
-            return availableFields;
-        }
-    }
-
-    private searchableMultiFields(field: CorpusField): CorpusField[] {
-        if (field.multiFields) {
-            if (field.multiFields.includes('text')) {
-                // replace keyword field with text multifield
-                return this.useTextMultifield(field);
-            }
-            if (field.multiFields.includes('stemmed')) {
-                return this.useStemmedMultifield(field);
-            }
-        }
-        return [field];
-    }
-
-    private useTextMultifield(field: CorpusField) {
-        const textField = _.clone(field);
-        textField.name = field.name + '.text';
-        textField.multiFields = null;
-        return [textField];
-    }
-
-    private useStemmedMultifield(field: CorpusField) {
-        const stemmedField = _.clone(field);
-        stemmedField.name = field.name + '.stemmed';
-        stemmedField.displayName = field.displayName + ' (stemmed)';
-        stemmedField.multiFields = null;
-
-        return [field, stemmedField];
     }
 
     public toggleAllFields() {
@@ -113,9 +76,9 @@ export class SelectFieldComponent implements OnChanges {
 
     private filterCoreFields() {
         if (this.filterCriterion === 'downloadable') {
-            return this.corpusFields.filter(field => field.csvCore);
+            return this.availableFields.filter(field => field.csvCore);
         } else if (this.filterCriterion === 'searchable') {
-            return this.corpusFields.filter(field => field.searchFieldCore);
+            return this.availableFields.filter(field => field.searchFieldCore);
         } else {
             return this.availableFields;
         }
