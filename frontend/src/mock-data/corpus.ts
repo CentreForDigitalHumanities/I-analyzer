4043119b20bf490e514e7822affb32f7a66b4948
Luka van der Plas <lukavdplas@mailfence.com>
2025-08-06 13:58:45 +0200

    create corpus factory functions

--- frontend/src/mock-data/corpus.ts
+++ frontend/src/mock-data/corpus.ts
@@ -4,6 +4,118 @@ import { findByName } from '../app/utils/utils';
 import { BooleanFilterOptions } from '../app/models/field-filter-options';
 import { Corpus, CorpusField } from '../app/models';
 
+export const keywordFieldFactory = (searchable = false): CorpusField => {
+    const mapping = searchable ?
+        { type: 'keyword', fields: { text: { type: 'text'}}} :
+        { type: 'keyword'};
+    return new CorpusField({
+        name: 'genre',
+        description: 'Genre of the document',
+        display_name: 'Genre',
+        display_type: 'keyword',
+        es_mapping: mapping,
+        hidden: false,
+        searchable,
+        downloadable: true,
+        search_filter: {
+            name: 'MultipleChoiceFilter',
+            option_count: 10,
+            description: 'Select genre',
+        },
+        results_overview: true,
+        search_field_core: false,
+        csv_core: true,
+        visualizations: ['resultscount', 'termfrequency'],
+        visualization_sort: '',
+        language: '',
+        sortable: false,
+        indexed: true,
+        required: false,
+    });
+};
+
+export const contentFieldFactory = (): CorpusField =>
+    new CorpusField({
+        name: 'content',
+        description: 'Text of the document',
+        display_name: 'Content',
+        display_type: 'text_content',
+        es_mapping: {
+            type: 'text',
+            term_vector: 'with_positions_offsets',
+            fields: {
+                length: { type: 'token_count', analyzer: 'standard' },
+                clean: { type: 'text', analyzer: 'clean_en', term_vector: 'with_positions_offsets' },
+                stemmed: { type: 'text', analyzer: 'stemmed_en', term_vector: 'with_positions_offsets' },
+            },
+        },
+        hidden: false,
+        sortable: false,
+        searchable: true,
+        downloadable: true,
+        search_filter: null,
+        results_overview: true,
+        search_field_core: true,
+        csv_core: true,
+        visualizations: ['wordcloud', 'ngram'],
+        visualization_sort: null,
+        indexed: true,
+        required: false,
+        language: 'eng',
+    });
+
+
+export const dateFieldFactory = () =>
+    new CorpusField({
+        name: 'date',
+        display_name: 'Date',
+        description: 'Publication date for the document',
+        display_type: 'date',
+        hidden: false,
+        sortable: true,
+        searchable: false,
+        downloadable: true,
+        search_filter: {
+            name: 'DateFilter',
+            lower: '1800-01-01',
+            upper: '1899-12-31',
+            description: 'Select date range for documents'
+        },
+        es_mapping: {type: 'date'},
+        results_overview: true,
+        search_field_core: false,
+        csv_core: true,
+        visualizations: ['resultscount', 'termfrequency'],
+        visualization_sort: null,
+        indexed: true,
+        required: false,
+        language: '',
+    });
+
+export const corpusFactory = () =>
+    new Corpus(
+        'test',
+        'Test corpus',
+        'A basic corpus for testing',
+        'test',
+        [
+            keywordFieldFactory(),
+            contentFieldFactory(),
+            dateFieldFactory(),
+        ],
+        1800,
+        1900,
+        '',
+        true,
+        false,
+        ['eng'],
+        'Books',
+        false,
+        null,
+        false,
+        [undefined, 'desc'],
+    );
+
 const mockFilterOptions: BooleanFilterOptions = {
     checked: false,
     name: 'BooleanFilter',
