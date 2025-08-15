d9422c809c9ba1a11cda534ba40389f5f5a052db
Luka van der Plas <lukavdplas@mailfence.com>
2025-08-15 18:14:35 +0200

    briefer traces of sound definition

--- backend/corpora/traces_of_sound/traces_of_sound.py
+++ backend/corpora/traces_of_sound/traces_of_sound.py
@@ -1,32 +1,15 @@
-
-from datetime import datetime
 from glob import glob
+from copy import copy
 
 from django.conf import settings
 from ianalyzer_readers.extract import CSV
 
+from addcorpus.es_mappings import keyword_mapping
 from addcorpus.es_settings import es_settings
 from addcorpus.python_corpora.corpus import CSVCorpusDefinition, FieldDefinition
 from addcorpus.python_corpora.filters import MultipleChoiceFilter
+from corpora.dutchnewspapers.dutchnewspapers_public import DutchNewspapersPublic
 
-from corpora.dutchnewspapers.field_definitions import (
-    article_category,
-    article_title,
-    circulation,
-    content,
-    date,
-    identifier,
-    issue_number,
-    language,
-    newspaper_title,
-    ocr_confidence,
-    publication_place,
-    publisher,
-    source,
-    temporal,
-    url,
-    version_of,
-)
 
 def extract_tag_category(data: str, category: str):
     tags = data.split(",")
@@ -48,67 +31,30 @@ class TracesOfSound(CSVCorpusDefinition):
 
     title = "Traces of Sound"
     description = "Collection of articiles from Dutch newspapers in the public domain with references to sound"
-    min_date = datetime(year=1600, month=1, day=1)
-    max_date = datetime(year=1876, month=12, day=31)
+    min_date = DutchNewspapersPublic.min_date
+    max_date = DutchNewspapersPublic.max_date
     data_directory = settings.TOS_DATA
     es_index = getattr(settings, 'TOS_ES_INDEX', 'traces-of-sound')
-    languages = ['nl']
-    category = 'periodical'
+    languages = DutchNewspapersPublic.languages
+    category = DutchNewspapersPublic.category
     description_page = 'traces_of_sound.md'
 
     @property
     def es_settings(self):
         return es_settings(self.languages[:1], stopword_analysis=True, stemming_analysis=True)
-    
-    def sources(self, start=min_date, end=max_date):
+
+    def sources(self, **kwargs):
         for csv_file in glob(f"{self.data_directory}/*.csv"):
-            yield csv_file, {}
-
-    article_category = article_category()
-    article_category.extractor = CSV('category')
-
-    article_title = article_title()
-    article_title.extractor = CSV('article_title')
-
-    circulation = circulation()
-    circulation.extractor = CSV('circulation')
-    
-    content = content()
-    content.extractor = CSV('content')
-    
-    date = date(min_date, max_date)
-    date.extractor = CSV('date')
-    
-    identifier = identifier()
-    identifier.extractor = CSV('id')
-
-    issue_number = issue_number()
-    issue_number.extractor = CSV('issue_number')
-    
-    language = language()
-    language.extractor = CSV('language')
-
-    newspaper_title = newspaper_title(10)
-    newspaper_title.extractor = CSV('newspaper_title')
-    
-    ocr_confidence = ocr_confidence()
-    ocr_confidence.extractor = CSV('ocr')
-    
-    publication_place = publication_place()
-    publication_place.extractor = CSV('publication_place')
-    
-    publisher = publisher()
-    publisher.extractor = CSV('publisher')
+            yield csv_file
 
     sound_carrier = FieldDefinition(
         name="sound_carrier",
         display_name="Sound carrier",
         description="The carrier of the sound.",
-        es_mapping={"type": "keyword"},
+        es_mapping=keyword_mapping(),
         extractor=CSV('tags', transform=extract_sound_carrier),
         search_filter=MultipleChoiceFilter(
             description="Accept only articles with these sound carriers.",
-            option_count=10,
         ),
     )
 
@@ -116,11 +62,10 @@ class TracesOfSound(CSVCorpusDefinition):
         name="sound_quality",
         display_name="Sound quality",
         description="The quality of the sound.",
-        es_mapping={"type": "keyword"},
+        es_mapping=keyword_mapping(),
         extractor=CSV('tags', transform=extract_sound_quality),
         search_filter=MultipleChoiceFilter(
             description="Accept only articles with these sound qualities.",
-            option_count=10,
         ),
     )
 
@@ -128,46 +73,24 @@ class TracesOfSound(CSVCorpusDefinition):
         name="sound_source",
         display_name="Sound source",
         description="The source of the sound.",
-        es_mapping={"type": "keyword"},
+        es_mapping=keyword_mapping(),
         extractor=CSV('tags', transform=extract_sound_source),
         search_filter=MultipleChoiceFilter(
             description="Accept only articles with these sound sources.",
-            option_count=10,
         ),
     )
-    
-    source = source()
-    source.extractor = CSV('source')
 
-    temporal = temporal()
-    temporal.extractor = CSV('temporal')
-    
-    url = url()
-    url.extractor = CSV('url')
-    
-    version_of = version_of()
-    version_of.extractor = CSV('version_of')
+    extra_fields = [sound_carrier, sound_quality, sound_source]
 
     @property
     def fields(self):
-        return [
-            self.date,
-            self.sound_carrier,
-            self.sound_quality,
-            self.sound_source,
-            self.article_category,
-            self.circulation,
-            self.ocr_confidence,
-            self.language,
-            self.newspaper_title,
-            self.temporal,
-            self.content,
-            self.article_title,
-            self.publisher,
-            self.publication_place,
-            self.identifier,
-            self.issue_number,
-            self.source,
-            self.url,
-            self.version_of,
+        base_fields = [
+            self._copy_field(field) for field in DutchNewspapersPublic().fields
         ]
+        return base_fields[:1] + self.extra_fields + base_fields[1:]
+
+
+    def _copy_field(self, field: FieldDefinition) -> FieldDefinition:
+        clone = copy(field)
+        clone.extractor = CSV(field.name)
+        return clone
