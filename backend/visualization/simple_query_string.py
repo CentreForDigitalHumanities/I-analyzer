c3d10ca2d0c9f3948c4c90123f4c5f6bba984ed9
Luka van der Plas <lukavdplas@mailfence.com>
2025-09-02 18:06:25 +0200

    improve collect_terms
    handle parenthesis, proximity search
    
    add docstring

--- backend/visualization/simple_query_string.py
+++ backend/visualization/simple_query_string.py
@@ -3,38 +3,56 @@ Utilities for Simple Query String syntax
 '''
 
 from typing import List, Tuple, Optional
+import re
 
 def collect_terms(query_text: str) -> List[str]:
     '''
     Return all terms in a simple query string.
+
+    Splits a query into terms, which may be either phrase terms or single terms. Operators
+    are not included.
+
+    Returns a list of strings, each of which is a single term. These may still include
+    decorators like fuzzy search, negation, or quotation marks for phrase terms.
     '''
 
-    components = []
-    in_qoutes, outside_qoutes = _extract_phrase_term(query_text)
+    if '(' in query_text and ')' in query_text:
+        in_group, outside_group = _extract_group(query_text, '(', ')', False)
+        if in_group:
+            return collect_terms(in_group) + collect_terms(outside_group)
 
-    while in_qoutes != None:
-        components.append(in_qoutes)
-        in_qoutes, outside_qoutes = _extract_phrase_term(outside_qoutes)
+    if match := re.search(r'\"~\d', query_text):
+        closing = match.group(0)
+        phrase, outside_phrase = _extract_group(query_text, '"', closing, True)
+        if phrase:
+            return [phrase] + collect_terms(outside_phrase)
 
-    components += outside_qoutes.split()
+    if '"' in query_text:
+        in_qoutes, outside_qoutes = _extract_group(query_text, '"', '"', True)
+        if in_qoutes:
+            return [in_qoutes] + collect_terms(outside_qoutes)
 
-    return components
+    terms = list(filter(_is_term, query_text.split()))
+    return terms
 
-def _extract_phrase_term(query_text: str) -> Tuple[Optional[str], str]:
-    '''
-    Extract the first phrase term from a simple query string.
 
-    Returns a tuple with the first phrase term (if any), and the query string with the
-    phrase term removed.
-    '''
-    if query_text.find('"') != -1:
-        starting_quote = query_text.find('"')
-        ending_quote = query_text.find('"', starting_quote + 1)
+def _is_term(query_part: str) -> bool:
+    return query_part not in ['+', '|']
 
-        if ending_quote != -1:
-            in_quotes = query_text[starting_quote + 1 : ending_quote]
-            outside_quotes = query_text[:starting_quote] + query_text[ending_quote + 1:]
-            return in_quotes, outside_quotes
+
+def _extract_group(
+    query_text: str, opening: str, closing: str, keep_bounds: bool,
+) -> Tuple[Optional[str], str]:
+    opens_at = query_text.find(opening)
+    closes_at = query_text.find(closing, opens_at + len(opening))
+
+    if opens_at != -1 and closes_at != -1:
+        if keep_bounds:
+            in_group = query_text[opens_at : closes_at  + len(closing)]
+        else:
+            in_group = query_text[opens_at + len(opening) : closes_at]
+        outside_group = query_text[:opens_at] + query_text[closes_at + len(closing):]
+        return in_group, outside_group
 
     return (None, query_text)
 
