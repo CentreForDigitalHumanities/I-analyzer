6eee078cc25a6365e4afd456329befc2f15eacd3
Luka van der Plas <lukavdplas@mailfence.com>
2025-10-06 16:07:39 +0200

    update us news corpus import

--- backend/corpora/news_us_19/test_news_us_19.py
+++ backend/corpora/news_us_19/test_news_us_19.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 def test_news_us_19(settings, db, admin_client):
     settings.CORPORA = {
-        'news_us_19': os.path.join(here, 'news_us_19.py')
+        'news_us_19': 'corpora.news_us_19.news_us_19.NewsUS'
     }
     settings.NEWS_US_19_DATA = ''
 
