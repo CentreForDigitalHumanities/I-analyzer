6392c40e06ce1acfa7e9e1d3a892ae4c51df46b3
Luka van der Plas <lukavdplas@mailfence.com>
2025-10-06 16:05:59 +0200

    update traces of sound import

--- backend/corpora/traces_of_sound/conftest.py
+++ backend/corpora/traces_of_sound/conftest.py
@@ -6,6 +6,6 @@ here = abspath(dirname(__file__))
 @pytest.fixture()
 def traces_corpora_settings(settings):
     settings.CORPORA = {
-        'traces-of-sound': join(here, 'traces_of_sound.py'),
+        'traces-of-sound': 'corpora.traces_of_sound.traces_of_sound.TracesOfSound',
     }
     settings.TRACES_OF_SOUND_DATA = join(here, 'tests', 'data')
