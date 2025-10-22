cc84cb45424d331c24ceb2b7a94927f4ddc99755
Luka van der Plas <lukavdplas@mailfence.com>
2025-10-22 19:48:49 +0200

    nest balloon and toggle-button in shared module

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -9,7 +9,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DatePickerModule } from 'primeng/datepicker';
 import { DialogModule } from 'primeng/dialog';
 import { TableModule } from 'primeng/table';
-import { BalloonDirective } from '../balloon.directive';
+import { BalloonDirective } from './balloon/balloon.directive';
 import { DatePickerComponent } from '../corpus-selection/corpus-filter/date-picker/date-picker.component';
 import { ErrorComponent } from './error/error.component';
 import { ScrollToDirective } from '../scroll-to.directive';
@@ -18,7 +18,7 @@ import { TabPanelDirective } from './tabs/tab-panel.directive';
 import { TabsComponent } from './tabs/tabs.component';
 import { ToggleComponent } from './toggle/toggle.component';
 import { SlugifyPipe } from './pipes/slugify.pipe';
-import { ToggleButtonDirective } from './toggle-button.directive';
+import { ToggleButtonDirective } from './toggle-button/toggle-button.directive';
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 
 /** this should be imported by all other modules; provides basic building blocks
