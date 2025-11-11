fc57f09b1b9ff171cfe53e5a28bdf4c1a91b2ed8
Luka van der Plas <lukavdplas@mailfence.com>
2025-11-11 17:41:12 +0100

    adjust title weight + spacing

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
     appName: 'Textcavator',
     navbarBrand: {
         title: 'Textcavator',
-        subtitle: 'People & Parliament',
+        subtitle: undefined,
         logo: '/assets/logo.png',
         logoAlt: undefined,
     },
