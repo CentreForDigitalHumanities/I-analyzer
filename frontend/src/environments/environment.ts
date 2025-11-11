a50638cf117cee64ce6ee0f56a1f258374341fb8
Luka van der Plas <lukavdplas@mailfence.com>
2025-11-11 15:10:27 +0100

    brand styling

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -11,7 +11,7 @@ export const environment = {
     appName: 'Textcavator',
     navbarBrand: {
         title: 'Textcavator',
-        subtitle: undefined,
+        subtitle: 'People & Parliament',
         logo: '/assets/logo.png',
         logoAlt: undefined,
     },
